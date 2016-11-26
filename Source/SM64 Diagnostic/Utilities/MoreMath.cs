2df8adad70a49f9f22512a38738a90a6c776055b
danebouchie <danebouchie@knights.ucf.edu>
2016-11-26 13:49:33 -0500

    Bugfix: distance calculations not using X coordinate

--- Source/SM64 Diagnostic/Utilities/MoreMath.cs
+++ Source/SM64 Diagnostic/Utilities/MoreMath.cs
@@ -11,7 +11,7 @@ namespace SM64_Diagnostic.Utilities
         public static double DistanceTo(float x1, float y1, float z1, float x2, float y2, float z2)
         {
             float dx, dy, dz;
-            dx = x1 - x1;
+            dx = x1 - x2;
             dy = y1 - y2;
             dz = z1 - z2;
             return Math.Sqrt(dx * dx + dy * dy + dz * dz);
@@ -20,7 +20,7 @@ namespace SM64_Diagnostic.Utilities
         public static double DistanceTo(float x1, float y1, float x2, float y2)
         {
             float dx, dy;
-            dx = x1 - x1;
+            dx = x1 - x2;
             dy = y1 - y2;
             return Math.Sqrt(dx * dx + dy * dy);
         }
