83c8a9f0316dbc42c736e262171d187cdc39b646
scob <scob@seas.upenn.edu>
2017-07-01 11:38:28 -0400

    added functionality for expanded pendulum swing indexes

--- Source/SM64 Diagnostic/Structs/PendulumSwingTable.cs
+++ Source/SM64 Diagnostic/Structs/PendulumSwingTable.cs
@@ -32,10 +32,26 @@ namespace SM64_Diagnostic.Structs
 
         public int? GetPendulumSwingIndex(int amplitude)
         {
-            if (!_table.ContainsKey(amplitude))
-                return null;
+            if (_table.ContainsKey(amplitude))
+                return _table[amplitude].Index;
 
-            return _table[amplitude].Index;
+            // Check for pendulum swings beyond the standard indexes
+            int absAmplitude = Math.Abs(amplitude);
+            int tenativeFrames = (int)((-21 + Math.Sqrt(441 + 84 * absAmplitude)) / 42);
+            int tentativeAmplitude = tenativeFrames * (tenativeFrames + 1) * 21;
+            if (absAmplitude == tentativeAmplitude && absAmplitude > 7182)
+            {
+                if ((amplitude > 0) == (tenativeFrames % 2 == 0)) // beyond forward indexes
+                {
+                    return tenativeFrames + 270;
+                }
+                else // beyond backward indexes
+                {
+                    return -1 * tenativeFrames - 363;
+                }
+            }
+
+            return null;
         }
     }
 }
