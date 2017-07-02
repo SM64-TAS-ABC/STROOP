22f593a8a0444099c471635fb04e8ec95889811b
scob <scob@seas.upenn.edu>
2017-07-02 16:54:22 -0400

    added dummy controller image classes

---
+++ Source/SM64 Diagnostic/Structs/Gui/ControllerImageGui.cs
@@ -0,0 +1,50 @@
+﻿using SM64_Diagnostic.Controls;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SM64_Diagnostic.Structs
+{
+    public class ControllerImageGui
+    {
+        public Image SelectedObjectOverlayImage;
+        public Image TrackedAndShownObjectOverlayImage;
+        public Image TrackedNotShownObjectOverlayImage;
+        public Image StoodOnObjectOverlayImage;
+        public Image HeldObjectOverlayImage;
+        public Image InteractionObjectOverlayImage;
+        public Image UsedObjectOverlayImage;
+        public Image ClosestObjectOverlayImage;
+        public Image CameraObjectOverlayImage;
+        public Image FloorObjectOverlayImage;
+        public Image WallObjectOverlayImage;
+        public Image CeilingObjectOverlayImage;
+
+        public CheckBox LockLabelsCheckbox;
+        public TabControl TabControl;
+        public ComboBox MapObjectToggleModeComboBox;
+        public ComboBox SortMethodComboBox;
+        public ComboBox LabelMethodComboBox;
+        public NoTearFlowLayoutPanel FlowLayoutContainer;
+
+        ~ControllerImageGui()
+        {
+            SelectedObjectOverlayImage?.Dispose();
+            TrackedAndShownObjectOverlayImage?.Dispose();
+            TrackedNotShownObjectOverlayImage?.Dispose();
+            StoodOnObjectOverlayImage?.Dispose();
+            HeldObjectOverlayImage?.Dispose();
+            InteractionObjectOverlayImage?.Dispose();
+            UsedObjectOverlayImage?.Dispose();
+            ClosestObjectOverlayImage?.Dispose();
+            CameraObjectOverlayImage?.Dispose();
+            FloorObjectOverlayImage?.Dispose();
+            WallObjectOverlayImage?.Dispose();
+            CeilingObjectOverlayImage?.Dispose();
+        }
+    }
+}
