69dbe9dd21de522780d2e2fb334a9fb8002cac01
scob <scob@seas.upenn.edu>
2017-07-02 12:28:19 -0400

    added dummy controller display class

---
+++ Source/SM64 Diagnostic/Controls/ControllerDisplay.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using SM64_Diagnostic.Managers;
+using SM64_Diagnostic.Utilities;
+using SM64_Diagnostic.Structs;
+using SM64_Diagnostic.Controls;
+using SM64_Diagnostic.Extensions;
+using System.Drawing.Drawing2D;
+
+namespace SM64_Diagnostic
+{
+    public class ControllerDisplay : Panel
+    {
+        public static TabControl tabControlMain;
+
+        object _gfxLock = new object();
+
+        public ControllerDisplay()
+        {
+        }
+
+        /*
+        private void RebufferObjectImage()
+        {
+            // Remove last image reference
+            _bufferedObjectImage = null;
+
+            // Make sure object needs a new image
+            if (_objectImage == null)
+                return;
+
+            // Calculate new rectangle to draw image
+            var objectImageRec = (new Rectangle(BorderSize, BorderSize + 1,
+            Width - BorderSize * 2, _textLocation.Y - 1 - BorderSize))
+            .Zoom(_objectImage.Size);
+            _objectImageLocation = objectImageRec.Location;
+
+            // If the image is too small, we don't need to draw it
+            if (objectImageRec.Height <= 0 || objectImageRec.Width <= 0)
+            {
+                _bufferedObjectImage = new Bitmap(1, 1);
+                return;
+            }
+
+            // Look for cached image and use it if it exists
+            _bufferedObjectImage = _manager.ObjectAssoc.GetCachedBufferedObjectImage(_objectImage, objectImageRec.Size);
+            if (_bufferedObjectImage != null)
+                return;
+
+            // Otherwise create new image and add it to cache
+            _bufferedObjectImage = new Bitmap(objectImageRec.Width, objectImageRec.Height);
+            objectImageRec.Location = new Point();
+            using (var graphics = Graphics.FromImage(_bufferedObjectImage))
+            {
+                graphics.InterpolationMode = InterpolationMode.High;
+                graphics.DrawImage(_objectImage, objectImageRec);
+            }
+
+            _manager.ObjectAssoc.CreateCachedBufferedObjectImage(_objectImage, _bufferedObjectImage);
+        }
+        */
+
+        /*
+        public void UpdateColors(bool force = false)
+        {
+            var oldBorderColor = _borderColor;
+            var oldBackColor = _backColor;
+            bool imageUpdated = false;
+            var newColor = _mainColor;
+            switch (MouseState)
+            {
+                case MouseStateType.Down:
+                    _borderColor = newColor.Darken(0.5);
+                    _backColor = newColor.Darken(0.5).Lighten(0.5);
+                    break;
+                case MouseStateType.Over:
+                    _borderColor = newColor.Lighten(0.5);
+                    _backColor = newColor.Lighten(0.85);
+                    break;
+                default:
+                    _borderColor = newColor;
+                    _backColor = newColor.Lighten(0.7);
+                    break;
+            }
+            Image newImage = _manager.ObjectAssoc.GetObjectImage(_behavior, !_active);
+            if (_objectImage != newImage)
+            {
+                lock (_gfxLock)
+                {
+                    _objectImage = newImage;
+                    RebufferObjectImage();
+                }
+                imageUpdated = true;
+            }
+
+            bool colorUpdated = false;
+            colorUpdated |= (_backColor != oldBackColor);
+            colorUpdated |= (_borderColor != oldBorderColor);
+
+            if (colorUpdated)
+            {
+                lock (_gfxLock)
+                {
+                    _borderBrush.Color = _borderColor;
+                    _backBrush.Color = _backColor;
+                }
+            }
+
+            if (!imageUpdated && !colorUpdated && !force)
+                return;
+
+            Invalidate();
+        }
+        */
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+
+            /*
+            lock (_gfxLock)
+            {
+                // Border
+                e.Graphics.FillRectangle(_borderBrush, new Rectangle(new Point(), Size));
+
+                // Background
+                e.Graphics.FillRectangle(_backBrush, new Rectangle(BorderSize, BorderSize, Width - BorderSize * 2, Height - BorderSize * 2));
+
+                // Change font size
+                if (Height != prevHeight)
+                {
+                    prevHeight = Height;
+                    Font?.Dispose();
+                    Font = new Font(FontFamily.GenericSansSerif, Math.Max(6, 6 / 40.0f * Height));
+                }
+
+                // Draw Text
+                var textSize = TextRenderer.MeasureText(e.Graphics, Text, Font);
+                var textLocation = new Point((int)(Width - textSize.Width) / 2, (int)(Height - textSize.Height - BorderSize));
+                TextRenderer.DrawText(e.Graphics, Text, Font, textLocation, TextColor);
+                if (textLocation != _textLocation)
+                {
+                    _textLocation = textLocation;
+                    RebufferObjectImage();
+                }
+
+                // Draw Object Image
+                if (_objectImage != null)
+                {
+                    try
+                    {
+                        e.Graphics.DrawImageUnscaled(_bufferedObjectImage, _objectImageLocation);
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        // The buffered image may have gotten disposed
+                        RebufferObjectImage();
+                        Invalidate();
+                        return;
+                    }
+                }
+            }
+            */
+
+            // Draw Overlays
+
+            /*
+            if (_drawWallObject)
+                e.Graphics.DrawImage(_gui.WallObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawFloorObject)
+                e.Graphics.DrawImage(_gui.FloorObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawCeilingObject)
+                e.Graphics.DrawImage(_gui.CeilingObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawInteractionObject)
+                e.Graphics.DrawImage(_gui.InteractionObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawHeldOverlay)
+                e.Graphics.DrawImage(_gui.HeldObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawStoodOnOverlay)
+                e.Graphics.DrawImage(_gui.StoodOnObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawUsedObject)
+                e.Graphics.DrawImage(_gui.UsedObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawClosestOverlay)
+                e.Graphics.DrawImage(_gui.ClosestObjectOverlayImage, new Rectangle(new Point(), Size));
+            if (_drawCameraOverlay)
+                e.Graphics.DrawImage(_gui.CameraObjectOverlayImage, new Rectangle(new Point(), Size));
+                */
+        }
+
+
+    }
+}
