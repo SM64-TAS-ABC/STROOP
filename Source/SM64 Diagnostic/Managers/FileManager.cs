01f7bdb9566e2449bef8ddee2e875e5fdca87c67
scob <scob@seas.upenn.edu>
2017-07-04 15:45:26 -0400

    dummy implementation of file manager

--- Source/SM64 Diagnostic/Managers/FileManager.cs
+++ Source/SM64 Diagnostic/Managers/FileManager.cs
@@ -7,25 +7,97 @@ using SM64_Diagnostic.Structs;
 using System.Windows.Forms;
 using SM64_Diagnostic.Utilities;
 using SM64_Diagnostic.Controls;
+using SM64_Diagnostic.Structs.Configurations;
+using SM64Diagnostic.Controls;
 
 namespace SM64_Diagnostic.Managers
 {
     public class FileManager : DataManager
     {
-        Control _tabControl;
+        TabPage _tabControl;
+        uint _triangleAddress = 0;
+        CheckBox _useMisalignmentOffsetCheckbox;
+        int _closestVertex = 0;
 
-        public FileManager(ProcessStream stream, List<WatchVariable> fileData, Control tabControl, NoTearFlowLayoutPanel noTearFlowLayoutPanelFile)
+        uint TriangleAddress
+        {
+            get
+            {
+                return _triangleAddress;
+            }
+            set
+            {
+                if (_triangleAddress == value)
+                    return;
+
+                _triangleAddress = value;
+
+                foreach (var dataContainer in _dataControls)
+                {
+                    if (dataContainer is WatchVariableControl)
+                    {
+                        var watchVar = dataContainer as WatchVariableControl;
+                        watchVar.OtherOffsets = new List<uint>() { _triangleAddress };
+                    }
+                }
+            }
+        }
+
+        public enum TriangleMode { Floor, Wall, Ceiling, Other };
+
+        public TriangleMode Mode = TriangleMode.Floor;
+
+        public FileManager(ProcessStream stream, List<WatchVariable> fileData, TabPage tabControl, NoTearFlowLayoutPanel noTearFlowLayoutPanelFile)
             : base(stream, fileData, noTearFlowLayoutPanelFile)
         {
             _tabControl = tabControl;
 
-            SplitContainer splitContainerHud = tabControl.Controls["splitContainerFile"] as SplitContainer;
+            SplitContainer splitContainerTriangles = tabControl.Controls["splitContainerFile"] as SplitContainer;
 
             /*
-            (splitContainerHud.Panel1.Controls["buttonFillHp"] as Button).Click += buttonFill_Click;
-            (splitContainerHud.Panel1.Controls["buttonDie"] as Button).Click += buttonDie_Click;
-            (splitContainerHud.Panel1.Controls["buttonStandardHud"] as Button).Click += buttonStandardHud_Click;
-            */
+            _useMisalignmentOffsetCheckbox = splitContainerTriangles.Panel1.Controls["checkBoxVertexMisalignment"] as CheckBox;
+
+            (splitContainerTriangles.Panel1.Controls["radioButtonTriFloor"] as RadioButton).CheckedChanged
+                += (sender, e) => Mode_CheckedChanged(sender, e, TriangleMode.Floor);
+            (splitContainerTriangles.Panel1.Controls["radioButtonTriWall"] as RadioButton).CheckedChanged
+                += (sender, e) => Mode_CheckedChanged(sender, e, TriangleMode.Wall);
+            (splitContainerTriangles.Panel1.Controls["radioButtonTriCeiling"] as RadioButton).CheckedChanged
+                += (sender, e) => Mode_CheckedChanged(sender, e, TriangleMode.Ceiling);
+            (splitContainerTriangles.Panel1.Controls["radioButtonTriOther"] as RadioButton).CheckedChanged
+                += (sender, e) => Mode_CheckedChanged(sender, e, TriangleMode.Other);
+                */
+
+        }
+
+        private void Mode_CheckedChanged(object sender, EventArgs e, TriangleMode mode)
+        {
+            if (!(sender as RadioButton).Checked)
+                return;
+
+            Mode = mode;
+        }
+
+        public override void Update(bool updateView)
+        {
+            if (updateView)
+            {
+                switch (Mode)
+                {
+                    case TriangleMode.Ceiling:
+                        TriangleAddress = _stream.GetUInt32(Config.Mario.StructAddress + Config.Mario.CeilingTriangleOffset);
+                        break;
+
+                    case TriangleMode.Floor:
+                        TriangleAddress = _stream.GetUInt32(Config.Mario.StructAddress + Config.Mario.FloorTriangleOffset);
+                        break;
+
+                    case TriangleMode.Wall:
+                        TriangleAddress = _stream.GetUInt32(Config.Mario.StructAddress + Config.Mario.WallTriangleOffset);
+                        break;
+                }
+
+                base.Update(updateView);
+            }
         }
     }
 }
