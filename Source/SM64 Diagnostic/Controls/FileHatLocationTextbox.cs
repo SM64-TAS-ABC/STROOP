d7d95a4b694274d29c8e0b8937424eb506c1ec5a
scob <scob@seas.upenn.edu>
2017-07-09 12:22:38 -0400

    added unused hat location textbox class

---
+++ Source/SM64 Diagnostic/Controls/FileHatLocationTextbox.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using SM64_Diagnostic.Managers;
+using SM64_Diagnostic.Utilities;
+using SM64_Diagnostic.Structs;
+using SM64_Diagnostic.Controls;
+using SM64_Diagnostic.Extensions;
+using System.Drawing.Drawing2D;
+
+namespace SM64_Diagnostic
+{
+    public class FileHatLocationTextbox : FileTextbox
+    {
+        private short _currentValue;
+
+        public FileHatLocationTextbox()
+        {
+        }
+
+        public override void Initialize(ProcessStream stream, uint addressOffset)
+        {
+            base.Initialize(stream, addressOffset);
+
+            _currentValue = GetHatLocationValueFromMemory();
+            this.Text = _currentValue.ToString();
+        }
+
+        private short GetHatLocationValueFromMemory()
+        {
+            return _stream.GetInt16(FileManager.Instance.CurrentFileAddress + _addressOffset);
+        }
+
+        protected override void SubmitValue()
+        {
+            short value;
+            if (!short.TryParse(this.Text, out value))
+            {
+                this.Text = GetHatLocationValueFromMemory().ToString();
+                return;
+            }
+
+            _stream.SetValue(value, FileManager.Instance.CurrentFileAddress + _addressOffset);
+        }
+
+        public override void UpdateText()
+        {
+            short value = GetHatLocationValueFromMemory();
+            if (_currentValue != value)
+            {
+                this.Text = value.ToString();
+                _currentValue = value;
+            }
+        }
+    }
+}
