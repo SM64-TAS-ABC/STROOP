67a4eba5ed38ac74f9b550e69620f695fe23781f
scob <scob@seas.upenn.edu>
2017-08-04 21:05:28 -0400

    turn on/off hud button text updates

--- Source/SM64 Diagnostic/Managers/HudManager.cs
+++ Source/SM64 Diagnostic/Managers/HudManager.cs
@@ -7,12 +7,15 @@ using SM64_Diagnostic.Structs;
 using System.Windows.Forms;
 using SM64_Diagnostic.Utilities;
 using SM64_Diagnostic.Controls;
+using SM64_Diagnostic.Structs.Configurations;
 
 namespace SM64_Diagnostic.Managers
 {
     public class HudManager : DataManager
     {
         Control _tabControl;
+        bool _turnOnHud;
+        Button _turnOnOffHudButton;
 
         public HudManager(List<WatchVariable> hudData, Control tabControl, NoTearFlowLayoutPanel noTearFlowLayoutPanelHud)
             : base(hudData, noTearFlowLayoutPanelHud)
@@ -25,6 +28,23 @@ namespace SM64_Diagnostic.Managers
             (splitContainerHud.Panel1.Controls["buttonDie"] as Button).Click += (sender, e) => ButtonUtilities.Die();
             (splitContainerHud.Panel1.Controls["buttonStandardHud"] as Button).Click += (sender, e) => ButtonUtilities.StandardHud();
             (splitContainerHud.Panel1.Controls["button99Coins"] as Button).Click += (sender, e) => ButtonUtilities.Coins99();
+            _turnOnOffHudButton = splitContainerHud.Panel1.Controls["buttonTurnOnOffHud"] as Button;
+        }
+
+        public override void Update(bool updateView)
+        {
+            uint hudAddressOffset = 0xFB;
+            uint hudMask = 0x0F;
+            bool turnOnHud = (Config.Stream.GetByte(Config.Mario.StructAddress + hudAddressOffset) & hudMask) == 0;
+            if (turnOnHud != _turnOnHud)
+            {
+                _turnOnHud = turnOnHud;
+
+                // Update button text
+                _turnOnOffHudButton.Text = _turnOnHud ? "Turn On HUD" : "Turn Off HUD";
+            }
+
+            base.Update(updateView);
         }
     }
 }
