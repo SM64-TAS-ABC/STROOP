68aec90063e224ae6369642cefdd4e76a3d8859e
scob <scob@seas.upenn.edu>
2017-08-04 21:25:38 -0400

    created binary button class

---
+++ Source/SM64 Diagnostic/Controls/BinaryButton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using SM64_Diagnostic.Managers;
+using SM64_Diagnostic.Utilities;
+using SM64_Diagnostic.Structs;
+using SM64_Diagnostic.Controls;
+using SM64_Diagnostic.Extensions;
+using System.Drawing.Drawing2D;
+
+namespace SM64_Diagnostic
+{
+    public class BinaryButton : Button
+    {
+        private string _primaryText;
+        private string _secondaryText;
+        private Func<bool> _isSecondaryFunction;
+
+        private bool _isSecondary;
+
+        public BinaryButton()
+        {
+        }
+
+        public void Initialize(string primaryText, string secondaryText, Action primaryAction, Action secondaryAction, Func<bool> isSecondaryFunction)
+        {
+            _primaryText = primaryText;
+            _secondaryText = secondaryText;
+            _isSecondaryFunction = isSecondaryFunction;
+
+            base.Click += (sender, e) =>
+            {
+                if (_isSecondary) secondaryAction();
+                else primaryAction();
+            };
+        }
+
+        public void UpdateButton()
+        {
+            bool isSecondary = _isSecondaryFunction();
+            _isSecondary = isSecondary;
+            base.Text = isSecondary ? _secondaryText : _primaryText;
+        }
+    }
+}
