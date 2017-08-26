10dacef73d1b02be6fcb9f5a583524d007144f75
scob <scob@seas.upenn.edu>
2017-08-26 10:41:00 -0400

    created a better textbox

---
+++ Source/SM64 Diagnostic/Controls/BetterTextbox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using SM64_Diagnostic.Managers;
+using SM64_Diagnostic.Utilities;
+using SM64_Diagnostic.Structs;
+using SM64_Diagnostic.Controls;
+using SM64_Diagnostic.Extensions;
+using System.Drawing.Drawing2D;
+
+namespace SM64_Diagnostic
+{
+    public class BetterTextbox : TextBox
+    {
+
+    }
+}
