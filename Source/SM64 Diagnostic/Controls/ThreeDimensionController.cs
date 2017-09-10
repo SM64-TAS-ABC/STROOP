ebb27e2460bdeeff4f533ce471e940e28cd3dc96
scob <scob@seas.upenn.edu>
2017-09-10 09:07:14 -0400

    added dummy tool strip menu for 3d controllers

--- Source/SM64 Diagnostic/Controls/ThreeDimensionController.cs
+++ Source/SM64 Diagnostic/Controls/ThreeDimensionController.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 namespace SM64Diagnostic.Controls
 {
     public enum CoordinateSystem { Euler, Spherical };
+    public enum FacingDirection { Left, Right, Up, Down, UpLeft, DownLeft, UpRight, DownRight };
 
     public static class ThreeDimensionController
     {
@@ -17,14 +18,14 @@ namespace SM64Diagnostic.Controls
             GroupBox groupbox,
             Button buttonSquareLeft,
             Button buttonSquareRight,
-            Button buttonSquareTop,
-            Button buttonSquareBottom,
-            Button buttonSquareTopLeft,
-            Button buttonSquareBottomLeft,
-            Button buttonSquareTopRight,
-            Button buttonSquareBottomRight,
-            Button buttonLineTop,
-            Button buttonLineBottom,
+            Button buttonSquareUp,
+            Button buttonSquareDown,
+            Button buttonSquareUpLeft,
+            Button buttonSquareDownLeft,
+            Button buttonSquareUpRight,
+            Button buttonSquareDownRight,
+            Button buttonLineUp,
+            Button buttonLineDown,
             TextBox textboxSquare,
             TextBox textboxLine,
             CheckBox checkbox,
@@ -48,28 +49,28 @@ namespace SM64Diagnostic.Controls
             {
                 buttonSquareLeft.Text = "X-";
                 buttonSquareRight.Text = "X+";
-                buttonSquareTop.Text = "Z-";
-                buttonSquareBottom.Text = "Z+";
-                buttonSquareTopLeft.Text = "X-Z-";
-                buttonSquareBottomLeft.Text = "X-Z+";
-                buttonSquareTopRight.Text = "X+Z-";
-                buttonSquareBottomRight.Text = "X+Z+";
-                buttonLineTop.Text = "Y+";
-                buttonLineBottom.Text = "Y-";
+                buttonSquareUp.Text = "Z-";
+                buttonSquareDown.Text = "Z+";
+                buttonSquareUpLeft.Text = "X-Z-";
+                buttonSquareDownLeft.Text = "X-Z+";
+                buttonSquareUpRight.Text = "X+Z-";
+                buttonSquareDownRight.Text = "X+Z+";
+                buttonLineUp.Text = "Y+";
+                buttonLineDown.Text = "Y-";
             };
 
             Action setRelativeNames = () =>
             {
                 buttonSquareLeft.Text = "L";
                 buttonSquareRight.Text = "R";
-                buttonSquareTop.Text = "F";
-                buttonSquareBottom.Text = "B";
-                buttonSquareTopLeft.Text = "FL";
-                buttonSquareBottomLeft.Text = "BL";
-                buttonSquareTopRight.Text = "FR";
-                buttonSquareBottomRight.Text = "BR";
-                buttonLineTop.Text = "U";
-                buttonLineBottom.Text = "D";
+                buttonSquareUp.Text = "F";
+                buttonSquareDown.Text = "B";
+                buttonSquareUpLeft.Text = "FL";
+                buttonSquareDownLeft.Text = "BL";
+                buttonSquareUpRight.Text = "FR";
+                buttonSquareDownRight.Text = "BR";
+                buttonLineUp.Text = "U";
+                buttonLineDown.Text = "D";
             };
 
             Action actionCheckedChanged = () =>
@@ -80,18 +81,62 @@ namespace SM64Diagnostic.Controls
 
             buttonSquareLeft.Click += (sender, e) => actionSquare(-1, 0);
             buttonSquareRight.Click += (sender, e) => actionSquare(1, 0);
-            buttonSquareTop.Click += (sender, e) => actionSquare(0, 1);
-            buttonSquareBottom.Click += (sender, e) => actionSquare(0, -1);
-            buttonSquareTopLeft.Click += (sender, e) => actionSquare(-1, 1);
-            buttonSquareBottomLeft.Click += (sender, e) => actionSquare(-1, -1);
-            buttonSquareTopRight.Click += (sender, e) => actionSquare(1, 1);
-            buttonSquareBottomRight.Click += (sender, e) => actionSquare(1, -1);
-            buttonLineTop.Click += (sender, e) => actionLine(1);
-            buttonLineBottom.Click += (sender, e) => actionLine(-1);
+            buttonSquareUp.Click += (sender, e) => actionSquare(0, 1);
+            buttonSquareDown.Click += (sender, e) => actionSquare(0, -1);
+            buttonSquareUpLeft.Click += (sender, e) => actionSquare(-1, 1);
+            buttonSquareDownLeft.Click += (sender, e) => actionSquare(-1, -1);
+            buttonSquareUpRight.Click += (sender, e) => actionSquare(1, 1);
+            buttonSquareDownRight.Click += (sender, e) => actionSquare(1, -1);
+            buttonLineUp.Click += (sender, e) => actionLine(1);
+            buttonLineDown.Click += (sender, e) => actionLine(-1);
             if (coordinateSystem == CoordinateSystem.Euler)
             {
                 checkbox.CheckedChanged += (sender, e) => actionCheckedChanged();
             }
+
+            ToolStripMenuItem itemLeft = new ToolStripMenuItem("Face Left");
+            ToolStripMenuItem itemRight = new ToolStripMenuItem("Face Right");
+            ToolStripMenuItem itemUp = new ToolStripMenuItem("Face Up");
+            ToolStripMenuItem itemDown = new ToolStripMenuItem("Face Down");
+            ToolStripMenuItem itemUpLeft = new ToolStripMenuItem("Face Up-Left");
+            ToolStripMenuItem itemDownLeft = new ToolStripMenuItem("Face Down-Left");
+            ToolStripMenuItem itemUpRight = new ToolStripMenuItem("Face Up-Right");
+            ToolStripMenuItem itemDownRight = new ToolStripMenuItem("Face Down-Right");
+
+            Action<FacingDirection> SetFacingDirection = (FacingDirection facingDirection) =>
+            {
+                itemLeft.Checked = facingDirection == FacingDirection.Left;
+                itemRight.Checked = facingDirection == FacingDirection.Right;
+                itemUp.Checked = facingDirection == FacingDirection.Up;
+                itemDown.Checked = facingDirection == FacingDirection.Down;
+                itemUpLeft.Checked = facingDirection == FacingDirection.UpLeft;
+                itemDownLeft.Checked = facingDirection == FacingDirection.DownLeft;
+                itemUpRight.Checked = facingDirection == FacingDirection.UpRight;
+                itemDownRight.Checked = facingDirection == FacingDirection.DownRight;
+            };
+
+            itemLeft.Click += (sender, e) => SetFacingDirection(FacingDirection.Left);
+            itemRight.Click += (sender, e) => SetFacingDirection(FacingDirection.Right);
+            itemUp.Click += (sender, e) => SetFacingDirection(FacingDirection.Up);
+            itemDown.Click += (sender, e) => SetFacingDirection(FacingDirection.Down);
+            itemUpLeft.Click += (sender, e) => SetFacingDirection(FacingDirection.UpLeft);
+            itemDownLeft.Click += (sender, e) => SetFacingDirection(FacingDirection.DownLeft);
+            itemUpRight.Click += (sender, e) => SetFacingDirection(FacingDirection.UpRight);
+            itemDownRight.Click += (sender, e) => SetFacingDirection(FacingDirection.DownRight);
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            contextMenuStrip.Items.Add(itemLeft);
+            contextMenuStrip.Items.Add(itemRight);
+            contextMenuStrip.Items.Add(itemUp);
+            contextMenuStrip.Items.Add(itemDown);
+            contextMenuStrip.Items.Add(itemUpLeft);
+            contextMenuStrip.Items.Add(itemDownLeft);
+            contextMenuStrip.Items.Add(itemUpRight);
+            contextMenuStrip.Items.Add(itemDownRight);
+            groupbox.ContextMenuStrip = contextMenuStrip;
+
+            itemUp.Checked = true;
         }
+
     }
 }
