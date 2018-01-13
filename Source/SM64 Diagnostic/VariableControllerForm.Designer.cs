49c3f28ab43ea45c3e6f206c9d9a127669bafa14
scob <scob@seas.upenn.edu>
2018-01-12 22:53:54 -0500

    made textboxes editable

--- Source/SM64 Diagnostic/VariableControllerForm.Designer.cs
+++ Source/SM64 Diagnostic/VariableControllerForm.Designer.cs
@@ -30,87 +30,85 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VariableControllerForm));
             this._buttonSubtract = new System.Windows.Forms.Button();
-            this._textBoxAddSubtract = new BetterTextbox();
-            this._textBoxGetSet = new BetterTextbox();
+            this._textBoxAddSubtract = new SM64_Diagnostic.BetterTextbox();
+            this._textBoxGetSet = new SM64_Diagnostic.BetterTextbox();
             this._buttonAdd = new System.Windows.Forms.Button();
             this._buttonGet = new System.Windows.Forms.Button();
             this._buttonSet = new System.Windows.Forms.Button();
             this._labelVarName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // buttonSubtract
+            // _buttonSubtract
             // 
             this._buttonSubtract.Anchor = System.Windows.Forms.AnchorStyles.None;
             this._buttonSubtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._buttonSubtract.Location = new System.Drawing.Point(24, 59);
-            this._buttonSubtract.Name = "buttonSubtract";
+            this._buttonSubtract.Name = "_buttonSubtract";
             this._buttonSubtract.Size = new System.Drawing.Size(89, 33);
             this._buttonSubtract.TabIndex = 0;
             this._buttonSubtract.Text = "-";
             this._buttonSubtract.UseVisualStyleBackColor = true;
             // 
-            // textBoxAddSubtract
+            // _textBoxAddSubtract
             // 
             this._textBoxAddSubtract.BackColor = System.Drawing.Color.White;
             this._textBoxAddSubtract.Location = new System.Drawing.Point(119, 66);
-            this._textBoxAddSubtract.Name = "textBoxAddSubtract";
-            this._textBoxAddSubtract.ReadOnly = true;
+            this._textBoxAddSubtract.Name = "_textBoxAddSubtract";
             this._textBoxAddSubtract.Size = new System.Drawing.Size(99, 20);
             this._textBoxAddSubtract.TabIndex = 9;
             this._textBoxAddSubtract.Text = "100";
             this._textBoxAddSubtract.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBoxGetSet
+            // _textBoxGetSet
             // 
             this._textBoxGetSet.BackColor = System.Drawing.Color.White;
             this._textBoxGetSet.Location = new System.Drawing.Point(119, 120);
-            this._textBoxGetSet.Name = "textBoxGetSet";
-            this._textBoxGetSet.ReadOnly = true;
+            this._textBoxGetSet.Name = "_textBoxGetSet";
             this._textBoxGetSet.Size = new System.Drawing.Size(99, 20);
             this._textBoxGetSet.TabIndex = 9;
             this._textBoxGetSet.Text = "200";
             this._textBoxGetSet.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // buttonAdd
+            // _buttonAdd
             // 
             this._buttonAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
             this._buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._buttonAdd.Location = new System.Drawing.Point(224, 59);
-            this._buttonAdd.Name = "buttonAdd";
+            this._buttonAdd.Name = "_buttonAdd";
             this._buttonAdd.Size = new System.Drawing.Size(89, 33);
             this._buttonAdd.TabIndex = 0;
             this._buttonAdd.Text = "+";
             this._buttonAdd.UseVisualStyleBackColor = true;
             // 
-            // buttonGet
+            // _buttonGet
             // 
             this._buttonGet.Anchor = System.Windows.Forms.AnchorStyles.None;
             this._buttonGet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._buttonGet.Location = new System.Drawing.Point(24, 113);
-            this._buttonGet.Name = "buttonGet";
+            this._buttonGet.Name = "_buttonGet";
             this._buttonGet.Size = new System.Drawing.Size(89, 33);
             this._buttonGet.TabIndex = 0;
             this._buttonGet.Text = "Get";
             this._buttonGet.UseVisualStyleBackColor = true;
             // 
-            // buttonSet
+            // _buttonSet
             // 
             this._buttonSet.Anchor = System.Windows.Forms.AnchorStyles.None;
             this._buttonSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._buttonSet.Location = new System.Drawing.Point(224, 113);
-            this._buttonSet.Name = "buttonSet";
+            this._buttonSet.Name = "_buttonSet";
             this._buttonSet.Size = new System.Drawing.Size(89, 33);
             this._buttonSet.TabIndex = 0;
             this._buttonSet.Text = "Set";
             this._buttonSet.UseVisualStyleBackColor = true;
             // 
-            // labelVarName
+            // _labelVarName
             // 
             this._labelVarName.Anchor = System.Windows.Forms.AnchorStyles.None;
             this._labelVarName.AutoSize = true;
             this._labelVarName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this._labelVarName.Location = new System.Drawing.Point(87, 18);
-            this._labelVarName.Name = "labelVarName";
+            this._labelVarName.Name = "_labelVarName";
             this._labelVarName.Size = new System.Drawing.Size(166, 25);
             this._labelVarName.TabIndex = 10;
             this._labelVarName.Text = "Variable Name";
