e439cf54377b96d803e05bd796f49fc2698d85a3
scob <scob@seas.upenn.edu>
2018-01-12 21:05:29 -0500

    created BetterContextMenuStrip

---
+++ Source/SM64 Diagnostic/Controls/BetterContextMenuStrip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using SM64_Diagnostic.Managers;
+using SM64_Diagnostic.Utilities;
+using SM64_Diagnostic.Structs;
+using SM64_Diagnostic.Controls;
+using SM64_Diagnostic.Extensions;
+using System.Drawing.Drawing2D;
+
+namespace SM64_Diagnostic
+{
+    public class BetterContextMenuStrip : ContextMenuStrip
+    {
+        private int numBeginningItems;
+
+        public BetterContextMenuStrip()
+        {
+            numBeginningItems = 0;
+        }
+
+        public void AddToBeginningList(ToolStripMenuItem item)
+        {
+            base.Items.Insert(numBeginningItems, item);
+            numBeginningItems++;
+        }
+
+        public void AddToEndingList(ToolStripMenuItem item)
+        {
+            base.Items.Add(item);
+        }
+
+    }
+}
