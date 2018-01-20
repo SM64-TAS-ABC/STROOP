dc3d342e290e3efb53a67363d33cd00fab86b6d8
scob <scob@seas.upenn.edu>
2018-01-20 10:54:24 -0500

    made var hack panel control

---
+++ Source/SM64 Diagnostic/Controls/VarHackPanel.cs
@@ -0,0 +1,31 @@
+﻿using SM64_Diagnostic.Structs;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SM64_Diagnostic.Controls
+{
+    public class VarHackPanel : FlowLayoutPanel
+    {
+
+        public VarHackPanel()
+        {
+
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
+
+    }
+}
