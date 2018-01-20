71eb788a1f0ea102b26dbb572f17748e2fa11e61
scob <scob@seas.upenn.edu>
2018-01-20 11:22:37 -0500

    added some more controls to var hack container form

--- Source/SM64 Diagnostic/VarHackContainerForm.Designer.cs
+++ Source/SM64 Diagnostic/VarHackContainerForm.Designer.cs
@@ -35,28 +35,36 @@
             this.betterTextbox1 = new SM64_Diagnostic.BetterTextbox();
             this.betterTextbox2 = new SM64_Diagnostic.BetterTextbox();
             this.betterTextbox3 = new SM64_Diagnostic.BetterTextbox();
-            this.betterTextbox4 = new SM64_Diagnostic.BetterTextbox();
             this.betterTextbox5 = new SM64_Diagnostic.BetterTextbox();
             this.betterTextbox6 = new SM64_Diagnostic.BetterTextbox();
+            this.betterTextbox7 = new SM64_Diagnostic.BetterTextbox();
+            this.betterTextbox8 = new SM64_Diagnostic.BetterTextbox();
+            this.betterTextbox9 = new SM64_Diagnostic.BetterTextbox();
+            this.betterTextbox10 = new SM64_Diagnostic.BetterTextbox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 5;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.52316F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 46.32153F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 305F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.04242F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 101F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 173F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 149F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
             this.tableLayoutPanel1.Controls.Add(this.checkBox1, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this._textBoxGetSet, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.betterTextbox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.betterTextbox2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.betterTextbox3, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.betterTextbox4, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.betterTextbox5, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.betterTextbox6, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.betterTextbox7, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.betterTextbox9, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.betterTextbox10, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.betterTextbox8, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox2, 3, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -65,7 +73,6 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(778, 277);
             this.tableLayoutPanel1.TabIndex = 39;
             // 
@@ -73,17 +80,18 @@
             // 
             this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(167, 165);
+            this.checkBox1.Location = new System.Drawing.Point(81, 164);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
             this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Use Pointer";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // _textBoxGetSet
             // 
             this._textBoxGetSet.Anchor = System.Windows.Forms.AnchorStyles.None;
             this._textBoxGetSet.BackColor = System.Drawing.Color.White;
-            this._textBoxGetSet.Location = new System.Drawing.Point(129, 24);
+            this._textBoxGetSet.Location = new System.Drawing.Point(76, 24);
             this._textBoxGetSet.Name = "_textBoxGetSet";
             this._textBoxGetSet.Size = new System.Drawing.Size(91, 20);
             this._textBoxGetSet.TabIndex = 10;
@@ -98,7 +106,7 @@
             this.betterTextbox1.Location = new System.Drawing.Point(3, 28);
             this.betterTextbox1.Name = "betterTextbox1";
             this.betterTextbox1.ReadOnly = true;
-            this.betterTextbox1.Size = new System.Drawing.Size(84, 13);
+            this.betterTextbox1.Size = new System.Drawing.Size(65, 13);
             this.betterTextbox1.TabIndex = 10;
             this.betterTextbox1.Text = "Name:";
             this.betterTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -111,7 +119,7 @@
             this.betterTextbox2.Location = new System.Drawing.Point(3, 97);
             this.betterTextbox2.Name = "betterTextbox2";
             this.betterTextbox2.ReadOnly = true;
-            this.betterTextbox2.Size = new System.Drawing.Size(84, 13);
+            this.betterTextbox2.Size = new System.Drawing.Size(65, 13);
             this.betterTextbox2.TabIndex = 10;
             this.betterTextbox2.Text = "Address:";
             this.betterTextbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -120,25 +128,12 @@
             // 
             this.betterTextbox3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.betterTextbox3.BackColor = System.Drawing.Color.White;
-            this.betterTextbox3.Location = new System.Drawing.Point(129, 93);
+            this.betterTextbox3.Location = new System.Drawing.Point(76, 93);
             this.betterTextbox3.Name = "betterTextbox3";
             this.betterTextbox3.Size = new System.Drawing.Size(91, 20);
             this.betterTextbox3.TabIndex = 10;
             this.betterTextbox3.Text = "0x8033B1AC";
             this.betterTextbox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // betterTextbox4
-            // 
-            this.betterTextbox4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.betterTextbox4.BackColor = System.Drawing.SystemColors.Control;
-            this.betterTextbox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.betterTextbox4.Location = new System.Drawing.Point(3, 166);
-            this.betterTextbox4.Name = "betterTextbox4";
-            this.betterTextbox4.ReadOnly = true;
-            this.betterTextbox4.Size = new System.Drawing.Size(84, 13);
-            this.betterTextbox4.TabIndex = 10;
-            this.betterTextbox4.Text = "Use Pointer:";
-            this.betterTextbox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // betterTextbox5
             // 
@@ -148,7 +143,7 @@
             this.betterTextbox5.Location = new System.Drawing.Point(3, 235);
             this.betterTextbox5.Name = "betterTextbox5";
             this.betterTextbox5.ReadOnly = true;
-            this.betterTextbox5.Size = new System.Drawing.Size(84, 13);
+            this.betterTextbox5.Size = new System.Drawing.Size(65, 13);
             this.betterTextbox5.TabIndex = 10;
             this.betterTextbox5.Text = "Pointer Offset:";
             this.betterTextbox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -157,12 +152,71 @@
             // 
             this.betterTextbox6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.betterTextbox6.BackColor = System.Drawing.Color.White;
-            this.betterTextbox6.Location = new System.Drawing.Point(129, 232);
+            this.betterTextbox6.Location = new System.Drawing.Point(76, 232);
             this.betterTextbox6.Name = "betterTextbox6";
             this.betterTextbox6.Size = new System.Drawing.Size(91, 20);
             this.betterTextbox6.TabIndex = 10;
             this.betterTextbox6.Text = "0x10";
             this.betterTextbox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // betterTextbox7
+            // 
+            this.betterTextbox7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.betterTextbox7.BackColor = System.Drawing.SystemColors.Control;
+            this.betterTextbox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.betterTextbox7.Location = new System.Drawing.Point(226, 28);
+            this.betterTextbox7.Name = "betterTextbox7";
+            this.betterTextbox7.ReadOnly = true;
+            this.betterTextbox7.Size = new System.Drawing.Size(65, 13);
+            this.betterTextbox7.TabIndex = 10;
+            this.betterTextbox7.Text = "X Pos:";
+            this.betterTextbox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // betterTextbox8
+            // 
+            this.betterTextbox8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.betterTextbox8.BackColor = System.Drawing.SystemColors.Control;
+            this.betterTextbox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.betterTextbox8.Location = new System.Drawing.Point(226, 97);
+            this.betterTextbox8.Name = "betterTextbox8";
+            this.betterTextbox8.ReadOnly = true;
+            this.betterTextbox8.Size = new System.Drawing.Size(65, 13);
+            this.betterTextbox8.TabIndex = 10;
+            this.betterTextbox8.Text = "Y Pos:";
+            this.betterTextbox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // betterTextbox9
+            // 
+            this.betterTextbox9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.betterTextbox9.BackColor = System.Drawing.Color.White;
+            this.betterTextbox9.Location = new System.Drawing.Point(374, 24);
+            this.betterTextbox9.Name = "betterTextbox9";
+            this.betterTextbox9.Size = new System.Drawing.Size(91, 20);
+            this.betterTextbox9.TabIndex = 10;
+            this.betterTextbox9.Text = "100";
+            this.betterTextbox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // betterTextbox10
+            // 
+            this.betterTextbox10.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.betterTextbox10.BackColor = System.Drawing.Color.White;
+            this.betterTextbox10.Location = new System.Drawing.Point(374, 93);
+            this.betterTextbox10.Name = "betterTextbox10";
+            this.betterTextbox10.Size = new System.Drawing.Size(91, 20);
+            this.betterTextbox10.TabIndex = 10;
+            this.betterTextbox10.Text = "200";
+            this.betterTextbox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(386, 164);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(67, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Use Hex";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // VarHackContainerForm
             // 
@@ -190,8 +244,12 @@
         private BetterTextbox betterTextbox1;
         private BetterTextbox betterTextbox2;
         private BetterTextbox betterTextbox3;
-        private BetterTextbox betterTextbox4;
         private BetterTextbox betterTextbox5;
         private BetterTextbox betterTextbox6;
+        private BetterTextbox betterTextbox7;
+        private BetterTextbox betterTextbox9;
+        private BetterTextbox betterTextbox10;
+        private BetterTextbox betterTextbox8;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
