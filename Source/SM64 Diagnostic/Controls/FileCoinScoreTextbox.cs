e3195c2c109f4a91b4efbb52e2040989488cea2b
scob <scob@seas.upenn.edu>
2018-01-27 09:54:11 -0500

    remove some more uses of file manager instance

--- Source/SM64 Diagnostic/Controls/FileCoinScoreTextbox.cs
+++ Source/SM64 Diagnostic/Controls/FileCoinScoreTextbox.cs
@@ -26,14 +26,11 @@ namespace SM64_Diagnostic
         public override void Initialize(uint addressOffset)
         {
             base.Initialize(addressOffset);
-
-            _currentValue = GetCoinScoreFromMemory();
-            this.Text = _currentValue.ToString();
         }
 
         private byte GetCoinScoreFromMemory()
         {
-            return Config.Stream.GetByte(FileManager.Instance.CurrentFileAddress + _addressOffset);
+            return Config.Stream.GetByte(Config.FileManager.CurrentFileAddress + _addressOffset);
         }
 
         protected override void SubmitValue()
@@ -45,7 +42,7 @@ namespace SM64_Diagnostic
                 return;
             }
 
-            Config.Stream.SetValue(value, FileManager.Instance.CurrentFileAddress + _addressOffset);
+            Config.Stream.SetValue(value, Config.FileManager.CurrentFileAddress + _addressOffset);
         }
 
         protected override void ResetValue()
