e96869b9893ec9eddd3590922efb1c8e26a91627
scob <scob@seas.upenn.edu>
2018-01-28 15:52:59 -0500

    added more controls to custom tab

--- Source/SM64 Diagnostic/Forms/StroopMainForm.Designer.cs
+++ Source/SM64 Diagnostic/Forms/StroopMainForm.Designer.cs
@@ -1031,6 +1031,11 @@ namespace SM64_Diagnostic
             this.buttonShowLeftRightPane = new System.Windows.Forms.Button();
             this.buttonShowLeftPane = new System.Windows.Forms.Button();
             this.splitContainerCustomControls = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.betterTextbox1 = new SM64_Diagnostic.BetterTextbox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.groupBoxObjects.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarObjSlotSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
@@ -13440,6 +13445,11 @@ namespace SM64_Diagnostic
             // 
             // splitContainerCustomControls.Panel1
             // 
+            this.splitContainerCustomControls.Panel1.Controls.Add(this.checkBox2);
+            this.splitContainerCustomControls.Panel1.Controls.Add(this.betterTextbox1);
+            this.splitContainerCustomControls.Panel1.Controls.Add(this.button1);
+            this.splitContainerCustomControls.Panel1.Controls.Add(this.button2);
+            this.splitContainerCustomControls.Panel1.Controls.Add(this.checkBox1);
             this.splitContainerCustomControls.Panel1.Controls.Add(this.buttonClearVariables);
             // 
             // splitContainerCustomControls.Panel2
@@ -13453,6 +13463,58 @@ namespace SM64_Diagnostic
             this.splitContainerCustomControls.SplitterDistance = 172;
             this.splitContainerCustomControls.SplitterWidth = 1;
             this.splitContainerCustomControls.TabIndex = 31;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 25);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Clear Values";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 25);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Show Values";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 47);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 17);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Record Values";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // betterTextbox1
+            // 
+            this.betterTextbox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.betterTextbox1.BackColor = System.Drawing.SystemColors.Control;
+            this.betterTextbox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.betterTextbox1.Location = new System.Drawing.Point(129, 46);
+            this.betterTextbox1.Name = "betterTextbox1";
+            this.betterTextbox1.Size = new System.Drawing.Size(51, 20);
+            this.betterTextbox1.TabIndex = 34;
+            this.betterTextbox1.Text = "Record";
+            this.betterTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(11, 103);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(186, 17);
+            this.checkBox2.TabIndex = 35;
+            this.checkBox2.Text = "Use Value at Start of Global Timer";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // StroopMainForm
             // 
@@ -13872,6 +13934,7 @@ namespace SM64_Diagnostic
             this.panelConnect.ResumeLayout(false);
             this.panelConnect.PerformLayout();
             this.splitContainerCustomControls.Panel1.ResumeLayout(false);
+            this.splitContainerCustomControls.Panel1.PerformLayout();
             this.splitContainerCustomControls.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainerCustomControls)).EndInit();
             this.splitContainerCustomControls.ResumeLayout(false);
@@ -14879,6 +14942,11 @@ namespace SM64_Diagnostic
         private BetterTextbox textBoxRNGIndexTester;
         private Button buttonRNGIndexTester;
         private SplitContainer splitContainerCustomControls;
+        private CheckBox checkBox2;
+        private BetterTextbox betterTextbox1;
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
     }
 }
 
