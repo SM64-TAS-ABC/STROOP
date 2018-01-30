60de9237a95ac69dd434b8fb5a1bf9578c0b032d
scob <scob@seas.upenn.edu>
2018-01-29 20:55:17 -0500

    added LimitRefreshRate to refresh rate config

--- Source/SM64 Diagnostic/Structs/Configurations/RefreshRateConfig.cs
+++ Source/SM64 Diagnostic/Structs/Configurations/RefreshRateConfig.cs
@@ -10,12 +10,15 @@ namespace SM64_Diagnostic.Structs.Configurations
 {
     public static class RefreshRateConfig
     {
+        public static bool LimitRefreshRate = true;
+
         public static uint RefreshRateFreq;
+
         public static double RefreshRateInterval
         {
             get
             {
-                uint freq = RefreshRateFreq;
+                uint freq = LimitRefreshRate ? RefreshRateFreq : 0;
                 if (freq == 0) return 0;
                 else return 1000.0 / freq;
             }
