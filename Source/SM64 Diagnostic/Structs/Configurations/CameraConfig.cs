ae6434ed5ebfe8957b35993208022935dba2a135
scob <scob@seas.upenn.edu>
2018-02-04 21:27:34 -0500

    added fov function address/value to config

--- Source/SM64 Diagnostic/Structs/Configurations/CameraConfig.cs
+++ Source/SM64 Diagnostic/Structs/Configurations/CameraConfig.cs
@@ -25,5 +25,37 @@ namespace SM64_Diagnostic.Structs.Configurations
         public static uint SecondaryObjectAddress { get { return Config.SwitchRomVersion(SecondaryObjectAddressUS, SecondaryObjectAddressJP); } }
         public static readonly uint SecondaryObjectAddressUS = 0x8032DF30;
         public static readonly uint SecondaryObjectAddressJP = 0x8032CFD0;
+
+        public static uint FovFunctionStartSleepingAddress { get { return Config.SwitchRomVersion(FovFunctionStartSleepingAddressUS, FovFunctionStartSleepingAddressJP); } }
+        public static readonly uint FovFunctionStartSleepingAddressUS = 0x8029A774;
+        public static readonly uint FovFunctionStartSleepingAddressJP = 0x8029A058;
+
+        public static uint FovFunctionStopSleepingAddress { get { return Config.SwitchRomVersion(FovFunctionStopSleepingAddressUS, FovFunctionStopSleepingAddressJP); } }
+        public static readonly uint FovFunctionStopSleepingAddressUS = 0x8029A7C8;
+        public static readonly uint FovFunctionStopSleepingAddressJP = 0x8029A0AC;
+
+        public static uint FovFunctionUseDoorAddress { get { return Config.SwitchRomVersion(FovFunctionUseDoorAddressUS, FovFunctionUseDoorAddressJP); } }
+        public static readonly uint FovFunctionUseDoorAddressUS = 0x8029AA20;
+        public static readonly uint FovFunctionUseDoorAddressJP = 0x8029A304;
+
+        public static uint FovFunctionCollectStarAddress { get { return Config.SwitchRomVersion(FovFunctionCollectStarAddressUS, FovFunctionCollectStarAddressJP); } }
+        public static readonly uint FovFunctionCollectStarAddressUS = 0x80289A70;
+        public static readonly uint FovFunctionCollectStarAddressJP = 0x80289488;
+
+        public static uint FovFunctionStartSleepingValue { get { return Config.SwitchRomVersion(FovFunctionStartSleepingValueUS, FovFunctionStartSleepingValueJP); } }
+        public static readonly uint FovFunctionStartSleepingValueUS = 0x0C0A2673;
+        public static readonly uint FovFunctionStartSleepingValueJP = 0x0C0A24F9;
+
+        public static uint FovFunctionStopSleepingValue { get { return Config.SwitchRomVersion(FovFunctionStopSleepingValueUS, FovFunctionStopSleepingValueJP); } }
+        public static readonly uint FovFunctionStopSleepingValueUS = 0x0C0A2673;
+        public static readonly uint FovFunctionStopSleepingValueJP = 0x0C0A24F9;
+
+        public static uint FovFunctionUseDoorValue { get { return Config.SwitchRomVersion(FovFunctionUseDoorValueUS, FovFunctionUseDoorValueJP); } }
+        public static readonly uint FovFunctionUseDoorValueUS = 0xE420C5A4;
+        public static readonly uint FovFunctionUseDoorValueJP = 0xE420B234;
+
+        public static uint FovFunctionCollectStarValue { get { return Config.SwitchRomVersion(FovFunctionCollectStarValueUS, FovFunctionCollectStarValueJP); } }
+        public static readonly uint FovFunctionCollectStarValueUS = 0xE4920000;
+        public static readonly uint FovFunctionCollectStarValueJP = 0xE4920000;
     }
 }
