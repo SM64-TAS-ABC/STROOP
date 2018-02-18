cb870a438ecf383a350903f090bf86176ecb9efc
scob <scob@seas.upenn.edu>
2018-02-18 15:01:04 -0500

    create and hook up helpful hint utilities

---
+++ STROOP/Utilities/HelpfulHintUtilities.cs
@@ -0,0 +1,20 @@
+﻿using STROOP.Managers;
+using STROOP.Structs.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STROOP.Structs
+{
+    public static class HelpfulHintUtilities
+    {
+        public static string GetRandomHelpfulHint()
+        {
+            return System.DateTime.Now.ToString();
+        }
+
+    }
+}
