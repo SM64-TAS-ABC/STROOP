72f1d8e2b634ebee2f2628815dd509bade687d96
scob <scob@seas.upenn.edu>
2018-02-21 20:45:24 -0500

    added var hack special utilities

---
+++ STROOP/Utilities/VarHackSpecialUtilities.cs
@@ -0,0 +1,39 @@
+﻿using STROOP.Managers;
+using STROOP.Structs.Configurations;
+using STROOP.Utilities;
+using System;
+using System.Collections.Generic;
+
+namespace STROOP.Structs
+{
+    public static class VarHackSpecialUtilities
+    {
+        private readonly static Func<uint, string> DEFAULT_GETTER = (uint address) => "NOT IMPL";
+        private readonly static Func<string, uint, bool> DEFAULT_SETTER = (string value, uint address) => false;
+
+        public static (Func<uint, string> getter, Func<string, uint, bool> setter) CreateGetterSetterFunctions(string specialType)
+        {
+            Func<uint, string> getterFunction = DEFAULT_GETTER;
+            Func<string, uint, bool> setterFunction = DEFAULT_SETTER;
+
+            switch (specialType)
+            {
+                case "MarioDistanceToObject":
+                    getterFunction = (uint objAddress) =>
+                    {
+                        return "";
+                    };
+                    setterFunction = (string stringValue, uint objAddress) =>
+                    {
+                        return false;
+                    };
+                    break;
+
+                default:
+                    break;
+            }
+
+            return (getterFunction, setterFunction);
+        }
+    }
+}
