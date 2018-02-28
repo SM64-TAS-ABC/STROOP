0352bb201f26804b587c02a6cffecba496f38bd7
scob <scob@seas.upenn.edu>
2018-02-27 21:20:19 -0500

    simplfiy pu controller logic

--- STROOP/Managers/PuManager.cs
+++ STROOP/Managers/PuManager.cs
@@ -14,8 +14,6 @@ namespace STROOP.Managers
     {
         GroupBox _puController;
 
-        enum PuControl { Home, PuUp, PuDown, PuLeft, PuRight, QpuUp, QpuDown, QpuLeft, QpuRight };
-
         public PuManager(List<WatchVariableControlPrecursor> variables, TabPage tabControl, WatchVariableFlowLayoutPanel watchVariablePanel)
             : base(variables, watchVariablePanel)
         {
@@ -24,49 +22,15 @@ namespace STROOP.Managers
             _puController = splitContainerFile.Panel1.Controls["groupBoxPuController"] as GroupBox;
 
             // Pu Controller initialize and register click events
-            _puController.Controls["buttonPuConHome"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.Home);
-            _puController.Controls["buttonPuConZnQpu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.QpuUp);
-            _puController.Controls["buttonPuConZpQpu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.QpuDown);
-            _puController.Controls["buttonPuConXnQpu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.QpuLeft);
-            _puController.Controls["buttonPuConXpQpu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.QpuRight);
-            _puController.Controls["buttonPuConZnPu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.PuUp);
-            _puController.Controls["buttonPuConZpPu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.PuDown);
-            _puController.Controls["buttonPuConXnPu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.PuLeft);
-            _puController.Controls["buttonPuConXpPu"].Click += (sender, e) => PuControl_Click(sender, e, PuControl.PuRight);
-        }
-
-        private void PuControl_Click(object sender, EventArgs e, PuControl controlType)
-        {
-            switch (controlType)
-            {
-                case PuControl.Home:
-                    PuUtilities.MoveToPu(0, 0, 0);
-                    break;
-                case PuControl.PuUp:
-                    PuUtilities.MoveToRelativePu(0, 0, -1);
-                    break;
-                case PuControl.PuDown:
-                    PuUtilities.MoveToRelativePu(0, 0, 1);
-                    break;
-                case PuControl.PuLeft:
-                    PuUtilities.MoveToRelativePu(-1, 0, 0);
-                    break;
-                case PuControl.PuRight:
-                    PuUtilities.MoveToRelativePu(1, 0, 0);
-                    break;
-                case PuControl.QpuUp:
-                    PuUtilities.MoveToRelativePu(0, 0, -4);
-                    break;
-                case PuControl.QpuDown:
-                    PuUtilities.MoveToRelativePu(0, 0, 4);
-                    break;
-                case PuControl.QpuLeft:
-                    PuUtilities.MoveToRelativePu(-4, 0, 0);
-                    break;
-                case PuControl.QpuRight:
-                    PuUtilities.MoveToRelativePu(4, 0, 0);
-                    break;
-            }
+            _puController.Controls["buttonPuConHome"].Click += (sender, e) => PuUtilities.MoveToPu(0, 0, 0);
+            _puController.Controls["buttonPuConZnQpu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(0, 0, -1);
+            _puController.Controls["buttonPuConZpQpu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(0, 0, 1);
+            _puController.Controls["buttonPuConXnQpu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(-1, 0, 0);
+            _puController.Controls["buttonPuConXpQpu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(1, 0, 0);
+            _puController.Controls["buttonPuConZnPu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(0, 0, -4);
+            _puController.Controls["buttonPuConZpPu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(0, 0, 4);
+            _puController.Controls["buttonPuConXnPu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(-4, 0, 0);
+            _puController.Controls["buttonPuConXpPu"].Click += (sender, e) => PuUtilities.MoveToRelativePu(4, 0, 0);
         }
 
         public override void Update(bool updateView)
