820702a2cf74707a8b05267424372dbb5e77790f
scob <scob@seas.upenn.edu>
2018-03-03 21:17:53 -0500

    correctly implement groupBoxPuController

--- STROOP/Managers/PuManager.cs
+++ STROOP/Managers/PuManager.cs
@@ -48,6 +48,37 @@ namespace STROOP.Managers
             _puController.Controls["buttonPuConZpPu"].Click += (sender, e) => PuUtilities.TranslateMarioPu(0, 0, 1);
             _puController.Controls["buttonPuConXnPu"].Click += (sender, e) => PuUtilities.TranslateMarioPu(-1, 0, 0);
             _puController.Controls["buttonPuConXpPu"].Click += (sender, e) => PuUtilities.TranslateMarioPu(1, 0, 0);
+
+            GroupBox groupBoxMarioPu = splitContainerFile.Panel1.Controls["groupBoxMarioPu"] as GroupBox;
+
+            ControlUtilities.InitializeThreeDimensionController(
+                CoordinateSystem.Euler,
+                false,
+                splitContainerFile.Panel1.Controls["groupBoxPuController"] as GroupBox,
+                groupBoxMarioPu.Controls["buttonMarioPuXn"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuXp"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuZn"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuZp"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuXnZn"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuXnZp"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuXpZn"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuXpZp"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuYp"] as Button,
+                groupBoxMarioPu.Controls["buttonMarioPuYn"] as Button,
+                groupBoxMarioPu.Controls["textBoxMarioPuXZ"] as TextBox,
+                groupBoxMarioPu.Controls["textBoxMarioPuY"] as TextBox,
+                groupBoxMarioPu.Controls["checkBoxMarioPuQpu"] as CheckBox,
+                (float hOffset, float vOffset, float nOffset, bool useQpu) =>
+                {
+                    int hOffsetInt = ParsingUtilities.ParseInt(hOffset);
+                    int vOffsetInt = ParsingUtilities.ParseInt(vOffset);
+                    int nOffsetInt = ParsingUtilities.ParseInt(nOffset);
+                    int multiplier = useQpu ? 4 : 1;
+                    PuUtilities.TranslateMarioPu(
+                        hOffsetInt * multiplier,
+                        nOffsetInt * multiplier,
+                        -1 * vOffsetInt * multiplier);
+                });
         }
 
         public override void Update(bool updateView)
