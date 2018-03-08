5fb7bba283513dc71c63c1279eb76ac9664c6bdc
scob <scob@seas.upenn.edu>
2018-03-07 20:12:52 -0500

    make WatchVariableControlSettings class

---
+++ STROOP/Controls/WatchVariableControlSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using STROOP.Utilities;
+using STROOP.Structs;
+using STROOP.Extensions;
+using System.Reflection;
+using STROOP.Managers;
+using STROOP.Structs.Configurations;
+
+namespace STROOP.Controls
+{
+    public class WatchVariableControlSettings
+    {
+
+        public WatchVariableControlSettings()
+        {
+
+        }
+
+    }
+}
