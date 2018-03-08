f66e4af2b6ea1f9b1172b236c0346238236ec12d
scob <scob@seas.upenn.edu>
2018-03-07 20:50:28 -0500

    add description to watch var control settings

--- STROOP/Controls/WatchVariableControlSettings.cs
+++ STROOP/Controls/WatchVariableControlSettings.cs
@@ -14,6 +14,17 @@ using STROOP.Structs.Configurations;
 
 namespace STROOP.Controls
 {
+    /**
+     * Class for applying settings to a watch var control and wrapper.
+     * For each setting, there are 3 variables:
+     * (1) ChangeSetting: a boolean whether this setting should be changed
+     * (2) ChangeSettingToDefault: a boolean whether the change should be to the default value
+     * (3) NewSetting: the new value if we're not using the default value
+     * 
+     * When constructing this class, for each setting, either leave all 3 variables out, or:
+     * (1) Set changeSetting to true and changeSettingToDefault to true
+     * (2) Set changeSetting to true and newSetting to the new value
+     */
     public class WatchVariableControlSettings
     {
         public readonly bool ChangeAngleSigned;
