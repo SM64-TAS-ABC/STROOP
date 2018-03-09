8c3d8bedcb5e8e12de4b237552f2f690b6609b63
scob <scob@seas.upenn.edu>
2018-03-09 16:18:14 -0500

    create better split container

---
+++ STROOP/Controls/BetterSplitContainer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using STROOP.Managers;
+using STROOP.Utilities;
+using STROOP.Structs;
+using STROOP.Controls;
+using STROOP.Extensions;
+using System.Drawing.Drawing2D;
+
+namespace STROOP
+{
+    public class BetterSplitContainer : SplitContainer
+    {
+        public BetterSplitContainer()
+        {
+            ToolStripMenuItem itemJustPanel1 = new ToolStripMenuItem("Just Panel 1");
+            itemJustPanel1.Click += (sender, e) =>
+            {
+                Panel1Collapsed = false;
+                Panel2Collapsed = true;
+            };
+
+            ToolStripMenuItem itemJustPanel2 = new ToolStripMenuItem("Just Panel 2");
+            itemJustPanel2.Click += (sender, e) =>
+            {
+                Panel1Collapsed = true;
+                Panel2Collapsed = false;
+            };
+
+            ToolStripMenuItem itemBothPanels = new ToolStripMenuItem("Both Panels");
+            itemBothPanels.Click += (sender, e) =>
+            {
+                Panel1Collapsed = true;
+                Panel2Collapsed = false;
+            };
+
+            ContextMenuStrip = new ContextMenuStrip();
+            ContextMenuStrip.Items.Add(itemJustPanel1);
+            ContextMenuStrip.Items.Add(itemJustPanel2);
+            ContextMenuStrip.Items.Add(itemBothPanels);
+        }
+    }
+}
