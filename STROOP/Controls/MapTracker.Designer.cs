003e623f6bc16213891e07ff43d264ab3f7b2f2b
scob <scob@seas.upenn.edu>
2018-03-09 21:13:35 -0500

    do more rearranging on map tracker

--- STROOP/Controls/MapTracker.Designer.cs
+++ STROOP/Controls/MapTracker.Designer.cs
@@ -36,19 +36,14 @@
             this.textBoxAddressValue = new STROOP.BetterTextbox();
             this.textBoxPointerOffsetLabel = new STROOP.BetterTextbox();
             this.textBoxPointerOffsetValue = new STROOP.BetterTextbox();
-            this.radioButtonSByte = new System.Windows.Forms.RadioButton();
-            this.radioButtonByte = new System.Windows.Forms.RadioButton();
-            this.radioButtonShort = new System.Windows.Forms.RadioButton();
-            this.radioButtonUShort = new System.Windows.Forms.RadioButton();
-            this.radioButtonInt = new System.Windows.Forms.RadioButton();
-            this.radioButtonUInt = new System.Windows.Forms.RadioButton();
-            this.radioButtonFloat = new System.Windows.Forms.RadioButton();
-            this.checkBoxUseHex = new System.Windows.Forms.CheckBox();
             this.checkBoxNoNumber = new System.Windows.Forms.CheckBox();
             this.pictureBoxDownArrow = new System.Windows.Forms.PictureBox();
             this.pictureBoxUpArrow = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBoxLabelMethod = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.tableLayoutPanelVarHack.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDownArrow)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUpArrow)).BeginInit();
@@ -65,10 +60,10 @@
             this.tableLayoutPanelVarHack.ColumnCount = 6;
             this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 78F));
             this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 107F));
-            this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 56F));
-            this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 73F));
+            this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 91F));
             this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
-            this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.tableLayoutPanelVarHack.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 61F));
             this.tableLayoutPanelVarHack.Controls.Add(this.checkBoxUsePointer, 1, 2);
             this.tableLayoutPanelVarHack.Controls.Add(this.textBoxNameValue, 1, 0);
             this.tableLayoutPanelVarHack.Controls.Add(this.textBoxNameLabel, 0, 0);
@@ -76,19 +71,14 @@
             this.tableLayoutPanelVarHack.Controls.Add(this.textBoxAddressValue, 1, 1);
             this.tableLayoutPanelVarHack.Controls.Add(this.textBoxPointerOffsetLabel, 0, 3);
             this.tableLayoutPanelVarHack.Controls.Add(this.textBoxPointerOffsetValue, 1, 3);
-            this.tableLayoutPanelVarHack.Controls.Add(this.radioButtonSByte, 2, 0);
-            this.tableLayoutPanelVarHack.Controls.Add(this.radioButtonByte, 3, 0);
-            this.tableLayoutPanelVarHack.Controls.Add(this.radioButtonShort, 2, 1);
-            this.tableLayoutPanelVarHack.Controls.Add(this.radioButtonUShort, 3, 1);
-            this.tableLayoutPanelVarHack.Controls.Add(this.radioButtonInt, 2, 2);
-            this.tableLayoutPanelVarHack.Controls.Add(this.radioButtonUInt, 3, 2);
-            this.tableLayoutPanelVarHack.Controls.Add(this.radioButtonFloat, 2, 3);
-            this.tableLayoutPanelVarHack.Controls.Add(this.checkBoxUseHex, 3, 3);
             this.tableLayoutPanelVarHack.Controls.Add(this.checkBoxNoNumber, 0, 2);
             this.tableLayoutPanelVarHack.Controls.Add(this.pictureBoxDownArrow, 5, 3);
             this.tableLayoutPanelVarHack.Controls.Add(this.pictureBoxUpArrow, 5, 2);
             this.tableLayoutPanelVarHack.Controls.Add(this.pictureBox2, 5, 0);
             this.tableLayoutPanelVarHack.Controls.Add(this.pictureBox1, 5, 1);
+            this.tableLayoutPanelVarHack.Controls.Add(this.comboBoxLabelMethod, 3, 1);
+            this.tableLayoutPanelVarHack.Controls.Add(this.comboBox1, 3, 2);
+            this.tableLayoutPanelVarHack.Controls.Add(this.checkBox1, 3, 0);
             this.tableLayoutPanelVarHack.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelVarHack.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanelVarHack.Name = "tableLayoutPanelVarHack";
@@ -185,96 +175,6 @@
             this.textBoxPointerOffsetValue.Text = "0x10";
             this.textBoxPointerOffsetValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // radioButtonSByte
-            // 
-            this.radioButtonSByte.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.radioButtonSByte.AutoSize = true;
-            this.radioButtonSByte.Location = new System.Drawing.Point(191, 4);
-            this.radioButtonSByte.Name = "radioButtonSByte";
-            this.radioButtonSByte.Size = new System.Drawing.Size(50, 17);
-            this.radioButtonSByte.TabIndex = 11;
-            this.radioButtonSByte.Text = "sbyte";
-            this.radioButtonSByte.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonByte
-            // 
-            this.radioButtonByte.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.radioButtonByte.AutoSize = true;
-            this.radioButtonByte.Location = new System.Drawing.Point(248, 4);
-            this.radioButtonByte.Name = "radioButtonByte";
-            this.radioButtonByte.Size = new System.Drawing.Size(45, 17);
-            this.radioButtonByte.TabIndex = 11;
-            this.radioButtonByte.Text = "byte";
-            this.radioButtonByte.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonShort
-            // 
-            this.radioButtonShort.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.radioButtonShort.AutoSize = true;
-            this.radioButtonShort.Location = new System.Drawing.Point(191, 28);
-            this.radioButtonShort.Name = "radioButtonShort";
-            this.radioButtonShort.Size = new System.Drawing.Size(48, 17);
-            this.radioButtonShort.TabIndex = 11;
-            this.radioButtonShort.Text = "short";
-            this.radioButtonShort.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonUShort
-            // 
-            this.radioButtonUShort.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.radioButtonUShort.AutoSize = true;
-            this.radioButtonUShort.Location = new System.Drawing.Point(248, 28);
-            this.radioButtonUShort.Name = "radioButtonUShort";
-            this.radioButtonUShort.Size = new System.Drawing.Size(54, 17);
-            this.radioButtonUShort.TabIndex = 11;
-            this.radioButtonUShort.Text = "ushort";
-            this.radioButtonUShort.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonInt
-            // 
-            this.radioButtonInt.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.radioButtonInt.AutoSize = true;
-            this.radioButtonInt.Location = new System.Drawing.Point(191, 52);
-            this.radioButtonInt.Name = "radioButtonInt";
-            this.radioButtonInt.Size = new System.Drawing.Size(36, 17);
-            this.radioButtonInt.TabIndex = 11;
-            this.radioButtonInt.Text = "int";
-            this.radioButtonInt.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonUInt
-            // 
-            this.radioButtonUInt.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.radioButtonUInt.AutoSize = true;
-            this.radioButtonUInt.Location = new System.Drawing.Point(248, 52);
-            this.radioButtonUInt.Name = "radioButtonUInt";
-            this.radioButtonUInt.Size = new System.Drawing.Size(42, 17);
-            this.radioButtonUInt.TabIndex = 11;
-            this.radioButtonUInt.Text = "uint";
-            this.radioButtonUInt.UseVisualStyleBackColor = true;
-            // 
-            // radioButtonFloat
-            // 
-            this.radioButtonFloat.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.radioButtonFloat.AutoSize = true;
-            this.radioButtonFloat.Checked = true;
-            this.radioButtonFloat.Location = new System.Drawing.Point(191, 77);
-            this.radioButtonFloat.Name = "radioButtonFloat";
-            this.radioButtonFloat.Size = new System.Drawing.Size(45, 17);
-            this.radioButtonFloat.TabIndex = 11;
-            this.radioButtonFloat.TabStop = true;
-            this.radioButtonFloat.Text = "float";
-            this.radioButtonFloat.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxUseHex
-            // 
-            this.checkBoxUseHex.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.checkBoxUseHex.AutoSize = true;
-            this.checkBoxUseHex.Location = new System.Drawing.Point(248, 77);
-            this.checkBoxUseHex.Name = "checkBoxUseHex";
-            this.checkBoxUseHex.Size = new System.Drawing.Size(67, 17);
-            this.checkBoxUseHex.TabIndex = 4;
-            this.checkBoxUseHex.Text = "Use Hex";
-            this.checkBoxUseHex.UseVisualStyleBackColor = true;
-            // 
             // checkBoxNoNumber
             // 
             this.checkBoxNoNumber.Anchor = System.Windows.Forms.AnchorStyles.Left;
@@ -331,6 +231,37 @@
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
+            // comboBoxLabelMethod
+            // 
+            this.comboBoxLabelMethod.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxLabelMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLabelMethod.Location = new System.Drawing.Point(229, 27);
+            this.comboBoxLabelMethod.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxLabelMethod.Name = "comboBoxLabelMethod";
+            this.comboBoxLabelMethod.Size = new System.Drawing.Size(87, 21);
+            this.comboBoxLabelMethod.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Location = new System.Drawing.Point(229, 51);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(87, 21);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox1.Location = new System.Drawing.Point(230, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Rotate";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // MapTracker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -360,18 +291,13 @@
         private BetterTextbox textBoxAddressValue;
         private BetterTextbox textBoxPointerOffsetLabel;
         private BetterTextbox textBoxPointerOffsetValue;
-        private System.Windows.Forms.RadioButton radioButtonSByte;
-        private System.Windows.Forms.RadioButton radioButtonByte;
-        private System.Windows.Forms.RadioButton radioButtonShort;
-        private System.Windows.Forms.RadioButton radioButtonUShort;
-        private System.Windows.Forms.RadioButton radioButtonInt;
-        private System.Windows.Forms.RadioButton radioButtonUInt;
-        private System.Windows.Forms.RadioButton radioButtonFloat;
-        private System.Windows.Forms.CheckBox checkBoxUseHex;
         private System.Windows.Forms.PictureBox pictureBoxUpArrow;
         private System.Windows.Forms.PictureBox pictureBoxDownArrow;
         private System.Windows.Forms.CheckBox checkBoxNoNumber;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxLabelMethod;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
