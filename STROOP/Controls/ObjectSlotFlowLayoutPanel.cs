d023a2720faa7a22d7ab8d7f563d3543d09f2576
scob <scob@seas.upenn.edu>
2018-03-19 19:56:59 -0400

    added ObjectSlotFlowLayoutPanel

---
+++ STROOP/Controls/ObjectSlotFlowLayoutPanel.cs
@@ -0,0 +1,21 @@
+﻿using STROOP.Forms;
+using STROOP.Structs;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace STROOP.Controls
+{
+    public class ObjectSlotFlowLayoutPanel : NoTearFlowLayoutPanel
+    {
+        public ObjectSlotFlowLayoutPanel()
+        {
+
+        }
+    }
+}
