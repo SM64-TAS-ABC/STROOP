4c63884713a964687163685fae0c5c0eefe90c8e
scob <scob@seas.upenn.edu>
2018-03-20 18:08:13 -0400

    create ColorSelector

---
+++ STROOP/Controls/Map/Trackers/ColorSelector.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace STROOP.Controls.Map.Trackers
+{
+    partial class ColorSelector
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ColorSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ColorSelector";
+            this.Size = new System.Drawing.Size(373, 99);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ STROOP/Controls/Map/Trackers/ColorSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using STROOP.Structs;
+using STROOP.Utilities;
+using System.Xml.Linq;
+using STROOP.Structs.Configurations;
+using System.Drawing.Drawing2D;
+using STROOP.Interfaces;
+using STROOP.Controls.Map.Objects;
+
+namespace STROOP.Controls.Map.Trackers
+{
+    public partial class ColorSelector : UserControl
+    {
+        public ColorSelector()
+        {
+            InitializeComponent();
+        }
+    }
+}
