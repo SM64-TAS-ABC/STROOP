a78cca89cea00a1bae89faf8dfe01d9445a28ca9
scob <scob@seas.upenn.edu>
2018-03-20 18:14:07 -0400

    add more sub controls to color selector

--- STROOP/Controls/Map/Trackers/ColorSelector.Designer.cs
+++ STROOP/Controls/Map/Trackers/ColorSelector.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelColorSelector = new System.Windows.Forms.Panel();
+            this.textBoxColorSelector = new STROOP.BetterTextbox();
+            this.panelColorSelector.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelColorSelector
+            // 
+            this.panelColorSelector.BackColor = System.Drawing.SystemColors.Control;
+            this.panelColorSelector.Controls.Add(this.textBoxColorSelector);
+            this.panelColorSelector.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelColorSelector.Location = new System.Drawing.Point(0, 0);
+            this.panelColorSelector.Margin = new System.Windows.Forms.Padding(0);
+            this.panelColorSelector.Name = "panelColorSelector";
+            this.panelColorSelector.Size = new System.Drawing.Size(114, 37);
+            this.panelColorSelector.TabIndex = 0;
+            // 
+            // textBoxColorSelector
+            // 
+            this.textBoxColorSelector.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxColorSelector.Location = new System.Drawing.Point(26, 8);
+            this.textBoxColorSelector.Margin = new System.Windows.Forms.Padding(0);
+            this.textBoxColorSelector.Name = "textBoxColorSelector";
+            this.textBoxColorSelector.Size = new System.Drawing.Size(63, 20);
+            this.textBoxColorSelector.TabIndex = 34;
+            this.textBoxColorSelector.Text = "#000000";
+            this.textBoxColorSelector.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // ColorSelector
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.panelColorSelector);
+            this.Margin = new System.Windows.Forms.Padding(0);
             this.Name = "ColorSelector";
-            this.Size = new System.Drawing.Size(373, 99);
+            this.Size = new System.Drawing.Size(114, 37);
+            this.panelColorSelector.ResumeLayout(false);
+            this.panelColorSelector.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelColorSelector;
+        private BetterTextbox textBoxColorSelector;
     }
 }
