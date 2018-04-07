deac90c4db482a1c54713c1bc1692d3b1a1504d8
scob <scob@seas.upenn.edu>
2018-04-07 11:57:43 -0400

    add in game trig class

---
+++ STROOP/Structs/InGameTrig.cs
@@ -0,0 +1,16 @@
+﻿using STROOP.Forms;
+using STROOP.Structs;
+using STROOP.Structs.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STROOP.Utilities
+{
+    public static class InGameTrig
+    {
+        
+    }
+} 
