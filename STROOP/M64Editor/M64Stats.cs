9db4c3cef495959277e2367d3aac49d23856768a
scob <scob@seas.upenn.edu>
2018-04-08 17:30:30 -0400

    implement m64 stats context menu strip getter

--- STROOP/M64Editor/M64Stats.cs
+++ STROOP/M64Editor/M64Stats.cs
@@ -8,6 +8,8 @@ using System.Xml.Serialization;
 using STROOP.Structs;
 using System.ComponentModel;
 using STROOP.Utilities;
+using System.Windows.Forms;
+using STROOP.Forms;
 
 namespace STROOP.M64Editor
 {
@@ -123,7 +125,7 @@ namespace STROOP.M64Editor
         private List<(int, int)> FindPresses(Func<M64InputFrame, bool> isPressedFunction)
         {
             List<(int, int)> pressList = new List<(int, int)>();
-            int pressStart = 0;
+            int startFrame = 0;
             bool isAlreadyPressed = false;
 
             for (int i = 0; i <= _inputs.Count; i++)
@@ -138,8 +140,8 @@ namespace STROOP.M64Editor
                     }
                     else // just stopped pressing
                     {
-                        int pressEnd = i - 1;
-                        pressList.Add((pressStart, pressEnd));
+                        int endFrame = i - 1;
+                        pressList.Add((startFrame, endFrame));
                         isAlreadyPressed = false;
                     }
                 }
@@ -147,7 +149,7 @@ namespace STROOP.M64Editor
                 {
                     if (isCurrentlyPressed) // just started pressing
                     {
-                        pressStart = i;
+                        startFrame = i;
                         isAlreadyPressed = true;
                     }
                     else // still not pressing
@@ -167,6 +169,80 @@ namespace STROOP.M64Editor
             {
                 unpressFunction(_inputs[i]);
             }
+        }
+
+        private static readonly List<Func<M64InputFrame, bool>> isPressedFunctionList =
+            new List<Func<M64InputFrame, bool>>()
+            {
+                input => input.A,
+                input => input.B,
+                input => input.Z,
+                input => input.Start,
+                input => input.R,
+                input => input.C_Up,
+                input => input.C_Down,
+                input => input.C_Left,
+                input => input.C_Right,
+                input => input.L,
+                input => input.D_Up,
+                input => input.D_Down,
+                input => input.D_Left,
+                input => input.D_Right,
+            };
+
+        private static readonly List<string> buttonNameList =
+            new List<string>()
+            {
+                "A", "B", "Z", "Start", "R",
+                "C^", "Cv", "C<", "C>",
+                "L", "D^", "Dv", "D<", "D>",
+            };
+
+        public ContextMenuStrip CreateContextMenuStrip()
+        {
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            List<string> itemNames = buttonNameList.ConvertAll(
+                buttonName => "Show all " + buttonName + " presses");
+            List<ToolStripMenuItem> items = itemNames.ConvertAll(
+                itemName => new ToolStripMenuItem(itemName));
+
+            if (items.Count != isPressedFunctionList.Count)
+                throw new ArgumentOutOfRangeException();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                int index = i;
+                items[index].Click += (sender, e) =>
+                {
+                    List<(int, int)> buttonPresses = FindPresses(isPressedFunctionList[index]);
+                    string buttonPressesString =
+                        FormatButtonPressesString(buttonPresses, buttonNameList[index]);
+                    InfoForm.ShowText(
+                        "Num Button Presses",
+                        String.Format("Num {0} Presses", buttonNameList[index]),
+                        buttonPressesString);
+                };
+            }
+
+            return contextMenuStrip;
+        }
+
+        private string FormatButtonPressesString(List<(int, int)> buttonPresses, string buttonName)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(String.Format(
+                "{0} {1} presses total:", buttonPresses.Count, buttonName));
+            for (int i = 0; i < buttonPresses.Count; i++)
+            {
+                int countIndex = i + 1;
+                (int startFrame, int endFrame) = buttonPresses[i];
+                int frameSpan = endFrame - startFrame + 1;
+                string pluralitySuffix = frameSpan == 1 ? "" : "s";
+                builder.Append(String.Format(
+                    "{0} press #{1}: frame {2} to frame {3} ({4} frame{5})",
+                    buttonName, countIndex, startFrame, endFrame, frameSpan, pluralitySuffix));
+            }
+            return builder.ToString();
         }
     }
 }
