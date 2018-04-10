dde2484b563296c166b38003170727e409f7d012
scob <scob@seas.upenn.edu>
2018-04-09 21:34:20 -0400

    make copied frame class

--- STROOP/M64Editor/M64CopiedData.cs
+++ STROOP/M64Editor/M64CopiedData.cs
@@ -10,16 +10,24 @@ namespace STROOP.M64Editor
         private readonly int _startFrame;
         private readonly int _endFrame;
         private readonly int _totalFrames;
-        private readonly string _type;
+        private readonly string _typeString;
         private readonly string _fileName;
+        private readonly List<M64CopiedFrame> _copiedFrames;
 
-        private M64CopiedData(int startFrame, int endFrame, string type, string fileName)
+        private M64CopiedData(
+            int startFrame,
+            int endFrame,
+            string typeString,
+            string fileName,
+            List<M64CopiedFrame> copiedFrames)
         {
             _startFrame = startFrame;
             _endFrame = endFrame;
             _totalFrames = endFrame - startFrame + 1;
-            _type = type;
+            _typeString = typeString;
             _fileName = fileName;
+            _copiedFrames = copiedFrames;
+            if (_totalFrames != copiedFrames.Count) throw new ArgumentOutOfRangeException();
         }
 
         public static M64CopiedData CreateCopiedData(
@@ -32,33 +40,23 @@ namespace STROOP.M64Editor
             if (!useRows && inputsString == null) return null;
 
             string type = useRows ? "Row" : inputsString;
-            return new M64CopiedData(startFrame, endFrame, type, fileName);
+            return new M64CopiedData(startFrame, endFrame, type, fileName, new List<M64CopiedFrame>());
         }
 
-        /*
-        public static M64CopiedData CreateCopiedDataFromCells(DataGridView table, string fileName)
+        public void Apply(List<M64InputFrame> inputs)
         {
-            if (table.SelectedCells.Count == 0 || fileName == null) return null;
-            List<M64InputCell> cells = M64Utilities.GetSelectedInputCells(table);
-            (int minFrame, int maxFrame, string inputsString) = M64Utilities.GetCellStats(cells);
-            return new M64CopiedData(minFrame, maxFrame, inputsString, fileName);
+            if (inputs.Count != _copiedFrames.Count) throw new ArgumentOutOfRangeException();
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                _copiedFrames[i].Apply(inputs[i]);
+            }
         }
-
-        public static M64CopiedData CreateCopiedDataFromRows(DataGridView table, string fileName)
-        {
-            if (table.SelectedRows.Count == 0 || fileName == null) return null;
-            List<M64InputFrame> inputs = M64Utilities.GetSelectedInputFrames(table);
-            int minFrame = inputs.Min(input => input.Frame);
-            int maxFrame = inputs.Max(input => input.Frame);
-            return new M64CopiedData(minFrame, maxFrame, "Row", fileName);
-        }
-        */
 
         public override string ToString()
         {
             return String.Format(
                 "{0}f [{1}] {2}-{3} @{4}",
-                _totalFrames, _type, _startFrame, _endFrame, _fileName);
+                _totalFrames, _typeString, _startFrame, _endFrame, _fileName);
         }
     }
 }
---
+++ STROOP/M64Editor/M64CopiedFrame.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace STROOP.M64Editor
+{
+    public class M64CopiedFrame
+    {
+        public readonly sbyte? X;
+        public readonly sbyte? Y;
+        public readonly bool? A;
+        public readonly bool? B;
+        public readonly bool? Z;
+        public readonly bool? S;
+        public readonly bool? R;
+        public readonly bool? C_Up;
+        public readonly bool? C_Down;
+        public readonly bool? C_Left;
+        public readonly bool? C_Right;
+        public readonly bool? L;
+        public readonly bool? D_Up;
+        public readonly bool? D_Down;
+        public readonly bool? D_Left;
+        public readonly bool? D_Right;
+
+        public M64CopiedFrame(
+            sbyte? X = null,
+            sbyte? Y = null,
+            bool? A = null,
+            bool? B = null,
+            bool? Z = null,
+            bool? S = null,
+            bool? R = null,
+            bool? C_Up = null,
+            bool? C_Down = null,
+            bool? C_Left = null,
+            bool? C_Right = null,
+            bool? L = null,
+            bool? D_Up = null,
+            bool? D_Down = null,
+            bool? D_Left = null,
+            bool? D_Right = null)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.A = A;
+            this.B = B;
+            this.Z = Z;
+            this.S = S;
+            this.R = R;
+            this.C_Up = C_Up;
+            this.C_Down = C_Down;
+            this.C_Left = C_Left;
+            this.C_Right = C_Right;
+            this.L = L;
+            this.D_Up = D_Up;
+            this.D_Down = D_Down;
+            this.D_Left = D_Left;
+            this.D_Right = D_Right;
+        }
+
+        public void Apply(M64InputFrame input)
+        {
+            if (X.HasValue) input.X = X.Value;
+            if (Y.HasValue) input.Y = Y.Value;
+            if (A.HasValue) input.A = A.Value;
+            if (B.HasValue) input.B = B.Value;
+            if (Z.HasValue) input.Z = Z.Value;
+            if (S.HasValue) input.S = S.Value;
+            if (R.HasValue) input.R = R.Value;
+            if (C_Up.HasValue) input.C_Up = C_Up.Value;
+            if (C_Down.HasValue) input.C_Down = C_Down.Value;
+            if (C_Left.HasValue) input.C_Left = C_Left.Value;
+            if (C_Right.HasValue) input.C_Right = C_Right.Value;
+            if (L.HasValue) input.L = L.Value;
+            if (D_Up.HasValue) input.D_Up = D_Up.Value;
+            if (D_Down.HasValue) input.D_Down = D_Down.Value;
+            if (D_Left.HasValue) input.D_Left = D_Left.Value;
+            if (D_Right.HasValue) input.D_Right = D_Right.Value;
+        }
+    }
+}
