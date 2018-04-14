36aefe31a0c19d3f8815dd2d5714b8210e428eee
scob <scob@seas.upenn.edu>
2018-04-14 11:06:05 -0400

    implement lag var for mupen lua

--- STROOP/Utilities/MupenUtilities.cs
+++ STROOP/Utilities/MupenUtilities.cs
@@ -10,11 +10,41 @@ namespace STROOP.Utilities
 {
     public static class MupenUtilities
     {
-        private static int FrameCountAddress = 0x0047A7A4;
-        private static int VICountAddress = 0x0047A7A0;
+        private static int FrameCountAddress
+        {
+            get
+            {
+                switch (Config.Stream.ProcessName)
+                {
+                    case "mupen64-rerecording":
+                        return 0x0047A7A4;
+                    case "mupen64_lua":
+                        return 0x004D614C;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        private static int VICountAddress
+        {
+            get
+            {
+                switch (Config.Stream.ProcessName)
+                {
+                    case "mupen64-rerecording":
+                        return 0x0047A7A0;
+                    case "mupen64_lua":
+                        return 0x004D6150;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
 
+        /*
         private static int FrameCountAddress2 = 0x0077DF50;
         private static int VICountAddress2 = 0x0077DF44;
+        */
 
         public static int GetFrameCount()
         {
@@ -41,7 +71,8 @@ namespace STROOP.Utilities
 
         public static bool IsUsingMupen()
         {
-            return Config.Stream.ProcessName == "mupen64-rerecording";
+            return Config.Stream.ProcessName == "mupen64-rerecording" ||
+                Config.Stream.ProcessName == "mupen64_lua";
         }
 
     }
