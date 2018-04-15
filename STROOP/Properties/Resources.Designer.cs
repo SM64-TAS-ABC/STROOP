20a880fa4c608d90d9b1d46ee347a22c6ec2d0c8
scob <scob@seas.upenn.edu>
2018-04-14 23:33:23 -0400

    add lock blue obj

--- STROOP/Properties/Resources.Designer.cs
+++ STROOP/Properties/Resources.Designer.cs
@@ -381,6 +381,16 @@ namespace STROOP.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap lock_blue {
+            get {
+                object obj = ResourceManager.GetObject("lock_blue", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] python_xshd {
