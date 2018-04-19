72435f0f7836e8c9967e357fe850fdb291044a37
scob <scob@seas.upenn.edu>
2018-04-19 19:44:28 -0400

    change colors of columns

--- STROOP/Structs/Configurations/M64Config.cs
+++ STROOP/Structs/Configurations/M64Config.cs
@@ -13,6 +13,14 @@ namespace STROOP.Structs
         public static readonly Color NewRowColor = Color.FromArgb(186, 255, 166);
         public static readonly Color EditedCellColor = Color.Pink;
 
+        public static readonly Color? FrameColumnColor = Color.FromArgb(235, 255, 255);
+        public static readonly Color? MainButtonColor = Color.White;
+        public static readonly Color? CButtonColumnColor = Color.FromArgb(255, 255, 150);
+        public static readonly Color? NoopButtonColumnColor = Color.LightGray;
+
+        public static readonly int TextColumnFillWeight = 200;
+        public static readonly int CheckBoxColumnFillWeight = 100;
+
         public static readonly int HeaderSize = 0x400;
         public static readonly byte[] SignatureBytes = new byte[] { 0x4D, 0x36, 0x34, 0x1A };
 
