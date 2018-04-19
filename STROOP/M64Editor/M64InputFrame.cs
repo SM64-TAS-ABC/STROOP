4aff500deffd888621a31864a6657f5fa67b9b7f
scob <scob@seas.upenn.edu>
2018-04-19 18:46:16 -0400

    change logic for cell color updating

--- STROOP/M64Editor/M64InputFrame.cs
+++ STROOP/M64Editor/M64InputFrame.cs
@@ -55,22 +55,22 @@ namespace STROOP.M64Editor
 
         public int Frame { get => M64Utilities.ConvertFrameToDisplayedValue(FrameIndex); }
         public int Id { get => M64Utilities.ConvertFrameToDisplayedValue(IdIndex); }
-        public sbyte X { get => (sbyte)GetByte(2); set { SetByte(2, (byte)value); NotifyChange(2, X != _X); } }
-        public sbyte Y { get => (sbyte)GetByte(3); set { SetByte(3, (byte)value); NotifyChange(3, Y != _Y); } }
-        public bool A { get => GetBit(7); set { SetBit(7, value); NotifyChange(4, A != _A); } }
-        public bool B { get => GetBit(6); set { SetBit(6, value); NotifyChange(5, B != _B); } }
-        public bool Z { get => GetBit(5); set { SetBit(5, value); NotifyChange(6, Z != _Z); } }
-        public bool S { get => GetBit(4); set { SetBit(4, value); NotifyChange(7, S != _S); } }
-        public bool R { get => GetBit(12); set { SetBit(12, value); NotifyChange(8, R != _R); } }
-        public bool C_Up { get => GetBit(11); set { SetBit(11, value); NotifyChange(9, C_Up != _C_Up); } }
-        public bool C_Down { get => GetBit(10); set { SetBit(10, value); NotifyChange(10, C_Down != _C_Down); } }
-        public bool C_Left { get => GetBit(9); set { SetBit(9, value); NotifyChange(11, C_Left != _C_Left); } }
-        public bool C_Right { get => GetBit(8); set { SetBit(8, value); NotifyChange(12, C_Right != _C_Right); } }
-        public bool L { get => GetBit(13); set { SetBit(13, value); NotifyChange(13, L != _L); } }
-        public bool D_Up { get => GetBit(3); set { SetBit(3, value); NotifyChange(14, D_Up != _D_Up); } }
-        public bool D_Down { get => GetBit(2); set { SetBit(2, value); NotifyChange(15, D_Down != _D_Down); } }
-        public bool D_Left { get => GetBit(1); set { SetBit(1, value); NotifyChange(16, D_Left != _D_Left); } }
-        public bool D_Right { get => GetBit(0); set { SetBit(0, value); NotifyChange(17, D_Right != _D_Right); } }
+        public sbyte X { get => (sbyte)GetByte(2); set { SetByte(2, (byte)value); NotifyChange(); } }
+        public sbyte Y { get => (sbyte)GetByte(3); set { SetByte(3, (byte)value); NotifyChange(); } }
+        public bool A { get => GetBit(7); set { SetBit(7, value); NotifyChange(); } }
+        public bool B { get => GetBit(6); set { SetBit(6, value); NotifyChange(); } }
+        public bool Z { get => GetBit(5); set { SetBit(5, value); NotifyChange(); } }
+        public bool S { get => GetBit(4); set { SetBit(4, value); NotifyChange(); } }
+        public bool R { get => GetBit(12); set { SetBit(12, value); NotifyChange(); } }
+        public bool C_Up { get => GetBit(11); set { SetBit(11, value); NotifyChange(); } }
+        public bool C_Down { get => GetBit(10); set { SetBit(10, value); NotifyChange(); } }
+        public bool C_Left { get => GetBit(9); set { SetBit(9, value); NotifyChange(); } }
+        public bool C_Right { get => GetBit(8); set { SetBit(8, value); NotifyChange(); } }
+        public bool L { get => GetBit(13); set { SetBit(13, value); NotifyChange(); } }
+        public bool D_Up { get => GetBit(3); set { SetBit(3, value); NotifyChange(); } }
+        public bool D_Down { get => GetBit(2); set { SetBit(2, value); NotifyChange(); } }
+        public bool D_Left { get => GetBit(1); set { SetBit(1, value); NotifyChange(); } }
+        public bool D_Right { get => GetBit(0); set { SetBit(0, value); NotifyChange(); } }
 
         private readonly sbyte _X;
         private readonly sbyte _Y;
@@ -89,15 +89,49 @@ namespace STROOP.M64Editor
         private readonly bool _D_Left;
         private readonly bool _D_Right;
 
-        private void NotifyChange(int columnIndex, bool valueChanged)
+        private List<object> GetOriginalValues()
+        {
+            return new List<object>()
+            {
+                _X, _Y,
+                _A, _B, _Z, _S, _R,
+                _C_Up, _C_Down, _C_Left, _C_Right,
+                _L, _D_Up, _D_Down, _D_Left, _D_Right,
+            };
+        }
+
+        private List<object> GetCurrentValues()
+        {
+            return new List<object>()
+            {
+                X, Y,
+                A, B, Z, S, R,
+                C_Up, C_Down, C_Left, C_Right,
+                L, D_Up, D_Down, D_Left, D_Right,
+            };
+        }
+
+        private void NotifyChange()
         {
             _m64File.IsModified = true;
-            DataGridViewRow row = _table.Rows[FrameIndex];
-            DataGridViewColumn col = _table.Columns[columnIndex];
-            DataGridViewCell cell = row.Cells[columnIndex];
-            Color defaultColor = row.DefaultCellStyle.BackColor == M64Config.NewRowColor ?
-                M64Config.NewRowColor : col.DefaultCellStyle.BackColor;
-            cell.Style.BackColor = valueChanged ? M64Config.EditedCellColor : defaultColor;
+            UpdateCellColors();
+        }
+
+        public void UpdateCellColors()
+        {
+            List<object> originalValues = GetOriginalValues();
+            List<object> currentvalues = GetCurrentValues();
+            for (int i = 0; i < 16; i++)
+            {
+                bool valueChanged = !Equals(originalValues[i], currentvalues[i]);
+                int columnIndex = i + 2;
+                DataGridViewRow row = _table.Rows[FrameIndex];
+                DataGridViewColumn col = _table.Columns[columnIndex];
+                DataGridViewCell cell = row.Cells[columnIndex];
+                Color defaultColor = row.DefaultCellStyle.BackColor == M64Config.NewRowColor ?
+                    M64Config.NewRowColor : col.DefaultCellStyle.BackColor;
+                cell.Style.BackColor = valueChanged ? M64Config.EditedCellColor : defaultColor;
+            }
         }
 
         private void SetByte(int num, byte value)
