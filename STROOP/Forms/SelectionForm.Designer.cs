844a32d4bfbfb17872bf5a8186ff5a3d4f758337
scob <scob@seas.upenn.edu>
2018-04-30 14:12:47 -0400

    created selection form

---
+++ STROOP/Forms/SelectionForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace STROOP.Forms
+{
+    partial class SelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InfoForm));
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxTriangleInfo = new System.Windows.Forms.TextBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.Controls.Add(this.textBoxTriangleInfo, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.buttonOk, 0, 2);
+            this.tableLayoutPanelMain.Controls.Add(this.textBoxTitle, 0, 0);
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 3;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(317, 182);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // textBoxTriangleInfo
+            // 
+            this.textBoxTriangleInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxTriangleInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTriangleInfo.Location = new System.Drawing.Point(3, 33);
+            this.textBoxTriangleInfo.Multiline = true;
+            this.textBoxTriangleInfo.Name = "textBoxTriangleInfo";
+            this.textBoxTriangleInfo.ReadOnly = true;
+            this.textBoxTriangleInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxTriangleInfo.Size = new System.Drawing.Size(311, 116);
+            this.textBoxTriangleInfo.TabIndex = 3;
+            this.textBoxTriangleInfo.Text = "\r\n";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonOk.Location = new System.Drawing.Point(121, 155);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 0;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTitle.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTitle.Location = new System.Drawing.Point(3, 3);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.ReadOnly = true;
+            this.textBoxTitle.Size = new System.Drawing.Size(311, 13);
+            this.textBoxTitle.TabIndex = 17;
+            this.textBoxTitle.Text = "Triangle Info";
+            this.textBoxTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // InfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 206);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(10000, 10000);
+            this.MinimizeBox = false;
+            this.Name = "InfoForm";
+            this.ShowIcon = false;
+            this.Text = "Info";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.tableLayoutPanelMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.TextBox textBoxTriangleInfo;
+        private System.Windows.Forms.TextBox textBoxTitle;
+    }
+}
---
+++ STROOP/Forms/SelectionForm.cs
@@ -0,0 +1,156 @@
+﻿using STROOP.Structs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using STROOP.Extensions;
+using STROOP.Utilities;
+
+namespace STROOP.Forms
+{
+    public partial class SelectionForm : Form
+    {
+        public SelectionForm()
+        {
+            InitializeComponent();
+            buttonOk.Click += (sender, e) => Close();
+            textBoxTriangleInfo.DoubleClick += (sender, e) => textBoxTriangleInfo.SelectAll();
+        }
+
+        public void SetTriangleCoordinates(short[] coordinates)
+        {
+            this.Text = "Triangle Info";
+            textBoxTitle.Text = "Triangle Coordinates";
+            textBoxTriangleInfo.Text = StringifyCoordinates(coordinates);
+        }
+
+        public void SetTriangleEquation(float normalX, float normalY, float normalZ, float normalOffset)
+        {
+            this.Text = "Triangle Info";
+            textBoxTitle.Text = "Triangle Equation";
+            textBoxTriangleInfo.Text =
+                normalX + "x + " + normalY + "y + " + normalZ + "z + " + normalOffset + " = 0";
+        }
+
+        public void SetTriangleData(List<short[]> coordinateList, bool repeatFirstVertex)
+        {
+            this.Text = "Triangle Info";
+            textBoxTitle.Text = "Triangle Data";
+            textBoxTriangleInfo.Text = String.Join(
+                "\r\n\r\n",
+                coordinateList.ConvertAll(
+                    coordinates => StringifyCoordinates(coordinates, repeatFirstVertex)));
+        }
+
+        public void SetTriangleVertices(List<short[]> coordinateList)
+        {
+            this.Text = "Triangle Info";
+            textBoxTitle.Text = "Triangle Vertices";
+            List<short[]> vertexList = new List<short[]>();
+            coordinateList.ForEach(
+                coordinates =>
+                {
+                    vertexList.Add(new short[] { coordinates[0], coordinates[1], coordinates[2] });
+                    vertexList.Add(new short[] { coordinates[3], coordinates[4], coordinates[5] });
+                    vertexList.Add(new short[] { coordinates[6], coordinates[7], coordinates[8] });
+                });
+
+            List<short[]> uniqueVertexList = new List<short[]>();
+            vertexList.ForEach(
+                vertex =>
+                {
+                    bool hasAlready = uniqueVertexList.Any(v => Enumerable.SequenceEqual(v, vertex));
+                    if (!hasAlready) uniqueVertexList.Add(vertex);
+                });
+
+            uniqueVertexList.Sort(
+                (short[] v1, short[] v2) =>
+                {
+                    int diff = v1[0] - v2[0];
+                    if (diff != 0) return diff;
+                    diff = v1[1] - v2[1];
+                    if (diff != 0) return diff;
+                    diff = v1[2] - v2[2];
+                    return diff;
+                });
+
+            textBoxTriangleInfo.Text = String.Join(
+                "\r\n",
+                uniqueVertexList.ConvertAll(
+                    coordinate => StringifyCoordinate(coordinate)));
+        }
+
+        public void SetTriangles(List<TriangleDataModel> triangleList)
+        {
+            this.Text = "Triangle Info";
+            textBoxTitle.Text = "Triangles";
+            textBoxTriangleInfo.Text = TriangleDataModel.GetFieldNameString() + "\n" + String.Join("\n", triangleList);
+        }
+
+        private String StringifyCoordinates(short[] coordinates, bool repeatCoordinates = false)
+        {
+            if (coordinates.Length != 9) throw new ArgumentOutOfRangeException();
+
+            string text =
+                coordinates[0] + "\t" + coordinates[1] + "\t" + coordinates[2] + "\r\n" +
+                coordinates[3] + "\t" + coordinates[4] + "\t" + coordinates[5] + "\r\n" +
+                coordinates[6] + "\t" + coordinates[7] + "\t" + coordinates[8];
+
+            if (repeatCoordinates)
+            {
+                text += "\r\n" + coordinates[0] + "\t" + coordinates[1] + "\t" + coordinates[2];
+            }
+
+            return text;
+        }
+
+        private String StringifyCoordinate(short[] coordinate)
+        {
+            if (coordinate.Length != 3) throw new ArgumentOutOfRangeException();
+            string text = coordinate[0] + "\t" + coordinate[1] + "\t" + coordinate[2];
+            return text;
+        }
+
+        public void SetDictionary<TKey, TValue>(Dictionary<TKey, TValue> dictionary, string keyName = null, string valueName = null)
+        {
+            this.Text = "Dictionary Info";
+            textBoxTitle.Text = "Dictionary";
+            String text = "";
+            if (keyName != null && valueName != null)
+            {
+                text += (keyName + "\t" + valueName + "\r\n");
+            }
+            foreach (KeyValuePair<TKey, TValue> entry in dictionary)
+            {
+                text += (entry.Key + "\t" + entry.Value + "\r\n");
+            }
+            textBoxTriangleInfo.Text = text;
+        }
+
+        public void SetText(string formTitle, string textTitle, string text)
+        {
+            this.Text = formTitle;
+            textBoxTitle.Text = textTitle;
+            textBoxTriangleInfo.Text = text;
+        }
+
+        public static void ShowText(string formTitle, string textTitle, string text)
+        {
+            InfoForm infoForm = new InfoForm();
+            infoForm.SetText(formTitle, textTitle, text);
+            infoForm.Show();
+        }
+
+        public static void ShowValue(object value)
+        {
+            InfoForm infoForm = new InfoForm();
+            infoForm.SetText("Info", "Text", value.ToString());
+            infoForm.Show();
+        }
+    }
+}
