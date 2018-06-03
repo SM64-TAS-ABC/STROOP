a2d67a326879c06c9cd6a11dcdb49b70d1f3bc1c
scob <scob@seas.upenn.edu>
2018-06-03 13:49:42 -0400

    added point angle

---
+++ STROOP/Utilities/PositionAngle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace STROOP.Utilities
+{
+    public class PositionAngle
+    {
+        public readonly double X;
+        public readonly double Y;
+        public readonly double Z;
+        public readonly double? Angle;
+
+        public PositionAngle(double x, double y, double z, double? angle = null)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Angle = angle;
+        }
+    }
+}
