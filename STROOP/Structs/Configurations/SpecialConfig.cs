8e3719a1bb5974e0507d0344a92916643e8ab7de
scob <scob@seas.upenn.edu>
2018-06-03 13:34:13 -0400

    refactor point vars to use properties

--- STROOP/Structs/Configurations/SpecialConfig.cs
+++ STROOP/Structs/Configurations/SpecialConfig.cs
@@ -9,10 +9,37 @@ namespace STROOP.Structs.Configurations
 {
     public static class SpecialConfig
     {
-        public static double PointX = 0;
-        public static double PointY = 0;
-        public static double PointZ = 0;
-        public static double PointAngle = 0;
+        // Point vars
+
+        private static double _pointX = 0;
+        public static double PointX
+        {
+            get { return _pointX; }
+            set { _pointX = value; }
+        }
+
+        private static double _pointY = 0;
+        public static double PointY
+        {
+            get { return _pointY; }
+            set { _pointY = value; }
+        }
+
+        private static double _pointZ = 0;
+        public static double PointZ
+        {
+            get { return _pointZ; }
+            set { _pointZ = value; }
+        }
+
+        private static double _pointAngle = 0;
+        public static double PointAngle
+        {
+            get { return _pointAngle; }
+            set { _pointAngle = value; }
+        }
+
+        // PU vars
 
         public static int PuParam1 = 0;
         public static int PuParam2 = 1;
@@ -24,6 +51,8 @@ namespace STROOP.Structs.Configurations
                 return MoreMath.GetHypotenuse(PuParam1, PuParam2);
             }
         }
+
+        // Mupen vars
 
         public static int MupenLagOffset = 0;
     }
