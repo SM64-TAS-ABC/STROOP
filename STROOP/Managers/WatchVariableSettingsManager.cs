060f01700aaaddbbf5af1a564013ba4bb88592b3
scob <scob@seas.upenn.edu>
2018-06-05 17:20:50 -0400

    created watch var settings manager

---
+++ STROOP/Managers/WatchVariableSettingsManager.cs
@@ -0,0 +1,34 @@
+﻿using STROOP.Controls;
+using STROOP.Managers;
+using STROOP.Structs.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STROOP.Structs
+{
+    public static class WatchVariableSettingsManager
+    {
+        private static List<WatchVariableControlSettings> settingsList =
+            new List<WatchVariableControlSettings>();
+
+        public static void AddSettings(WatchVariableControlSettings settings)
+        {
+            settingsList.Add(settings);
+        }
+
+        public static int GetSettingsLevel()
+        {
+            return settingsList.Count;
+        }
+
+        public static List<WatchVariableControlSettings> GetSettingsToApply(int currentLevel)
+        {
+            return settingsList.Skip(currentLevel).ToList();
+        }
+    };
+}
