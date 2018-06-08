9e6a8e3f4326947bdd8c931632056878749daf69
scob <scob@seas.upenn.edu>
2018-06-08 13:26:36 -0400

    selection form remembers size

--- STROOP/Forms/SelectionForm.cs
+++ STROOP/Forms/SelectionForm.cs
@@ -9,9 +9,19 @@ namespace STROOP.Forms
 {
     public partial class SelectionForm : Form
     {
+        public static int? WIDTH = null;
+        public static int? HEIGHT = null;
+
         public SelectionForm()
         {
             InitializeComponent();
+            if (WIDTH.HasValue) Width = WIDTH.Value;
+            if (HEIGHT.HasValue) Height = HEIGHT.Value;
+            Resize += (sender, e) =>
+            {
+                WIDTH = Width;
+                HEIGHT = Height;
+            };
         }
 
         public void Initialize<T>(
