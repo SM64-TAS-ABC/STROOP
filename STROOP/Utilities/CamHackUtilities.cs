9101ef6c320931e7065f5e1f949bcf7f12923e96
scob <scob@seas.upenn.edu>
2018-06-11 16:52:43 -0400

    simplify GetCamHackYawFacing

--- STROOP/Utilities/CamHackUtilities.cs
+++ STROOP/Utilities/CamHackUtilities.cs
@@ -10,27 +10,13 @@ namespace STROOP.Utilities
 {
     public static class CamHackUtilities
     {
-        public static double GetCamHackYawFacing(CamHackMode? camHackModeNullable = null)
+        public static double GetCamHackYawFacing()
         {
-            CamHackMode camHackMode = camHackModeNullable ?? Config.CamHackManager.CurrentCamHackMode;
-            switch (camHackMode)
-            {
-                case CamHackMode.REGULAR:
-                    return Config.Stream.GetUInt16(CameraConfig.StructAddress + CameraConfig.FacingYawOffset);
-
-                case CamHackMode.RELATIVE_ANGLE:
-                case CamHackMode.ABSOLUTE_ANGLE:
-                case CamHackMode.FIXED_POS:
-                case CamHackMode.FIXED_ORIENTATION:
-                    float camHackPosX = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.CameraXOffset);
-                    float camHackPosZ = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.CameraZOffset);
-                    float camHackFocusX = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.FocusXOffset);
-                    float camHackFocusZ = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.FocusZOffset);
-                    return MoreMath.AngleTo_AngleUnits(camHackPosX, camHackPosZ, camHackFocusX, camHackFocusZ);
-
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+            float camHackPosX = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.CameraXOffset);
+            float camHackPosZ = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.CameraZOffset);
+            float camHackFocusX = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.FocusXOffset);
+            float camHackFocusZ = Config.Stream.GetSingle(CamHackConfig.StructAddress + CamHackConfig.FocusZOffset);
+            return MoreMath.AngleTo_AngleUnits(camHackPosX, camHackPosZ, camHackFocusX, camHackFocusZ);
         }
     }
 }
