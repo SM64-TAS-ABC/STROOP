01b69656bd02d305eafb161491e846f0381a952b
scob <scob@seas.upenn.edu>
2018-07-07 09:07:15 -0400

    add ttc savestate class

---
+++ STROOP/TTC/TtcSaveState.cs
@@ -0,0 +1,31 @@
+﻿using STROOP.Structs.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace STROOP.Ttc
+{
+
+    public class TtcSaveState
+    {
+
+        public TtcSaveState(List<TtcObject> objects)
+        {
+
+        }
+
+        public TtcSaveState(string saveState)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+
+
+}
