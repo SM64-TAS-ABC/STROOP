f2ba5826df11cda2307f04e03b1a2e91ef390cef
scob <scob@seas.upenn.edu>
2018-07-07 10:00:09 -0400

    fill in constructors of ttc save state

--- STROOP/TTC/TtcSaveState.cs
+++ STROOP/TTC/TtcSaveState.cs
@@ -1,4 +1,5 @@
 ﻿using STROOP.Structs.Configurations;
+using STROOP.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,24 +11,33 @@ namespace STROOP.Ttc
     public class TtcSaveState
     {
 
-        public TtcSaveState()
-        {
+        private readonly List<byte> _bytes;
 
+        public TtcSaveState() : this(TtcUtilities.CreateRngObjectsFromGame(new TtcRng(0)))
+        {
         }
 
         public TtcSaveState(List<TtcObject> objects)
         {
-
+            List<object> fields = objects.ConvertAll(obj => obj.GetFields())
+                .SelectMany(list => list).ToList();
         }
 
-        public TtcSaveState(string saveState)
+        public TtcSaveState(string stringValue)
         {
-
+            _bytes = new List<byte>();
+            for (int i = 0; i < stringValue.Length; i += 2)
+            {
+                string substring = stringValue.Substring(i, 2);
+                byte b = Convert.ToByte(substring, 16);
+                _bytes.Add(b);
+            }
         }
 
         public override string ToString()
         {
-            return base.ToString();
+            List<string> byteStrings = _bytes.ConvertAll(b => HexUtilities.FormatValue(b, 2, false));
+            return String.Join("", byteStrings);
         }
 
     }
