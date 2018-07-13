7b3877e1aec22d031b0711438c2b9c2610d8b111
scob <scob@seas.upenn.edu>
2018-07-13 14:15:41 -0400

    fix main save data

--- STROOP/Structs/Configurations/MainSaveConfig.cs
+++ STROOP/Structs/Configurations/MainSaveConfig.cs
@@ -9,8 +9,8 @@ namespace STROOP.Structs.Configurations
     public static class MainSaveConfig
     {
         public static uint MainSaveStructAddress { get => RomVersionConfig.Switch(MainSaveStructAddressUS, MainSaveStructAddressJP); }
-        public static readonly uint MainSaveStructAddressUS = 0x802078C4;
-        public static readonly uint MainSaveStructAddressJP = 0x802078D0;
+        public static readonly uint MainSaveStructAddressUS = 0x802078C0;
+        public static readonly uint MainSaveStructAddressJP = 0x80207CC0;
 
         public static readonly uint MainSaveStructSize = 0x20;
 
