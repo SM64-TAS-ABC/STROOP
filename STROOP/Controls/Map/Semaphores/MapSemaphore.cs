1159fcc0867bc198f18e74afb2682ee337f50025
scob <scob@seas.upenn.edu>
2018-07-15 11:52:51 -0400

    add semaphor classes

---
+++ STROOP/Controls/Map/Semaphores/MapSemaphore.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STROOP.Controls.Map.Semaphores
+{
+    public class MapSemaphore
+    {
+        public bool IsUsed = false;
+    }
+}
