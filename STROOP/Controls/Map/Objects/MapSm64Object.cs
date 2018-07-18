ebc22ac0505e64579c8d8cee7ce476e7861db12a
scob <scob@seas.upenn.edu>
2018-07-18 11:38:52 -0400

    implement visibility type

--- STROOP/Controls/Map/Objects/MapSm64Object.cs
+++ STROOP/Controls/Map/Objects/MapSm64Object.cs
@@ -17,10 +17,41 @@ namespace STROOP.Controls.Map.Objects
         int _slotIndex;
         protected override MapGraphicsIconItem _iconGraphics { get; set; }
 
+        private BehaviorCriteria? _visibilityBehaviorCriteria = null;
+        public override MapTrackerVisibilityType VisibilityType
+        {
+            set
+            {
+                base.VisibilityType = value;
+                if (VisibilityType == MapTrackerVisibilityType.VisibleWhenThisBhvrIsLoaded)
+                {
+                    ObjectDataModel obj = DataModels.Objects[_slotIndex];
+                    _visibilityBehaviorCriteria = obj.BehaviorCriteria;
+                }
+            }
+        }
+
         public MapSm64Object(int slotIndex) : base("Object", null, null, false)
         {
             _iconGraphics = new MapGraphicsIconItem(null);
             _slotIndex = slotIndex;
+        }
+
+        private bool GetDisplayed()
+        {
+            ObjectDataModel obj = DataModels.Objects[_slotIndex];
+            switch (VisibilityType)
+            {
+                case MapTrackerVisibilityType.VisibleAlways:
+                    return true;
+                case MapTrackerVisibilityType.VisibleWhenLoaded:
+                    return obj.IsActive;
+                case MapTrackerVisibilityType.VisibleWhenThisBhvrIsLoaded:
+                    if (!_visibilityBehaviorCriteria.HasValue) return false;
+                    return obj.IsActive && obj.BehaviorCriteria == _visibilityBehaviorCriteria;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         public override void Update()
@@ -34,6 +65,7 @@ namespace STROOP.Controls.Map.Objects
             string slotLabel = Config.ObjectSlotsManager.GetDescriptiveSlotLabelFromAddress(address, true);
             Name = String.Format("[{0}] {1}", slotLabel, objectName);
             BackColor = ObjectUtilities.GetProcessingGroupColorForObjAddress(address);
+            Displayed = GetDisplayed();
 
             Bitmap currentGraphics = Config.ObjectAssociations.GetObjectMapImage(obj.BehaviorCriteria) as Bitmap;
             if (currentGraphics != BitmapImage)
