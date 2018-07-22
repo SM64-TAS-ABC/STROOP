f7f9b6a27effa0fcfcf86b74121f08cb5e7b8bb1
scob <scob@seas.upenn.edu>
2018-07-22 14:36:47 -0400

    create action form

---
+++ STROOP/Forms/ActionForm.cs
@@ -0,0 +1,16 @@
+﻿using STROOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace STROOP.Forms
+{
+    public partial class ActionForm : Form
+    {
+        public ActionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
