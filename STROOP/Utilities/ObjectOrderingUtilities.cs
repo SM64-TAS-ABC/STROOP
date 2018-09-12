17c52199739d31483182da72efc1268aa72b2dc9
scob <scob@seas.upenn.edu>
2018-09-12 17:54:38 -0400

    add object ordering utilities

---
+++ STROOP/Utilities/ObjectOrderingUtilities.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Xml;
+using System.Text.RegularExpressions;
+using STROOP.Structs;
+using STROOP.Structs.Configurations;
+using STROOP.Forms;
+
+namespace STROOP.Utilities
+{
+    public static class ObjectOrderingUtilities
+    {
+
+        private static List<List<uint>> GetProcessGroups()
+        {
+            List<List<uint>> processGroups = new List<List<uint>>();
+            int slotIndex = 0;
+
+            // processed slots
+            foreach (byte processGroupByte in ObjectSlotsConfig.ProcessingGroups)
+            {
+                uint processGroupStructAddress = ObjectSlotsConfig.FirstGroupingAddress + processGroupByte * ObjectSlotsConfig.ProcessGroupStructSize;
+                List<uint> processGroup = new List<uint>();
+                uint objAddress = Config.Stream.GetUInt32(processGroupStructAddress + ObjectConfig.ProcessedNextLinkOffset);
+                while ((objAddress != processGroupStructAddress && slotIndex < ObjectSlotsConfig.MaxSlots))
+                {
+                    processGroup.Add(objAddress);
+                    slotIndex++;
+                    objAddress = Config.Stream.GetUInt32(objAddress + ObjectConfig.ProcessedNextLinkOffset);
+                }
+                processGroups.Add(processGroup);
+            }
+
+            // vacant slots
+            {
+                List<uint> processGroup = new List<uint>();
+                uint objAddress = Config.Stream.GetUInt32(ObjectSlotsConfig.VactantPointerAddress);
+                while ((objAddress != 0 && slotIndex < ObjectSlotsConfig.MaxSlots))
+                {
+                    processGroup.Add(objAddress);
+                    slotIndex++;
+                    objAddress = Config.Stream.GetUInt32(objAddress + ObjectConfig.ProcessedNextLinkOffset);
+                }
+                processGroups.Add(processGroup);
+            }
+
+            return processGroups;
+        }
+
+        public static void Debug()
+        {
+            List<List<string>> labelLists = GetProcessGroups().ConvertAll(
+                processGroup => processGroup.ConvertAll(
+                    objAddress => Config.ObjectSlotsManager.GetDescriptiveSlotLabelFromAddress(objAddress, true)));
+            string output = String.Join("\r\n", labelLists.ConvertAll(labelList => String.Join(", ", labelList)));
+            InfoForm.ShowValue(output);
+        }
+
+    }
+}
--- STROOP/Utilities/TestUtilities.cs
+++ STROOP/Utilities/TestUtilities.cs
@@ -17,7 +17,7 @@ namespace STROOP.Utilities
     {
         public static void TestSomething()
         {
-
+            ObjectOrderingUtilities.Debug();
         }
 
         public static void AddGraphicsTriangleVerticesToTriangleTab()
