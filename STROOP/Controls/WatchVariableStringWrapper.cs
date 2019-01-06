520616952526e14601296c1f9169ef92ac1d6727
scob <scob@seas.upenn.edu>
2019-01-06 13:44:28 -0500

    added action description select value

--- STROOP/Controls/WatchVariableStringWrapper.cs
+++ STROOP/Controls/WatchVariableStringWrapper.cs
@@ -1,4 +1,5 @@
 ﻿using STROOP.Extensions;
+using STROOP.Forms;
 using STROOP.Managers;
 using STROOP.Structs;
 using STROOP.Structs.Configurations;
@@ -19,9 +20,29 @@ namespace STROOP.Controls
             WatchVariableControl watchVarControl)
             : base(watchVar, watchVarControl, DEFAULT_USE_CHECKBOX)
         {
-            
+            AddStringContextMenuStripItems(watchVar.SpecialType);
         }
-        
+
+        private void AddStringContextMenuStripItems(string specialType)
+        {
+            ToolStripMenuItem itemSelectValue = new ToolStripMenuItem("Select Value...");
+            bool addedClickAction = false;
+
+            switch (specialType)
+            {
+                case "ActionDescription":
+                    itemSelectValue.Click += (sender, e) => SelectionForm.ShowActionSelectionForm();
+                    addedClickAction = true;
+                    break;
+            }
+
+            if (addedClickAction)
+            {
+                _contextMenuStrip.AddToBeginningList(new ToolStripSeparator());
+                _contextMenuStrip.AddToBeginningList(itemSelectValue);
+            }
+        }
+
         protected override void HandleVerification(object value)
         {
             base.HandleVerification(value);
