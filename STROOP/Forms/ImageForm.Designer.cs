640f998e55bec35089bc10687cbb59a3be1132c3
scob <scob@seas.upenn.edu>
2019-02-09 16:13:59 -0500

    add border to image form

--- STROOP/Forms/ImageForm.Designer.cs
+++ STROOP/Forms/ImageForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageForm));
             this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.trackBarObjSlotSize = new System.Windows.Forms.TrackBar();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.trackBarObjSlotSize = new System.Windows.Forms.TrackBar();
             this.tableLayoutPanelMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarObjSlotSize)).BeginInit();
             this.SuspendLayout();
@@ -42,6 +42,7 @@
             this.tableLayoutPanelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelMain.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tableLayoutPanelMain.ColumnCount = 2;
             this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
@@ -57,39 +58,39 @@
             this.tableLayoutPanelMain.Size = new System.Drawing.Size(260, 150);
             this.tableLayoutPanelMain.TabIndex = 0;
             // 
+            // trackBarObjSlotSize
+            // 
+            this.trackBarObjSlotSize.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBarObjSlotSize.Location = new System.Drawing.Point(130, 1);
+            this.trackBarObjSlotSize.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBarObjSlotSize.Maximum = 100;
+            this.trackBarObjSlotSize.Minimum = 15;
+            this.trackBarObjSlotSize.Name = "trackBarObjSlotSize";
+            this.trackBarObjSlotSize.Size = new System.Drawing.Size(129, 30);
+            this.trackBarObjSlotSize.TabIndex = 4;
+            this.trackBarObjSlotSize.TickFrequency = 10;
+            this.trackBarObjSlotSize.Value = 100;
+            // 
             // panel1
             // 
             this.tableLayoutPanelMain.SetColumnSpan(this.panel1, 2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 30);
+            this.panel1.Location = new System.Drawing.Point(1, 32);
             this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(260, 120);
+            this.panel1.Size = new System.Drawing.Size(258, 117);
             this.panel1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(1, 1);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 30);
+            this.button1.Size = new System.Drawing.Size(128, 30);
             this.button1.TabIndex = 1;
             this.button1.Text = "Open Image";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // trackBarObjSlotSize
-            // 
-            this.trackBarObjSlotSize.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.trackBarObjSlotSize.Location = new System.Drawing.Point(130, 0);
-            this.trackBarObjSlotSize.Margin = new System.Windows.Forms.Padding(0);
-            this.trackBarObjSlotSize.Maximum = 100;
-            this.trackBarObjSlotSize.Minimum = 15;
-            this.trackBarObjSlotSize.Name = "trackBarObjSlotSize";
-            this.trackBarObjSlotSize.Size = new System.Drawing.Size(130, 30);
-            this.trackBarObjSlotSize.TabIndex = 4;
-            this.trackBarObjSlotSize.TickFrequency = 10;
-            this.trackBarObjSlotSize.Value = 100;
             // 
             // ImageForm
             // 
