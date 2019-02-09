13146962466430905a6180c5948e0f991a6273fb
scob <scob@seas.upenn.edu>
2019-02-09 16:12:37 -0500

    cleaned image form

--- STROOP/Forms/ImageForm.Designer.cs
+++ STROOP/Forms/ImageForm.Designer.cs
@@ -28,12 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectionForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageForm));
             this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
-            this.buttonSet = new System.Windows.Forms.Button();
-            this.textBoxSelect = new System.Windows.Forms.TextBox();
-            this.listBoxSelections = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBarObjSlotSize = new System.Windows.Forms.TrackBar();
             this.tableLayoutPanelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarObjSlotSize)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanelMain
@@ -41,54 +42,56 @@
             this.tableLayoutPanelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanelMain.ColumnCount = 1;
-            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelMain.Controls.Add(this.buttonSet, 0, 2);
-            this.tableLayoutPanelMain.Controls.Add(this.textBoxSelect, 0, 0);
-            this.tableLayoutPanelMain.Controls.Add(this.listBoxSelections, 0, 1);
+            this.tableLayoutPanelMain.ColumnCount = 2;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelMain.Controls.Add(this.trackBarObjSlotSize, 1, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanelMain.Controls.Add(this.button1, 0, 0);
             this.tableLayoutPanelMain.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanelMain.Margin = new System.Windows.Forms.Padding(0);
             this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
-            this.tableLayoutPanelMain.RowCount = 3;
+            this.tableLayoutPanelMain.RowCount = 2;
             this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanelMain.Size = new System.Drawing.Size(260, 150);
             this.tableLayoutPanelMain.TabIndex = 0;
             // 
-            // buttonSet
+            // panel1
             // 
-            this.buttonSet.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.buttonSet.Location = new System.Drawing.Point(3, 123);
-            this.buttonSet.Name = "buttonSet";
-            this.buttonSet.Size = new System.Drawing.Size(254, 24);
-            this.buttonSet.TabIndex = 0;
-            this.buttonSet.Text = "Set";
-            this.buttonSet.UseVisualStyleBackColor = true;
+            this.tableLayoutPanelMain.SetColumnSpan(this.panel1, 2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 30);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 120);
+            this.panel1.TabIndex = 0;
             // 
-            // textBoxSelect
+            // button1
             // 
-            this.textBoxSelect.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxSelect.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBoxSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSelect.Location = new System.Drawing.Point(3, 3);
-            this.textBoxSelect.Name = "textBoxSelect";
-            this.textBoxSelect.ReadOnly = true;
-            this.textBoxSelect.Size = new System.Drawing.Size(254, 13);
-            this.textBoxSelect.TabIndex = 17;
-            this.textBoxSelect.Text = "Select";
-            this.textBoxSelect.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open Image";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // listBoxSelections
+            // trackBarObjSlotSize
             // 
-            this.listBoxSelections.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBoxSelections.FormattingEnabled = true;
-            this.listBoxSelections.Location = new System.Drawing.Point(3, 33);
-            this.listBoxSelections.Name = "listBoxSelections";
-            this.listBoxSelections.Size = new System.Drawing.Size(254, 84);
-            this.listBoxSelections.TabIndex = 18;
+            this.trackBarObjSlotSize.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trackBarObjSlotSize.Location = new System.Drawing.Point(130, 0);
+            this.trackBarObjSlotSize.Margin = new System.Windows.Forms.Padding(0);
+            this.trackBarObjSlotSize.Maximum = 100;
+            this.trackBarObjSlotSize.Minimum = 15;
+            this.trackBarObjSlotSize.Name = "trackBarObjSlotSize";
+            this.trackBarObjSlotSize.Size = new System.Drawing.Size(130, 30);
+            this.trackBarObjSlotSize.TabIndex = 4;
+            this.trackBarObjSlotSize.TickFrequency = 10;
+            this.trackBarObjSlotSize.Value = 100;
             // 
-            // SelectionForm
+            // ImageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -98,11 +101,12 @@
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(10000, 10000);
             this.MinimizeBox = false;
-            this.Name = "SelectionForm";
+            this.Name = "ImageForm";
             this.ShowIcon = false;
-            this.Text = "Selection Form";
+            this.Text = "Image Form";
             this.tableLayoutPanelMain.ResumeLayout(false);
             this.tableLayoutPanelMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarObjSlotSize)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -110,8 +114,8 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
-        private System.Windows.Forms.Button buttonSet;
-        private System.Windows.Forms.TextBox textBoxSelect;
-        private System.Windows.Forms.ListBox listBoxSelections;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBarObjSlotSize;
     }
 }
