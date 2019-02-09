a8bcf4338146efb4134446f0cad648a50a84be66
scob <scob@seas.upenn.edu>
2019-02-09 00:26:22 -0500

    add unit coord list

--- STROOP/Utilities/TestUtilities.cs
+++ STROOP/Utilities/TestUtilities.cs
@@ -17,7 +17,7 @@ namespace STROOP.Utilities
     {
         public static void TestSomething()
         {
-            TestScuttlebugDrops();
+            TestSomething11();
         }
 
         public static void TestSomethingElse()
@@ -331,5 +331,76 @@ namespace STROOP.Utilities
             }
             InfoForm.ShowValue(output);
         }
+
+        public static void TestSomething11()
+        {
+
+        }
+
+        private static List<int[]> unitCoordinates = new List<int[]>()
+        {
+            new int[] {5,13},
+            new int[] {5,12},
+            new int[] {6,12},
+            new int[] {7,12},
+            new int[] {5,11},
+            new int[] {6,11},
+            new int[] {7,11},
+            new int[] {8,11},
+            new int[] {9,11},
+            new int[] {10,11},
+            new int[] {4,10},
+            new int[] {5,10},
+            new int[] {6,10},
+            new int[] {7,10},
+            new int[] {8,10},
+            new int[] {9,10},
+            new int[] {4,9},
+            new int[] {5,9},
+            new int[] {6,9},
+            new int[] {7,9},
+            new int[] {8,9},
+            new int[] {9,9},
+            new int[] {3,8},
+            new int[] {4,8},
+            new int[] {5,8},
+            new int[] {6,8},
+            new int[] {7,8},
+            new int[] {8,8},
+            new int[] {3,7},
+            new int[] {4,7},
+            new int[] {5,7},
+            new int[] {6,7},
+            new int[] {7,7},
+            new int[] {8,7},
+            new int[] {3,6},
+            new int[] {4,6},
+            new int[] {5,6},
+            new int[] {6,6},
+            new int[] {7,6},
+            new int[] {8,6},
+            new int[] {2,5},
+            new int[] {3,5},
+            new int[] {4,5},
+            new int[] {5,5},
+            new int[] {6,5},
+            new int[] {7,5},
+            new int[] {2,4},
+            new int[] {3,4},
+            new int[] {4,4},
+            new int[] {5,4},
+            new int[] {6,4},
+            new int[] {7,4},
+            new int[] {1,3},
+            new int[] {2,3},
+            new int[] {3,3},
+            new int[] {4,3},
+            new int[] {5,3},
+            new int[] {6,3},
+            new int[] {4,2},
+            new int[] {5,2},
+            new int[] {6,2},
+            new int[] {6,1},
+        };
     }
 } 
