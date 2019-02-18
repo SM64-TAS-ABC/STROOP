2b900607746b2927ed2c738bd7ca3db82601062d
scob <scob@seas.upenn.edu>
2019-02-18 18:11:14 -0500

    add controls to triplet controller

--- STROOP/Forms/VariableTripletControllerForm.Designer.cs
+++ STROOP/Forms/VariableTripletControllerForm.Designer.cs
@@ -29,35 +29,208 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VariablePopOutForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VariableTripletControllerForm));
             this._watchVariablePanel = new STROOP.Controls.WatchVariableFlowLayoutPanel();
+            this.groupBoxVar = new System.Windows.Forms.GroupBox();
+            this.checkBoxVarRelative = new System.Windows.Forms.CheckBox();
+            this.textBoxVarY = new STROOP.BetterTextbox();
+            this.buttonVarYp = new System.Windows.Forms.Button();
+            this.buttonVarYn = new System.Windows.Forms.Button();
+            this.buttonVarXpZp = new System.Windows.Forms.Button();
+            this.textBoxVarXZ = new STROOP.BetterTextbox();
+            this.buttonVarXp = new System.Windows.Forms.Button();
+            this.buttonVarXpZn = new System.Windows.Forms.Button();
+            this.buttonVarZn = new System.Windows.Forms.Button();
+            this.buttonVarZp = new System.Windows.Forms.Button();
+            this.buttonVarXnZp = new System.Windows.Forms.Button();
+            this.buttonVarXn = new System.Windows.Forms.Button();
+            this.buttonVarXnZn = new System.Windows.Forms.Button();
+            this._watchVariablePanel.SuspendLayout();
+            this.groupBoxVar.SuspendLayout();
             this.SuspendLayout();
             // 
             // _watchVariablePanel
             // 
             this._watchVariablePanel.AutoScroll = true;
+            this._watchVariablePanel.Controls.Add(this.groupBoxVar);
             this._watchVariablePanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this._watchVariablePanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this._watchVariablePanel.Location = new System.Drawing.Point(0, 0);
             this._watchVariablePanel.Margin = new System.Windows.Forms.Padding(0);
             this._watchVariablePanel.Name = "_watchVariablePanel";
             this._watchVariablePanel.Padding = new System.Windows.Forms.Padding(2);
-            this._watchVariablePanel.Size = new System.Drawing.Size(284, 174);
+            this._watchVariablePanel.Size = new System.Drawing.Size(195, 155);
             this._watchVariablePanel.TabIndex = 3;
             // 
-            // VariablePopOutForm
+            // groupBoxVar
+            // 
+            this.groupBoxVar.Controls.Add(this.checkBoxVarRelative);
+            this.groupBoxVar.Controls.Add(this.textBoxVarY);
+            this.groupBoxVar.Controls.Add(this.buttonVarYp);
+            this.groupBoxVar.Controls.Add(this.buttonVarYn);
+            this.groupBoxVar.Controls.Add(this.buttonVarXpZp);
+            this.groupBoxVar.Controls.Add(this.textBoxVarXZ);
+            this.groupBoxVar.Controls.Add(this.buttonVarXp);
+            this.groupBoxVar.Controls.Add(this.buttonVarXpZn);
+            this.groupBoxVar.Controls.Add(this.buttonVarZn);
+            this.groupBoxVar.Controls.Add(this.buttonVarZp);
+            this.groupBoxVar.Controls.Add(this.buttonVarXnZp);
+            this.groupBoxVar.Controls.Add(this.buttonVarXn);
+            this.groupBoxVar.Controls.Add(this.buttonVarXnZn);
+            this.groupBoxVar.Location = new System.Drawing.Point(5, 5);
+            this.groupBoxVar.Name = "groupBoxVar";
+            this.groupBoxVar.Size = new System.Drawing.Size(185, 146);
+            this.groupBoxVar.TabIndex = 29;
+            this.groupBoxVar.TabStop = false;
+            this.groupBoxVar.Text = "Position";
+            // 
+            // checkBoxVarRelative
+            // 
+            this.checkBoxVarRelative.AutoSize = true;
+            this.checkBoxVarRelative.Location = new System.Drawing.Point(120, 0);
+            this.checkBoxVarRelative.Name = "checkBoxVarRelative";
+            this.checkBoxVarRelative.Size = new System.Drawing.Size(65, 17);
+            this.checkBoxVarRelative.TabIndex = 37;
+            this.checkBoxVarRelative.Text = "Relative";
+            this.checkBoxVarRelative.UseVisualStyleBackColor = true;
+            // 
+            // textBoxVarY
+            // 
+            this.textBoxVarY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxVarY.Location = new System.Drawing.Point(140, 70);
+            this.textBoxVarY.Name = "textBoxVarY";
+            this.textBoxVarY.Size = new System.Drawing.Size(42, 20);
+            this.textBoxVarY.TabIndex = 33;
+            this.textBoxVarY.Text = "100";
+            this.textBoxVarY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonVarYp
+            // 
+            this.buttonVarYp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonVarYp.Location = new System.Drawing.Point(140, 16);
+            this.buttonVarYp.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarYp.Name = "buttonVarYp";
+            this.buttonVarYp.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarYp.TabIndex = 35;
+            this.buttonVarYp.Text = "Y+";
+            this.buttonVarYp.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarYn
+            // 
+            this.buttonVarYn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonVarYn.Location = new System.Drawing.Point(140, 100);
+            this.buttonVarYn.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarYn.Name = "buttonVarYn";
+            this.buttonVarYn.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarYn.TabIndex = 34;
+            this.buttonVarYn.Text = "Y-";
+            this.buttonVarYn.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarXpZp
+            // 
+            this.buttonVarXpZp.Location = new System.Drawing.Point(87, 100);
+            this.buttonVarXpZp.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarXpZp.Name = "buttonVarXpZp";
+            this.buttonVarXpZp.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarXpZp.TabIndex = 32;
+            this.buttonVarXpZp.Text = "X+Z+";
+            this.buttonVarXpZp.UseVisualStyleBackColor = true;
+            // 
+            // textBoxVarXZ
+            // 
+            this.textBoxVarXZ.Location = new System.Drawing.Point(45, 70);
+            this.textBoxVarXZ.Name = "textBoxVarXZ";
+            this.textBoxVarXZ.Size = new System.Drawing.Size(42, 20);
+            this.textBoxVarXZ.TabIndex = 27;
+            this.textBoxVarXZ.Text = "100";
+            this.textBoxVarXZ.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonVarXp
+            // 
+            this.buttonVarXp.Location = new System.Drawing.Point(87, 58);
+            this.buttonVarXp.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarXp.Name = "buttonVarXp";
+            this.buttonVarXp.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarXp.TabIndex = 31;
+            this.buttonVarXp.Text = "X+";
+            this.buttonVarXp.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarXpZn
+            // 
+            this.buttonVarXpZn.Location = new System.Drawing.Point(87, 16);
+            this.buttonVarXpZn.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarXpZn.Name = "buttonVarXpZn";
+            this.buttonVarXpZn.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarXpZn.TabIndex = 30;
+            this.buttonVarXpZn.Text = "X+Z-";
+            this.buttonVarXpZn.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarZn
+            // 
+            this.buttonVarZn.Location = new System.Drawing.Point(45, 16);
+            this.buttonVarZn.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarZn.Name = "buttonVarZn";
+            this.buttonVarZn.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarZn.TabIndex = 29;
+            this.buttonVarZn.Text = "Z-";
+            this.buttonVarZn.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarZp
+            // 
+            this.buttonVarZp.Location = new System.Drawing.Point(45, 100);
+            this.buttonVarZp.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarZp.Name = "buttonVarZp";
+            this.buttonVarZp.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarZp.TabIndex = 28;
+            this.buttonVarZp.Text = "Z+";
+            this.buttonVarZp.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarXnZp
+            // 
+            this.buttonVarXnZp.Location = new System.Drawing.Point(3, 100);
+            this.buttonVarXnZp.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarXnZp.Name = "buttonVarXnZp";
+            this.buttonVarXnZp.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarXnZp.TabIndex = 27;
+            this.buttonVarXnZp.Text = "X-Z+";
+            this.buttonVarXnZp.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarXn
+            // 
+            this.buttonVarXn.Location = new System.Drawing.Point(3, 58);
+            this.buttonVarXn.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarXn.Name = "buttonVarXn";
+            this.buttonVarXn.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarXn.TabIndex = 26;
+            this.buttonVarXn.Text = "X-";
+            this.buttonVarXn.UseVisualStyleBackColor = true;
+            // 
+            // buttonVarXnZn
+            // 
+            this.buttonVarXnZn.Location = new System.Drawing.Point(3, 16);
+            this.buttonVarXnZn.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonVarXnZn.Name = "buttonVarXnZn";
+            this.buttonVarXnZn.Size = new System.Drawing.Size(42, 42);
+            this.buttonVarXnZn.TabIndex = 25;
+            this.buttonVarXnZn.Text = "X-Z-";
+            this.buttonVarXnZn.UseVisualStyleBackColor = true;
+            // 
+            // VariableTripletControllerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 174);
+            this.ClientSize = new System.Drawing.Size(195, 155);
             this.Controls.Add(this._watchVariablePanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(10000, 10000);
             this.MinimizeBox = false;
-            this.Name = "VariablePopOutForm";
+            this.Name = "VariableTripletControllerForm";
             this.ShowIcon = false;
-            this.Text = "Variables";
+            this.Text = "Triplet Controller";
+            this._watchVariablePanel.ResumeLayout(false);
+            this.groupBoxVar.ResumeLayout(false);
+            this.groupBoxVar.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -65,5 +238,19 @@
         #endregion
 
         private Controls.WatchVariableFlowLayoutPanel _watchVariablePanel;
+        private System.Windows.Forms.GroupBox groupBoxVar;
+        private System.Windows.Forms.CheckBox checkBoxVarRelative;
+        private BetterTextbox textBoxVarY;
+        private System.Windows.Forms.Button buttonVarYp;
+        private System.Windows.Forms.Button buttonVarYn;
+        private System.Windows.Forms.Button buttonVarXpZp;
+        private BetterTextbox textBoxVarXZ;
+        private System.Windows.Forms.Button buttonVarXp;
+        private System.Windows.Forms.Button buttonVarXpZn;
+        private System.Windows.Forms.Button buttonVarZn;
+        private System.Windows.Forms.Button buttonVarZp;
+        private System.Windows.Forms.Button buttonVarXnZp;
+        private System.Windows.Forms.Button buttonVarXn;
+        private System.Windows.Forms.Button buttonVarXnZn;
     }
 }
