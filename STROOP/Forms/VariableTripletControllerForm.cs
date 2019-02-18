0c6511e7eea28e0df0741ad265f2193c2fb78c31
scob <scob@seas.upenn.edu>
2019-02-18 18:17:22 -0500

    hook up controls in triplet controller

--- STROOP/Forms/VariableTripletControllerForm.cs
+++ STROOP/Forms/VariableTripletControllerForm.cs
@@ -1,6 +1,7 @@
 ﻿using STROOP.Controls;
 using STROOP.Structs;
 using STROOP.Structs.Configurations;
+using STROOP.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
@@ -17,7 +18,31 @@ namespace STROOP.Forms
 
         public void Initialize(List<WatchVariableControl> controls)
         {
-
+            ControlUtilities.InitializeThreeDimensionController(
+                CoordinateSystem.Euler,
+                true,
+                groupBoxVar,
+                groupBoxVar.Controls["buttonVarXn"] as Button,
+                groupBoxVar.Controls["buttonVarXp"] as Button,
+                groupBoxVar.Controls["buttonVarZn"] as Button,
+                groupBoxVar.Controls["buttonVarZp"] as Button,
+                groupBoxVar.Controls["buttonVarXnZn"] as Button,
+                groupBoxVar.Controls["buttonVarXnZp"] as Button,
+                groupBoxVar.Controls["buttonVarXpZn"] as Button,
+                groupBoxVar.Controls["buttonVarXpZp"] as Button,
+                groupBoxVar.Controls["buttonVarYp"] as Button,
+                groupBoxVar.Controls["buttonVarYn"] as Button,
+                groupBoxVar.Controls["textBoxVarXZ"] as TextBox,
+                groupBoxVar.Controls["textBoxVarY"] as TextBox,
+                groupBoxVar.Controls["checkBoxVarRelative"] as CheckBox,
+                (float hOffset, float vOffset, float nOffset, bool useRelative) =>
+                {
+                    ButtonUtilities.TranslateMario(
+                        hOffset,
+                        nOffset,
+                        -1 * vOffset,
+                        useRelative);
+                });
         }
 
         public void ShowForm()
