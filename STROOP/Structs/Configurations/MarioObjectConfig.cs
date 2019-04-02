8f2cd15da21e8b94213ce5bc3a1ec6d3ab2e66ff
scob <scob@seas.upenn.edu>
2019-04-01 22:37:20 -0400

    fix reference to mario obj pointer

--- STROOP/Structs/Configurations/MarioObjectConfig.cs
+++ STROOP/Structs/Configurations/MarioObjectConfig.cs
@@ -9,9 +9,10 @@ namespace STROOP.Structs
 {
     public static class MarioObjectConfig
     {
-        public static uint PointerAddress { get => RomVersionConfig.Switch(PointerAddressUS, PointerAddressJP); }
+        public static uint PointerAddress { get => RomVersionConfig.Switch(PointerAddressUS, PointerAddressJP, PointerAddressSH); }
         public static readonly uint PointerAddressUS = 0x80361158;
         public static readonly uint PointerAddressJP = 0x8035FDE8;
+        public static readonly uint PointerAddressSH = 0x80343318;
 
         public static readonly uint AnimationOffset = 0x38;
         public static readonly uint AnimationTimerOffset = 0x40;
