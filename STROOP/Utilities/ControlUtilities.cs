5d69df86ace9440d67116a07e1848e7ff733aef5
scob <scob@seas.upenn.edu>
2019-04-13 14:05:46 -0400

    simplfy inversion context menu strip

--- STROOP/Utilities/ControlUtilities.cs
+++ STROOP/Utilities/ControlUtilities.cs
@@ -360,27 +360,21 @@ namespace STROOP.Utilities
             Point leftPoint = new Point(buttonLeft.Location.X, buttonLeft.Location.Y);
             Point rightPoint = new Point(buttonRight.Location.X, buttonRight.Location.Y);
 
-            ToolStripMenuItem itemNormal = new ToolStripMenuItem("Normal");
             ToolStripMenuItem itemInverted = new ToolStripMenuItem("Inverted");
 
             Action<bool> SetOrientation = (bool inverted) =>
             {
-                itemNormal.Checked = !inverted;
                 itemInverted.Checked = inverted;
                 buttonLeft.Location = inverted ? rightPoint : leftPoint;
                 buttonRight.Location = inverted ? leftPoint : rightPoint;
             };
 
-            itemNormal.Click += (sender, e) => SetOrientation(false);
-            itemInverted.Click += (sender, e) => SetOrientation(true);
+            itemInverted.Click += (sender, e) => SetOrientation(!itemInverted.Checked);
 
             ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
-            contextMenuStrip.Items.Add(itemNormal);
             contextMenuStrip.Items.Add(itemInverted);
             buttonLeft.ContextMenuStrip = contextMenuStrip;
             buttonRight.ContextMenuStrip = contextMenuStrip;
-
-            itemNormal.Checked = true;
         }
 
         public static void AddContextMenuStripFunctions(
