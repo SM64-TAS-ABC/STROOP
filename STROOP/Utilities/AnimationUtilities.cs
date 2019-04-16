3cd1987a6b5a4ed75043601c5b454b06e646c988
scob <scob@seas.upenn.edu>
2019-04-15 23:30:27 -0400

    implement replacing

--- STROOP/Utilities/AnimationUtilities.cs
+++ STROOP/Utilities/AnimationUtilities.cs
@@ -11,14 +11,28 @@ namespace STROOP.Structs
 {
     public static class AnimationUtilities
     {
+        private static Dictionary<int, (int, int)> _dictionary;
+
         static AnimationUtilities()
         {
-
+            _dictionary = new Dictionary<int, (int, int)>();
+            for (int i = 0; i <= 208; i++)
+            {
+                uint address1 = 0x80064040 + 0x8 + (uint)i * 0x8 + 0x0;
+                uint address2 = 0x80064040 + 0x8 + (uint)i * 0x8 + 0x4;
+                int value1 = Config.Stream.GetInt32(address1);
+                int value2 = Config.Stream.GetInt32(address2);
+                _dictionary[i] = (value1, value2);
+            }
         }
 
         public static void ReplaceAnimation(int animationToBeReplaced, int animationToReplaceIt)
         {
-
+            (int value1, int value2) = _dictionary[animationToReplaceIt];
+            uint address1 = 0x80064040 + 0x8 + (uint)animationToBeReplaced * 0x8 + 0x0;
+            uint address2 = 0x80064040 + 0x8 + (uint)animationToBeReplaced * 0x8 + 0x4;
+            Config.Stream.SetValue(value1, address1);
+            Config.Stream.SetValue(value2, address2);
         }
     }
 }
