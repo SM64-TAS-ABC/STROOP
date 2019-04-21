da1f9320bcd5d9c4f65dad415128af7730aeebae
scob <scob@seas.upenn.edu>
2019-04-21 15:26:23 -0400

    rename music data

--- STROOP/Managers/SoundManager.cs
+++ STROOP/Managers/SoundManager.cs
@@ -27,6 +27,22 @@ namespace STROOP.Managers
             ListBox listBoxSoundSoundEffect = splitContainerSoundSoundEffect.Panel1.Controls["listBoxSoundSoundEffect"] as ListBox;
             TextBox textBoxSoundSoundEffect = splitContainerSoundSoundEffect.Panel2.Controls["textBoxSoundSoundEffect"] as TextBox;
             Button buttonSoundPlaySoundEffect = splitContainerSoundSoundEffect.Panel2.Controls["buttonSoundPlaySoundEffect"] as Button;
+
+            TableConfig.MusicData.GetMusicEntryList().ForEach(musicEntry => listBoxSoundMusic.Items.Add(musicEntry));
+            listBoxSoundMusic.Click += (sender, e) =>
+            {
+                MusicEntry musicEntry = listBoxSoundMusic.SelectedItem as MusicEntry;
+                textBoxSoundMusic.Text = musicEntry.Index.ToString();
+            };
+            buttonSoundPlayMusic.Click += (sender, e) =>
+            {
+                int? musicIndexNullable = ParsingUtilities.ParseIntNullable(textBoxSoundMusic.Text);
+                if (musicIndexNullable == null) return;
+                int musicIndex = musicIndexNullable.Value;
+                //if (musicIndex < 0 || musicIndex > 34) return;
+                uint setMusic = RomVersionConfig.Switch(0x80320544, 0x8031F690);
+                InGameFunctionCall.WriteInGameFunctionCall(setMusic, 0, (uint)musicIndex, 0);
+            };
         }
 
         public void Update(bool updateView)
