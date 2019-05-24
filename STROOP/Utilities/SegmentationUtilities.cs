26d7148b44070ea89b994fbaae73e443783ebc49
scob <scob@seas.upenn.edu>
2019-05-24 01:39:19 -0400

    fix segmentation table addressing

--- STROOP/Utilities/SegmentationUtilities.cs
+++ STROOP/Utilities/SegmentationUtilities.cs
@@ -12,8 +12,8 @@ namespace STROOP.Structs
     public static class SegmentationUtilities
     {
         public static uint SegmentationTableAddress { get => RomVersionConfig.Switch(SegmentationTableAddressUS, SegmentationTableAddressJP); }
-        public static uint SegmentationTableAddressUS = 0x33B400;
-        public static uint SegmentationTableAddressJP = 0x33A090;
+        public static uint SegmentationTableAddressUS = 0x8033B400;
+        public static uint SegmentationTableAddressJP = 0x8033A090;
 
         // A segmented address is 4 bytes. The first byte contains the index of the segment in the segment table, the 
         // other 3 bytes are the offset from the segment. Segmented addresses are used for locating object behavior scripts, 
@@ -22,7 +22,7 @@ namespace STROOP.Structs
         {
             var offset = segmentedAddress & 0xFFFFFF;
             var segment = (segmentedAddress >> 24);
-            return offset + Config.Stream.GetUInt32(4 * segment + SegmentationTableAddress);
+            return offset + Config.Stream.GetUInt32(4 * segment + (SegmentationTableAddress & 0xFFFFFF));
         }
     }
 }
