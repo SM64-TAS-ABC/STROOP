930c0f64690f44013f43e7ba820fe7e32c0172f2
scob <scob@seas.upenn.edu>
2019-05-29 02:56:54 -0400

    add easing methods

--- STROOP/Utilities/EasingUtilities.cs
+++ STROOP/Utilities/EasingUtilities.cs
@@ -12,5 +12,20 @@ namespace STROOP.Utilities
         {
             return proportion;
         }
+
+        public static double EaseIn(double power, double t)
+        {
+            return Math.Pow(t, power);
+        }
+
+        public static double EaseOut (double power, double t)
+        {
+            return 1 - Math.Abs(Math.Pow(t - 1, power));
+        }
+
+        public static double EaseInOut (double power, double t)
+        {
+            return t < 0.5 ? EaseIn(power, t * 2) / 2 : EaseOut(power, t * 2 - 1) / 2 + 0.5;
+        }
     }
 }
