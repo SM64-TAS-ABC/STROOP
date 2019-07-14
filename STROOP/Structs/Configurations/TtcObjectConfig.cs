cfbcc1dfc6d05c7301b332e84847ca08ec2fa31d
scob <scob@seas.upenn.edu>
2019-07-14 15:02:32 -0400

    add ttc object config

---
+++ STROOP/Structs/Configurations/TtcObjectConfig.cs
@@ -0,0 +1,31 @@
+﻿using STROOP.Structs.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STROOP.Structs
+{
+    public static class TtcObjectConfig
+    {
+        public static readonly uint TtcRotatingBlockAddressUS = 0x8033D488;
+        public static readonly uint TtcRotatingTriangularPrismAddressUS = 0x8033E2C8;
+        public static readonly uint TtcPendulumAddressUS = 0x8033E788;
+        public static readonly uint TtcTreadmill1AddressUS = 0x8033F108;
+        public static readonly uint TtcPusherAddressUS = 0x8033FCE8;
+        public static readonly uint TtcCog1AddressUS = 0x80341968;
+        public static readonly uint TtcSpinningTriangleAddressUS = 0x80342548;
+        public static readonly uint TtcPitBlockAddressUS = 0x80342A08;
+        public static readonly uint TtcHandAddressUS = 0x80342C68;
+        public static readonly uint TtcSpinnerAddressUS = 0x80343128;
+        public static readonly uint TtcWheelAddressUS = 0x80345268;
+        public static readonly uint TtcElevatorAddressUS = 0x80347608;
+        public static readonly uint TtcCog2AddressUS = 0x8034B3C8;
+        public static readonly uint TtcTreadmill2AddressUS = 0x8034D508;
+        public static readonly uint TtcThwompAddressUS = 0x8034E808;
+        public static readonly uint TtcAmp1AddressUS = 0x80347AC8;
+        public static readonly uint TtcAmp2AddressUS = 0x8034A328;
+
+    }
+}
