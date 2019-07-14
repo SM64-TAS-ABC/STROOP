42fae64af99262e3c285f60c19252b0a558b0a17
scob <scob@seas.upenn.edu>
2019-07-14 15:10:02 -0400

    add dummy jp addresses

--- STROOP/Structs/Configurations/TtcObjectConfig.cs
+++ STROOP/Structs/Configurations/TtcObjectConfig.cs
@@ -10,22 +10,54 @@ namespace STROOP.Structs
     public static class TtcObjectConfig
     {
         public static readonly uint TtcRotatingBlockAddressUS = 0x8033D488;
-        public static readonly uint TtcRotatingTriangularPrismAddressUS = 0x8033E2C8;
-        public static readonly uint TtcPendulumAddressUS = 0x8033E788;
-        public static readonly uint TtcTreadmill1AddressUS = 0x8033F108;
-        public static readonly uint TtcPusherAddressUS = 0x8033FCE8;
-        public static readonly uint TtcCog1AddressUS = 0x80341968;
-        public static readonly uint TtcSpinningTriangleAddressUS = 0x80342548;
-        public static readonly uint TtcPitBlockAddressUS = 0x80342A08;
-        public static readonly uint TtcHandAddressUS = 0x80342C68;
-        public static readonly uint TtcSpinnerAddressUS = 0x80343128;
-        public static readonly uint TtcWheelAddressUS = 0x80345268;
-        public static readonly uint TtcElevatorAddressUS = 0x80347608;
-        public static readonly uint TtcCog2AddressUS = 0x8034B3C8;
-        public static readonly uint TtcTreadmill2AddressUS = 0x8034D508;
-        public static readonly uint TtcThwompAddressUS = 0x8034E808;
-        public static readonly uint TtcAmp1AddressUS = 0x80347AC8;
-        public static readonly uint TtcAmp2AddressUS = 0x8034A328;
+        public static readonly uint TtcRotatingBlockAddressJP = 0x00000000;
 
+        public static readonly uint TtcRotatingTriangularPrismAddressUS = 0x8033E2C8;
+        public static readonly uint TtcRotatingTriangularPrismAddressJP = 0x00000000;
+
+        public static readonly uint TtcPendulumAddressUS = 0x8033E788;
+        public static readonly uint TtcPendulumAddressJP = 0x00000000;
+
+        public static readonly uint TtcTreadmill1AddressUS = 0x8033F108;
+        public static readonly uint TtcTreadmill1AddressJP = 0x00000000;
+
+        public static readonly uint TtcPusherAddressUS = 0x8033FCE8;
+        public static readonly uint TtcPusherAddressJP = 0x00000000;
+
+        public static readonly uint TtcCog1AddressUS = 0x80341968;
+        public static readonly uint TtcCog1AddressJP = 0x00000000;
+
+        public static readonly uint TtcSpinningTriangleAddressUS = 0x80342548;
+        public static readonly uint TtcSpinningTriangleAddressJP = 0x00000000;
+
+        public static readonly uint TtcPitBlockAddressUS = 0x80342A08;
+        public static readonly uint TtcPitBlockAddressJP = 0x00000000;
+
+        public static readonly uint TtcHandAddressUS = 0x80342C68;
+        public static readonly uint TtcHandAddressJP = 0x00000000;
+
+        public static readonly uint TtcSpinnerAddressUS = 0x80343128;
+        public static readonly uint TtcSpinnerAddressJP = 0x00000000;
+
+        public static readonly uint TtcWheelAddressUS = 0x80345268;
+        public static readonly uint TtcWheelAddressJP = 0x00000000;
+
+        public static readonly uint TtcElevatorAddressUS = 0x80347608;
+        public static readonly uint TtcElevatorAddressJP = 0x00000000;
+
+        public static readonly uint TtcCog2AddressUS = 0x8034B3C8;
+        public static readonly uint TtcCog2AddressJP = 0x00000000;
+
+        public static readonly uint TtcTreadmill2AddressUS = 0x8034D508;
+        public static readonly uint TtcTreadmill2AddressJP = 0x00000000;
+
+        public static readonly uint TtcThwompAddressUS = 0x8034E808;
+        public static readonly uint TtcThwompAddressJP = 0x00000000;
+
+        public static readonly uint TtcAmp1AddressUS = 0x80347AC8;
+        public static readonly uint TtcAmp1AddressJP = 0x00000000;
+
+        public static readonly uint TtcAmp2AddressUS = 0x8034A328;
+        public static readonly uint TtcAmp2AddressJP = 0x00000000;
     }
 }
