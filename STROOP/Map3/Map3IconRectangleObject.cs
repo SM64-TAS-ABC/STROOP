7e47b06405f0872be0c3fb3c27378f46e808dc3b
scob <scob@seas.upenn.edu>
2019-09-21 13:51:16 -0400

    partially implement map obj

---
+++ STROOP/Map3/Map3IconRectangleObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using STROOP.Controls.Map;
+using OpenTK.Graphics.OpenGL;
+using STROOP.Utilities;
+using STROOP.Structs.Configurations;
+using STROOP.Structs;
+using OpenTK;
+
+namespace STROOP.Map3
+{
+    public abstract class Map3IconRectangleObject : Map3IconObject
+    {
+        public Map3IconRectangleObject(Map3Graphics graphics, Func<Image> imageFunction)
+            : base(graphics, imageFunction)
+        {
+        }
+
+        public override void DrawOnControl()
+        {
+            UpdateImage();
+
+            (PointF location, SizeF size) = GetDimensions();
+        }
+
+        protected abstract (PointF location, SizeF size) GetDimensions();
+    }
+}
