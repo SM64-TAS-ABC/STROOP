cdcde7d1a9a6dabff63d8f213f8a978d0ff06f01
scob <scob@seas.upenn.edu>
2019-09-22 01:44:30 -0400

    update data labels in map3 tab

--- STROOP/Managers/Map3Manager.cs
+++ STROOP/Managers/Map3Manager.cs
@@ -69,8 +69,40 @@ namespace STROOP.Managers
             if (!updateView) return;
             if (!_isLoaded) return;
 
-            // Update gui by drawing images (invokes _mapGraphics.OnPaint())
+            UpdateData();
             Config.Map3Graphics.Control.Invalidate();
+        }
+
+        private void UpdateData()
+        {
+            float marioX = Config.Stream.GetSingle(MarioConfig.StructAddress + MarioConfig.XOffset);
+            float marioY = Config.Stream.GetSingle(MarioConfig.StructAddress + MarioConfig.YOffset);
+            float marioZ = Config.Stream.GetSingle(MarioConfig.StructAddress + MarioConfig.ZOffset);
+
+            int puX = PuUtilities.GetPuIndex(marioX);
+            int puY = PuUtilities.GetPuIndex(marioY);
+            int puZ = PuUtilities.GetPuIndex(marioZ);
+
+            double qpuX = puX / 4.0;
+            double qpuY = puY / 4.0;
+            double qpuZ = puZ / 4.0;
+
+            uint floorTriangleAddress = Config.Stream.GetUInt32(MarioConfig.StructAddress + MarioConfig.FloorTriangleOffset);
+            float? yNorm = floorTriangleAddress == 0 ? (float?)null : Config.Stream.GetSingle(floorTriangleAddress + TriangleOffsetsConfig.NormY);
+
+            byte level = Config.Stream.GetByte(MiscConfig.LevelAddress);
+            byte area = Config.Stream.GetByte(MiscConfig.AreaAddress);
+            ushort loadingPoint = Config.Stream.GetUInt16(MiscConfig.LoadingPointAddress);
+            ushort missionLayout = Config.Stream.GetUInt16(MiscConfig.MissionAddress);
+
+            MapLayout map = Config.MapAssociations.GetBestMap();
+
+            Config.Map3Gui.labelMap3DataMapName.Text = map.Name;
+            Config.Map3Gui.labelMap3DataMapSubName.Text = (map.SubName != null) ? map.SubName : "";
+            Config.Map3Gui.labelMap3DataPuCoordinateValues.Text = string.Format("[{0}:{1}:{2}]", puX, puY, puZ);
+            Config.Map3Gui.labelMap3DataQpuCoordinateValues.Text = string.Format("[{0}:{1}:{2}]", qpuX, qpuY, qpuZ);
+            Config.Map3Gui.labelMap3DataId.Text = string.Format("[{0}:{1}:{2}:{3}]", level, area, loadingPoint, missionLayout);
+            Config.Map3Gui.labelMap3DataYNorm.Text = yNorm?.ToString() ?? "(none)";
         }
     }
 }
