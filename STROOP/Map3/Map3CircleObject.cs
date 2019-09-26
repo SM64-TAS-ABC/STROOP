412ee7d9d957f21786e43edd660b0b86e45c2cc2
scob <scob@seas.upenn.edu>
2019-09-25 21:09:13 -0400

    add Map3ResizableCircleObject

---
+++ STROOP/Map3/Map3CircleObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using STROOP.Controls.Map;
+using OpenTK.Graphics.OpenGL;
+using STROOP.Utilities;
+using STROOP.Structs.Configurations;
+using STROOP.Structs;
+using OpenTK;
+using System.Drawing.Imaging;
+
+namespace STROOP.Map3
+{
+    public abstract class Map3CircleObject : Map3Object
+    {
+        private readonly static int NUM_POINTS = 8;
+
+        public Map3CircleObject()
+            : base()
+        {
+        }
+
+        public override void DrawOnControl()
+        {
+            (float centerX, float centerZ, float radius) = GetDimensions();
+            (float controlCenterX, float controlCenterZ) = Map3Utilities.ConvertCoordsForControl(centerX, centerZ);
+            float controlRadius = radius * Config.Map3Graphics.MapViewScaleValue;
+            List <(float pointX, float pointZ)> controlPoints = Enumerable.Range(0, NUM_POINTS).ToList()
+                .ConvertAll(index => (index / (float)NUM_POINTS) * 65536)
+                .ConvertAll(angle => ((float, float))MoreMath.AddVectorToPoint(controlRadius, angle, controlCenterX, controlCenterZ));
+
+            GL.BindTexture(TextureTarget.Texture2D, -1);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Color4(Color.R, Color.G, Color.B, OpacityByte);
+            GL.Begin(PrimitiveType.TriangleFan);
+            GL.Vertex2(controlCenterX, controlCenterZ);
+            foreach ((float x, float z) in controlPoints)
+            {
+                GL.Vertex2(x, z);
+            }
+            GL.End();
+            GL.Color4(1, 1, 1, 1.0f);
+        }
+
+        protected abstract (float centerX, float centerZ, float radius) GetDimensions();
+    }
+}
---
+++ STROOP/Map3/Map3ResizableCircleObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using STROOP.Controls.Map;
+using OpenTK.Graphics.OpenGL;
+using STROOP.Utilities;
+using STROOP.Structs.Configurations;
+using STROOP.Structs;
+using OpenTK;
+using System.Drawing.Imaging;
+
+namespace STROOP.Map3
+{
+    public abstract class Map3ResizableCircleObject : Map3CircleObject
+    {
+        private readonly PositionAngle _posAngle;
+
+        public Map3ResizableCircleObject(PositionAngle posAngle)
+            : base()
+        {
+            _posAngle = posAngle;
+        }
+
+        protected override (float centerX, float centerZ, float radius) GetDimensions()
+        {
+            return ((float)_posAngle.X, (float)_posAngle.Z, Size);
+        }
+    }
+}
