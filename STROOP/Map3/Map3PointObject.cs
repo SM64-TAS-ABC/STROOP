9657155e748d8c7f214c71467e7bed357a13ce65
scob <scob@seas.upenn.edu>
2019-09-26 22:22:33 -0400

    add point/self map objs

---
+++ STROOP/Map3/Map3PointObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using STROOP.Controls.Map;
+using OpenTK.Graphics.OpenGL;
+using STROOP.Utilities;
+using STROOP.Structs.Configurations;
+using STROOP.Structs;
+using OpenTK;
+
+namespace STROOP.Map3
+{
+    public class Map3PointObject : Map3IconPointObject
+    {
+        public Map3PointObject()
+            : base()
+        {
+        }
+
+        public override Image GetImage()
+        {
+            return Config.ObjectAssociations.MarioMapImage;
+        }
+
+        public override PositionAngle GetPositionAngle()
+        {
+            return SpecialConfig.PointPA;
+        }
+
+        public override string GetName()
+        {
+            return "Point";
+        }
+
+        public override float GetY()
+        {
+            return (float)SpecialConfig.PointY;
+        }
+    }
+}
---
+++ STROOP/Map3/Map3SelfObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using STROOP.Controls.Map;
+using OpenTK.Graphics.OpenGL;
+using STROOP.Utilities;
+using STROOP.Structs.Configurations;
+using STROOP.Structs;
+using OpenTK;
+
+namespace STROOP.Map3
+{
+    public class Map3SelfObject : Map3IconPointObject
+    {
+        public Map3SelfObject()
+            : base()
+        {
+        }
+
+        public override Image GetImage()
+        {
+            return Config.ObjectAssociations.MarioMapImage;
+        }
+
+        public override PositionAngle GetPositionAngle()
+        {
+            return SpecialConfig.SelfPA;
+        }
+
+        public override string GetName()
+        {
+            return "Self";
+        }
+
+        public override float GetY()
+        {
+            return (float)SpecialConfig.SelfY;
+        }
+    }
+}
