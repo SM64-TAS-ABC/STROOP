e186614e992ecd0a4eaf7ad8e1b27dd9c541bd26
scob <scob@seas.upenn.edu>
2019-09-26 22:42:14 -0400

    prevent self/point in TAS tab

--- STROOP/Utilities/WatchVariableSpecialDictionary.cs
+++ STROOP/Utilities/WatchVariableSpecialDictionary.cs
@@ -159,6 +159,7 @@ namespace STROOP.Structs
                 if (objectValue == null) return false;
                 PositionAngle posAngle = PositionAngle.FromString(objectValue.ToString());
                 if (posAngle == null) return false;
+                if (posAngle.IsSelfOrPoint()) return false;
                 return setter(posAngle, address);
             };
             _dictionary[key] = (getter, newSetter);
