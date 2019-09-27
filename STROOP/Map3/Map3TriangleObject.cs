4e4dba332d153b9498c947d1a999973104a3f6fe
scob <scob@seas.upenn.edu>
2019-09-26 21:37:04 -0400

    add outline to triangle obj

--- STROOP/Map3/Map3TriangleObject.cs
+++ STROOP/Map3/Map3TriangleObject.cs
@@ -30,6 +30,8 @@ namespace STROOP.Map3
             GL.BindTexture(TextureTarget.Texture2D, -1);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
+
+            // Draw triangle
             GL.Color4(Color.R, Color.G, Color.B, OpacityByte);
             GL.Begin(PrimitiveType.Triangles);
             foreach ((float x, float z) in veriticesForControl)
@@ -37,6 +39,20 @@ namespace STROOP.Map3
                 GL.Vertex2(x, z);
             }
             GL.End();
+
+            // Draw outline
+            if (OutlineWidth != 0)
+            {
+                GL.Color4(OutlineColor.R, OutlineColor.G, OutlineColor.B, (byte)255);
+                GL.LineWidth(OutlineWidth);
+                GL.Begin(PrimitiveType.LineLoop);
+                foreach ((float x, float z) in veriticesForControl)
+                {
+                    GL.Vertex2(x, z);
+                }
+                GL.End();
+            }
+
             GL.Color4(1, 1, 1, 1.0f);
         }
 
