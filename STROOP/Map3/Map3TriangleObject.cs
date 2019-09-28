71ed61238640cda408dc5c815e77f6646add1839
scob <scob@seas.upenn.edu>
2019-09-28 00:13:37 -0400

    implement tri as units

--- STROOP/Map3/Map3TriangleObject.cs
+++ STROOP/Map3/Map3TriangleObject.cs
@@ -23,6 +23,18 @@ namespace STROOP.Map3
 
         public override void DrawOnControl()
         {
+            if (ShowTriUnits)
+            {
+                DrawOnControlWithUnits();
+            }
+            else
+            {
+                DrawOnControlWithoutUnits();
+            }
+        }
+
+        private void DrawOnControlWithoutUnits()
+        {
             List<List<(float x, float z)>> vertexLists = GetVertexLists();
             List<List<(float x, float z)>> vertexListsForControl =
                 vertexLists.ConvertAll(vertexList => vertexList.ConvertAll(
@@ -53,6 +65,109 @@ namespace STROOP.Map3
                 {
                     GL.Begin(PrimitiveType.LineLoop);
                     foreach ((float x, float z) in vertexList)
+                    {
+                        GL.Vertex2(x, z);
+                    }
+                    GL.End();
+                }
+            }
+
+            GL.Color4(1, 1, 1, 1.0f);
+        }
+
+        private void DrawOnControlWithUnits()
+        {
+            List<List<(float x, float z)>> triVertexLists = GetVertexLists();
+            List<(int x, int z)> unitPoints = triVertexLists.ConvertAll(vertexList =>
+            {
+                int xMin = (int)Math.Max(vertexList.Min(vertex => vertex.x), Config.Map3Graphics.MapViewXMin);
+                int xMax = (int)Math.Min(vertexList.Max(vertex => vertex.x), Config.Map3Graphics.MapViewXMax);
+                int zMin = (int)Math.Max(vertexList.Min(vertex => vertex.z), Config.Map3Graphics.MapViewZMin);
+                int zMax = (int)Math.Min(vertexList.Max(vertex => vertex.z), Config.Map3Graphics.MapViewZMax);
+
+                List<(int x, int z)> points = new List<(int x, int z)>();
+                for (int x = xMin; x <= xMax; x++)
+                {
+                    for (int z = zMin; z <= zMax; z++)
+                    {
+                        if (MoreMath.IsPointInsideTriangle(
+                            x, z,
+                            vertexList[0].x, vertexList[0].z,
+                            vertexList[1].x, vertexList[1].z,
+                            vertexList[2].x, vertexList[2].z))
+                        {
+                            points.Add((x, z));
+                        }
+                    }
+                }
+                return points;
+            }).SelectMany(points => points).Distinct().ToList();
+
+            List<List<(float x, float z)>> quadList = new List<List<(float x, float z)>>();
+            Action<int, int, int, int> addQuad = (int xBase, int zBase, int xAdd, int zAdd) =>
+            {
+                quadList.Add(new List<(float, float)>()
+                {
+                    (xBase, zBase),
+                    (xBase + xAdd, zBase),
+                    (xBase + xAdd, zBase + zAdd),
+                    (xBase, zBase + zAdd),
+                });
+            };
+            foreach ((int x, int z) in unitPoints)
+            {
+                if (x == 0 && z == 0)
+                {
+                    addQuad(x, z, 1, 1);
+                    addQuad(x, z, 1, -1);
+                    addQuad(x, z, -1, 1);
+                    addQuad(x, z, -1, -1);
+                }
+                else if (x == 0)
+                {
+                    addQuad(x, z, 1, Math.Sign(z));
+                    addQuad(x, z, -1, Math.Sign(z));
+                }
+                else if (z == 0)
+                {
+                    addQuad(x, z, Math.Sign(x), 1);
+                    addQuad(x, z, Math.Sign(x), -1);
+                }
+                else
+                {
+                    addQuad(x, z, Math.Sign(x), Math.Sign(z));
+                }
+            }
+
+            List<List<(float x, float z)>> quadListForControl =
+                quadList.ConvertAll(quad => quad.ConvertAll(
+                    vertex => Map3Utilities.ConvertCoordsForControl(vertex.x, vertex.z)));
+
+            GL.BindTexture(TextureTarget.Texture2D, -1);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            // Draw quad
+            GL.Color4(Color.R, Color.G, Color.B, OpacityByte);
+            GL.Begin(PrimitiveType.Quads);
+            foreach (List<(float x, float z)> quad in quadListForControl)
+            {
+                foreach ((float x, float z) in quad)
+                {
+                    GL.Vertex2(x, z);
+                }
+            }
+            GL.End();
+
+            // Draw outline
+            if (OutlineWidth != 0)
+            {
+                GL.Color4(OutlineColor.R, OutlineColor.G, OutlineColor.B, (byte)255);
+                GL.LineWidth(OutlineWidth);
+                foreach (List<(float x, float z)> quad in quadListForControl)
+                {
+                    GL.Begin(PrimitiveType.LineLoop);
+                    foreach ((float x, float z) in quad)
                     {
                         GL.Vertex2(x, z);
                     }
