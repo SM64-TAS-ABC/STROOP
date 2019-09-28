901382466e8f735f229c6f48e92d3ba8d500676c
scob <scob@seas.upenn.edu>
2019-09-28 19:36:40 -0400

    add track bar ex

---
+++ STROOP/Controls/TrackBarEx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using STROOP.Managers;
+using STROOP.Utilities;
+using STROOP.Structs;
+using STROOP.Controls;
+using STROOP.Extensions;
+using System.Drawing.Drawing2D;
+
+namespace STROOP
+{
+    public class TrackBarEx : TrackBar
+    {
+        private bool _isBeingChangedByCode = false;
+
+        public TrackBarEx()
+        {
+        }
+
+        public void AddManualChangeAction(Action action)
+        {
+            ValueChanged += (sender, e) =>
+            {
+                if (!_isBeingChangedByCode) action();
+            };
+        }
+
+        public void StartChangingByCode()
+        {
+            _isBeingChangedByCode = true;
+        }
+
+        public void StopChangingByCode()
+        {
+            _isBeingChangedByCode = false;
+        }
+    }
+}
