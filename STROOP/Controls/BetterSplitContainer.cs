a2c5a014b0e6c1499a3457a580dd09cc574ab663
scob <scob@seas.upenn.edu>
2019-10-02 20:49:40 -0400

    make panels update when split container changes size

--- STROOP/Controls/BetterSplitContainer.cs
+++ STROOP/Controls/BetterSplitContainer.cs
@@ -18,33 +18,70 @@ namespace STROOP
     {
         public BetterSplitContainer()
         {
-            /*
-            ToolStripMenuItem itemJustPanel1 = new ToolStripMenuItem("Just Panel 1");
-            itemJustPanel1.Click += (sender, e) =>
-            {
-                Panel1Collapsed = false;
-                Panel2Collapsed = true;
-            };
+            MouseDown += splitCont_MouseDown;
+            MouseUp += splitCont_MouseUp;
+            MouseMove += splitCont_MouseMove;
+        }
 
-            ToolStripMenuItem itemJustPanel2 = new ToolStripMenuItem("Just Panel 2");
-            itemJustPanel2.Click += (sender, e) =>
-            {
-                Panel1Collapsed = true;
-                Panel2Collapsed = false;
-            };
+        //assign this to the SplitContainer's MouseDown event
+        private void splitCont_MouseDown(object sender, MouseEventArgs e)
+        {
+            // This disables the normal move behavior
+            ((SplitContainer)sender).IsSplitterFixed = true;
+        }
 
-            ToolStripMenuItem itemBothPanels = new ToolStripMenuItem("Both Panels");
-            itemBothPanels.Click += (sender, e) =>
-            {
-                Panel1Collapsed = false;
-                Panel2Collapsed = false;
-            };
+        //assign this to the SplitContainer's MouseUp event
+        private void splitCont_MouseUp(object sender, MouseEventArgs e)
+        {
+            // This allows the splitter to be moved normally again
+            ((SplitContainer)sender).IsSplitterFixed = false;
+        }
 
-            ContextMenuStrip = new ContextMenuStrip();
-            ContextMenuStrip.Items.Add(itemJustPanel1);
-            ContextMenuStrip.Items.Add(itemJustPanel2);
-            ContextMenuStrip.Items.Add(itemBothPanels);
-            */
+        //assign this to the SplitContainer's MouseMove event
+        private void splitCont_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Check to make sure the splitter won't be updated by the
+            // normal move behavior also
+            if (((SplitContainer)sender).IsSplitterFixed)
+            {
+                // Make sure that the button used to move the splitter
+                // is the left mouse button
+                if (e.Button.Equals(MouseButtons.Left))
+                {
+                    // Checks to see if the splitter is aligned Vertically
+                    if (((SplitContainer)sender).Orientation.Equals(Orientation.Vertical))
+                    {
+                        // Only move the splitter if the mouse is within
+                        // the appropriate bounds
+                        if (e.X > 0 && e.X < ((SplitContainer)sender).Width)
+                        {
+                            // Move the splitter & force a visual refresh
+                            ((SplitContainer)sender).SplitterDistance = e.X;
+                            ((SplitContainer)sender).Refresh();
+                        }
+                    }
+                    // If it isn't aligned vertically then it must be
+                    // horizontal
+                    else
+                    {
+                        // Only move the splitter if the mouse is within
+                        // the appropriate bounds
+                        if (e.Y > 0 && e.Y < ((SplitContainer)sender).Height)
+                        {
+                            // Move the splitter & force a visual refresh
+                            ((SplitContainer)sender).SplitterDistance = e.Y;
+                            ((SplitContainer)sender).Refresh();
+                        }
+                    }
+                }
+                // If a button other than left is pressed or no button
+                // at all
+                else
+                {
+                    // This allows the splitter to be moved normally again
+                    ((SplitContainer)sender).IsSplitterFixed = false;
+                }
+            }
         }
     }
 }
