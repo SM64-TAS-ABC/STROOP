e15fccd7a0e89d9849087cfe0a32cdecb5285b10
scob <scob@seas.upenn.edu>
2019-10-03 02:17:43 -0400

    implement part of value qualifies

--- STROOP/Managers/SearchManager.cs
+++ STROOP/Managers/SearchManager.cs
@@ -20,14 +20,22 @@ namespace STROOP.Managers
         private enum ValueRelationship
         {
             EqualTo,
-            Between,
             GreaterThan,
             LessThan,
+            GreaterThanOrEqualTo,
+            LessThanOrEqualTo,
+
+            Changed,
+            DidNotChange,
             Increased,
             Decreased,
             IncreasedBy,
             DecreasedBy,
-            Unknown,
+
+            BetweenExclusive,
+            BetweenInclusive,
+
+            EverythingPasses,
         };
 
         private readonly Dictionary<uint, object> _dictionary;
@@ -158,32 +166,11 @@ namespace STROOP.Managers
 
         private (object searchValue1, object searchValue2) ParseSearchValue(string text, Type type)
         {
-            ValueRelationship valueRelationship = (ValueRelationship)_comboBoxValueRelationship.SelectedItem;
-            switch (valueRelationship)
-            {
-                case ValueRelationship.EqualTo:
-                case ValueRelationship.GreaterThan:
-                case ValueRelationship.LessThan:
-                case ValueRelationship.Increased:
-                case ValueRelationship.Decreased:
-                case ValueRelationship.IncreasedBy:
-                case ValueRelationship.DecreasedBy:
-                case ValueRelationship.Unknown:
-                    return (ParsingUtilities.ParseValueNullable(text, type), null);
-                case ValueRelationship.Between:
-                    List<string> stringValues = ParsingUtilities.ParseStringList(text);
-                    string stringValue1 = stringValues.Count >= 1 ? stringValues[0] : null;
-                    string stringValue2 = stringValues.Count >= 2 ? stringValues[1] : null;
-                    return (ParsingUtilities.ParseValueNullable(stringValue1, type),
-                        ParsingUtilities.ParseValueNullable(stringValue2, type));
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        private bool ValueQualifies(object memoryValue, object oldMemoryValue, object searchValue1, object searchValue2, Type type)
-        {
-            return Equals(memoryValue, searchValue1);
+            List<string> stringValues = ParsingUtilities.ParseStringList(text);
+            string stringValue1 = stringValues.Count >= 1 ? stringValues[0] : null;
+            string stringValue2 = stringValues.Count >= 2 ? stringValues[1] : null;
+            return (ParsingUtilities.ParseValueNullable(stringValue1, type),
+                ParsingUtilities.ParseValueNullable(stringValue2, type));
         }
 
         private void UpdateControlsBasedOnDictionary()
@@ -202,6 +189,66 @@ namespace STROOP.Managers
             if (!updateView) return;
 
             base.Update(updateView);
+        }
+
+        private bool ValueQualifies(object memoryObject, object oldMemoryObject, object searchObject1, object searchObject2, Type type)
+        {
+            if (type == typeof(byte))
+            {
+                byte? memoryValue = ParsingUtilities.ParseByteNullable(memoryObject);
+                if (!memoryValue.HasValue) return false;
+                byte? oldMemoryValue = ParsingUtilities.ParseByteNullable(oldMemoryObject);
+                byte? searchValue1 = ParsingUtilities.ParseByteNullable(searchObject1);
+                byte? searchValue2 = ParsingUtilities.ParseByteNullable(searchObject2);
+                ValueRelationship valueRelationship = (ValueRelationship)_comboBoxValueRelationship.SelectedItem;
+                switch (valueRelationship)
+                {
+                    case ValueRelationship.EqualTo:
+                        if (!searchValue1.HasValue) return false;
+                        return memoryValue.Value == searchValue1.Value;
+                    case ValueRelationship.GreaterThan:
+                        if (!searchValue1.HasValue) return false;
+                        return memoryValue.Value > searchValue1.Value;
+                    case ValueRelationship.LessThan:
+                        if (!searchValue1.HasValue) return false;
+                        return memoryValue.Value < searchValue1.Value;
+                    case ValueRelationship.GreaterThanOrEqualTo:
+                        if (!searchValue1.HasValue) return false;
+                        return memoryValue.Value >= searchValue1.Value;
+                    case ValueRelationship.LessThanOrEqualTo:
+                        if (!searchValue1.HasValue) return false;
+                        return memoryValue.Value <= searchValue1.Value;
+                    case ValueRelationship.Changed:
+                        if (!oldMemoryValue.HasValue) return false;
+                        return memoryValue.Value != oldMemoryValue.Value;
+                    case ValueRelationship.DidNotChange:
+                        if (!oldMemoryValue.HasValue) return false;
+                        return memoryValue.Value == oldMemoryValue.Value;
+                    case ValueRelationship.Increased:
+                        if (!oldMemoryValue.HasValue) return false;
+                        return memoryValue.Value > oldMemoryValue.Value;
+                    case ValueRelationship.Decreased:
+                        if (!oldMemoryValue.HasValue) return false;
+                        return memoryValue.Value < oldMemoryValue.Value;
+                    case ValueRelationship.IncreasedBy:
+                        if (!oldMemoryValue.HasValue || !searchValue1.HasValue) return false;
+                        return memoryValue.Value == oldMemoryValue.Value + searchValue1.Value;
+                    case ValueRelationship.DecreasedBy:
+                        if (!oldMemoryValue.HasValue || !searchValue1.HasValue) return false;
+                        return memoryValue.Value == oldMemoryValue.Value - searchValue1.Value;
+                    case ValueRelationship.BetweenExclusive:
+                        if (!searchValue1.HasValue || !searchValue2.HasValue) return false;
+                        return searchValue1.Value < memoryValue.Value && memoryValue.Value < searchValue2.Value;
+                    case ValueRelationship.BetweenInclusive:
+                        if (!searchValue1.HasValue || !searchValue2.HasValue) return false;
+                        return searchValue1.Value <= memoryValue.Value && memoryValue.Value <= searchValue2.Value;
+                    case ValueRelationship.EverythingPasses:
+                        return true;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            return Equals(memoryObject, searchObject1);
         }
     }
 }
