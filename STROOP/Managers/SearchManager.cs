de50501c3411a969cdc654fd9dfa4455890ed793
scob <scob@seas.upenn.edu>
2019-10-02 22:49:47 -0400

    parse controls

--- STROOP/Managers/SearchManager.cs
+++ STROOP/Managers/SearchManager.cs
@@ -19,7 +19,15 @@ namespace STROOP.Managers
         public SearchManager(TabPage tabControl, WatchVariableFlowLayoutPanel watchVariablePanel)
             : base(null, watchVariablePanel)
         {
-            
+            SplitContainer splitContainerSearch = tabControl.Controls["splitContainerSearch"] as SplitContainer;
+            SplitContainer splitContainerSearchOptions = splitContainerSearch.Panel1.Controls["splitContainerSearchOptions"] as SplitContainer;
+
+            ComboBox comboBoxSearchMemoryType = splitContainerSearchOptions.Panel1.Controls["comboBoxSearchMemoryType"] as ComboBox;
+            ComboBox comboBoxValueRelationship = splitContainerSearchOptions.Panel1.Controls["comboBoxValueRelationship"] as ComboBox;
+            BetterTextbox textBoxSearchValue = splitContainerSearchOptions.Panel1.Controls["textBoxSearchValue"] as BetterTextbox;
+            Button buttonSearchFirstScan = splitContainerSearchOptions.Panel1.Controls["buttonSearchFirstScan"] as Button;
+            Button buttonSearchNextScan = splitContainerSearchOptions.Panel1.Controls["buttonSearchNextScan"] as Button;
+            DataGridView dataGridViewSearch = splitContainerSearchOptions.Panel2.Controls["dataGridViewSearch"] as DataGridView;
         }
 
         public override void Update(bool updateView)
