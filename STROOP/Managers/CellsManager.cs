ed74a64f4193b666b3e24dbf23826a77137d04d6
scob <scob@seas.upenn.edu>
2019-10-20 23:44:11 -0400

    implement most of cells manager

--- STROOP/Managers/CellsManager.cs
+++ STROOP/Managers/CellsManager.cs
@@ -26,7 +26,60 @@ namespace STROOP.Managers
             SplitContainer splitContainerCellsControls = splitContainerCells.Panel1.Controls["splitContainerCellsControls"] as SplitContainer;
 
             _buttonCellsBuildTree = splitContainerCellsControls.Panel1.Controls["buttonCellsBuildTree"] as Button;
+            _buttonCellsBuildTree.Click += (sender, e) => BuildTree();
+
             _treeViewCells = splitContainerCellsControls.Panel2.Controls["treeViewCells"] as TreeView;
+        }
+
+        private void BuildTree()
+        {
+            _treeViewCells.Nodes.Clear();
+            _treeViewCells.Nodes.Add(GetTreeNodeForPartition(true));
+            _treeViewCells.Nodes.Add(GetTreeNodeForPartition(false));
+        }
+
+        private TreeNode GetTreeNodeForPartition(bool staticPartition)
+        {
+            string name = staticPartition ? "Static Cells" : "Dynamic Cells";
+            uint address = staticPartition ? 0x8038BE98 : 0x8038D698;
+            TreeNode node = new TreeNode(name);
+            for (int z = 0; z < 16; z++)
+            {
+                node.Nodes.Add(GetTreeNodeForZ(address, z));
+            }
+            return node;
+        }
+
+        private TreeNode GetTreeNodeForZ(uint address, int z)
+        {
+            int lowerBound = -8192 + z * 1024;
+            int upperBound = lowerBound + 1024;
+            string name = "z = " + z + " (" + lowerBound + " < z < " + upperBound + ")";
+            TreeNode node = new TreeNode(name);
+            for (int x = 0; x < 16; x++)
+            {
+                node.Nodes.Add(GetTreeNodeForX(address, z, x));
+            }
+            return node;
+        }
+
+        private TreeNode GetTreeNodeForX(uint address, int z, int x)
+        {
+            int lowerBound = -8192 + x * 1024;
+            int upperBound = lowerBound + 1024;
+            string name = "x = " + x + " (" + lowerBound + " < x < " + upperBound + ")";
+            TreeNode node = new TreeNode(name);
+            for (int type = 0; type < 3; type++)
+            {
+                node.Nodes.Add(GetTreeNodeForType(address, z, x, type));
+            }
+            return node;
+        }
+
+        private TreeNode GetTreeNodeForType(uint address, int z, int x, int type)
+        {
+            string name = address + " " + z + " " + x + " " + type;
+            return new TreeNode(name);
         }
 
         public override void Update(bool updateView)
