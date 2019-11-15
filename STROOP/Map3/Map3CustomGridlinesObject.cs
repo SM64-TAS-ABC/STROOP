305ea67c194e0fd086cdbd68c667d319edcded35
scob <scob@seas.upenn.edu>
2019-11-15 14:59:58 -0500

    add programming for custom gridlines

--- STROOP/Map3/Map3CustomGridlinesObject.cs
+++ STROOP/Map3/Map3CustomGridlinesObject.cs
@@ -18,19 +18,23 @@ namespace STROOP.Map3
         public Map3CustomGridlinesObject()
             : base()
         {
+            Size = 1;
             OutlineWidth = 3;
             OutlineColor = Color.Black;
         }
 
         protected override List<(float x, float z)> GetVertices()
         {
+            int size = (int)Size;
+            if (size < 0) size = 0;
+            int increment = (int)(16384 / Math.Pow(2, size));
             List<(float x, float z)> vertices = new List<(float x, float z)>();
-            for (int x = -8192; x <= 8192; x += 1024)
+            for (int x = -8192; x <= 8192; x += increment)
             {
                 vertices.Add((x, -8192));
                 vertices.Add((x, 8192));
             }
-            for (int z = -8192; z <= 8192; z += 1024)
+            for (int z = -8192; z <= 8192; z += increment)
             {
                 vertices.Add((-8192, z));
                 vertices.Add((8192, z));
