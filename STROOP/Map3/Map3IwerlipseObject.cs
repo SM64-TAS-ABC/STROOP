a2ef64ae0a312b96edcb3f23ff10d9b86f58d702
Scott Buchanan <scob@seas.upenn.edu>
2019-12-18 01:24:20 -0500

    add more logic to iwerlipse

--- STROOP/Map3/Map3IwerlipseObject.cs
+++ STROOP/Map3/Map3IwerlipseObject.cs
@@ -27,37 +27,35 @@ namespace STROOP.Map3
 
         public override void DrawOnControl()
         {
-            float marioX = Config.Stream.GetSingle(MarioConfig.StructAddress + MarioConfig.XOffset);
-            float marioY = Config.Stream.GetSingle(MarioConfig.StructAddress + MarioConfig.YOffset);
-            float marioZ = Config.Stream.GetSingle(MarioConfig.StructAddress + MarioConfig.ZOffset);
-            ushort marioAngle = Config.Stream.GetUInt16(MarioConfig.StructAddress + MarioConfig.FacingYawOffset);
-
             MarioState marioState = MarioState.CreateMarioState();
-            MarioState MarioStateForward = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Forward);
-            MarioState MarioStateBackward = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Backward);
-            MarioState MarioStateLeft = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Left);
-            MarioState MarioStateRight = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Right);
-            DrawCircles(MarioStateForward.X, MarioStateForward.Z, 20, Color.Blue);
-            DrawCircles(MarioStateBackward.X, MarioStateBackward.Z, 20, Color.Orange);
-            DrawCircles(MarioStateLeft.X, MarioStateLeft.Z, 20, Color.Purple);
-            DrawCircles(MarioStateRight.X, MarioStateRight.Z, 20, Color.Red);
-        }
+            MarioState marioStateForward = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Forward);
+            MarioState marioStateBackward = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Backward);
+            MarioState marioStateLeft = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Left);
+            MarioState marioStateRight = AirMovementCalculator.ApplyInput(marioState, RelativeDirection.Right);
 
-        private void DrawCircles(float centerX, float centerZ, float radius, Color color)
-        {
-            (float controlCenterX, float controlCenterZ) = Map3Utilities.ConvertCoordsForControl(centerX, centerZ);
-            float controlRadius = radius * Config.Map3Graphics.MapViewScaleValue;
-            List <(float pointX, float pointZ)> controlPoints = Enumerable.Range(0, NUM_POINTS).ToList()
+            ushort marioAngle = marioState.MarioAngle;
+            (float cx, float cz) = (marioState.X, marioState.Z);
+            (float fx, float fz) = (marioStateForward.X, marioStateForward.Z);
+            (float bx, float bz) = (marioStateBackward.X, marioStateBackward.Z);
+            (float lx, float lz) = (marioStateLeft.X, marioStateLeft.Z);
+            (float rx, float rz) = (marioStateRight.X, marioStateRight.Z);
+
+            double sideDist = MoreMath.GetDistanceBetween(cx, cz, lx, lz);
+            double forwardDist = MoreMath.GetDistanceBetween(cx, cz, fx, fz);
+            double backwardDist = MoreMath.GetDistanceBetween(cx, cz, bx, bz);
+
+            (float controlCenterX, float controlCenterZ) = Map3Utilities.ConvertCoordsForControl(cx, cz);
+            List<(float pointX, float pointZ)> controlPoints = Enumerable.Range(0, NUM_POINTS).ToList()
                 .ConvertAll(index => (index / (float)NUM_POINTS) * 65536)
-                .ConvertAll(angle => ((float, float))MoreMath.AddVectorToPoint(controlRadius, angle, controlCenterX, controlCenterZ));
+                .ConvertAll(angle => GetEllipsePoint(cx, cz, sideDist, forwardDist, 0, angle))
+                .ConvertAll(point => Map3Utilities.ConvertCoordsForControl((float)point.x, (float)point.z));
 
             GL.BindTexture(TextureTarget.Texture2D, -1);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
 
             // Draw circle
-            //GL.Color4(Color.R, Color.G, Color.B, OpacityByte);
-            GL.Color4(color.R, color.G, color.B, OpacityByte);
+            GL.Color4(Color.R, Color.G, Color.B, OpacityByte);
             GL.Begin(PrimitiveType.TriangleFan);
             GL.Vertex2(controlCenterX, controlCenterZ);
             foreach ((float x, float z) in controlPoints)
@@ -81,6 +79,16 @@ namespace STROOP.Map3
             }
 
             GL.Color4(1, 1, 1, 1.0f);
+        }
+
+        private (double x, double z) GetEllipsePoint(
+            double centerX, double centerZ, double sidewaysDist, double forwardDist, double rotatedAngle, double angle)
+        {
+            double angleRadians = MoreMath.AngleUnitsToRadians(angle);
+            double term1 = forwardDist * Math.Cos(angleRadians);
+            double term2 = sidewaysDist * Math.Sin(angleRadians);
+            double radius = MoreMath.GetHypotenuse(term1, term2);
+            return MoreMath.AddVectorToPoint(radius, angle, centerX, centerZ);
         }
 
         public override string GetName()
