63cef3c1728ee5e706901b8a48e103395cb94699
Scott Buchanan <scob@seas.upenn.edu>
2019-12-24 00:22:11 -0500

    move tri 3d draw func

--- STROOP/Map3/Map3HorizontalTriangleObject.cs
+++ STROOP/Map3/Map3HorizontalTriangleObject.cs
@@ -145,5 +145,79 @@ namespace STROOP.Map3
 
             GL.Color4(1, 1, 1, 1.0f);
         }
+
+        public override void DrawOn3DControl()
+        {
+            List<List<(float x, float y, float z)>> topSurfaces = GetTriangles()
+                .ConvertAll(tri => new List<(float x, float y, float z)>()
+                {
+                    (tri.X1, tri.Y1, tri.Z1),
+                    (tri.X2, tri.Y2, tri.Z2),
+                    (tri.X3, tri.Y3, tri.Z3),
+                });
+
+            (float x, float y, float z) OffsetVertex((float x, float y, float z) vertex, float offset)
+            {
+                return (vertex.x, vertex.y + offset, vertex.z);
+            }
+            List<List<(float x, float y, float z)>> bottomSurfaces =
+                topSurfaces.ConvertAll(topSurface =>
+                    topSurface.ConvertAll(vertex =>
+                        OffsetVertex(vertex, -78)));
+
+            List<List<(float x, float y, float z)>> GetSideSurfaces(int index1, int index2) =>
+                topSurfaces.ConvertAll(topSurface =>
+                    new List<(float x, float y, float z)>()
+                    {
+                        topSurface[index1],
+                        topSurface[index2],
+                        OffsetVertex(topSurface[index2], -78),
+                        OffsetVertex(topSurface[index1], -78),
+                    });
+            List<List<(float x, float y, float z)>> side1Surfaces = GetSideSurfaces(0, 1);
+            List<List<(float x, float y, float z)>> side2Surfaces = GetSideSurfaces(1, 2);
+            List<List<(float x, float y, float z)>> side3Surfaces = GetSideSurfaces(2, 0);
+
+            List<List<(float x, float y, float z)>> allSurfaces =
+                topSurfaces
+                .Concat(bottomSurfaces)
+                .Concat(side1Surfaces)
+                .Concat(side2Surfaces)
+                .Concat(side3Surfaces)
+                .ToList();
+
+            List<Map4Vertex[]> vertexArray1 = allSurfaces.ConvertAll(
+                vertexList => vertexList.ConvertAll(vertex => new Map4Vertex(new Vector3(
+                    vertex.x, vertex.y, vertex.z), Color4)).ToArray());
+            List<Map4Vertex[]> vertexArray2 = allSurfaces.ConvertAll(
+                vertexList => vertexList.ConvertAll(vertex => new Map4Vertex(new Vector3(
+                    vertex.x, vertex.y, vertex.z), OutlineColor)).ToArray());
+
+            vertexArray1.ForEach(vertexes =>
+            {
+                int buffer = GL.GenBuffer();
+                GL.BindTexture(TextureTarget.Texture2D, Config.Map4Graphics.Utilities.WhiteTexture);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
+                GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexes.Length * Map4Vertex.Size), vertexes, BufferUsageHint.DynamicDraw);
+                Config.Map4Graphics.BindVertices();
+                GL.DrawArrays(PrimitiveType.Polygon, 0, vertexes.Length);
+                GL.DeleteBuffer(buffer);
+            });
+
+            if (OutlineWidth != 0)
+            {
+                vertexArray2.ForEach(vertexes =>
+                {
+                    int buffer = GL.GenBuffer();
+                    GL.BindTexture(TextureTarget.Texture2D, Config.Map4Graphics.Utilities.WhiteTexture);
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
+                    GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexes.Length * Map4Vertex.Size), vertexes, BufferUsageHint.DynamicDraw);
+                    GL.LineWidth(OutlineWidth);
+                    Config.Map4Graphics.BindVertices();
+                    GL.DrawArrays(PrimitiveType.LineLoop, 0, vertexes.Length);
+                    GL.DeleteBuffer(buffer);
+                });
+            }
+        }
     }
 }
