9308315b7383b1c98bfbbab319ccd85ac532ea5c
Scott Buchanan <scob@seas.upenn.edu>
2019-12-25 01:23:02 -0500

    fill in side of cylinder

--- STROOP/Map3/Map3CylinderObject.cs
+++ STROOP/Map3/Map3CylinderObject.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
 using STROOP.Controls.Map;
 using OpenTK.Graphics.OpenGL;
 using STROOP.Utilities;
 using STROOP.Structs.Configurations;
 using STROOP.Structs;
 using OpenTK;
-using System.Drawing.Imaging;
 using STROOP.Map3.Map.Graphics;
+using OpenTK.Graphics;
+using System.Drawing;
 
 namespace STROOP.Map3
 {
@@ -34,24 +34,48 @@ namespace STROOP.Map3
         {
             (float centerX, float centerZ, float radius, float minY, float maxY) = Get3DDimensions();
 
-            Map4Vertex[] GetBaseVertices(float height)
+            Map4Vertex[] GetBaseVertices(float height, Color4 color)
             {
-                List<(float x, float y, float z)> points = Enumerable.Range(0, NUM_POINTS).ToList()
+                List<(float x, float y, float z)> points3D = Enumerable.Range(0, NUM_POINTS).ToList()
                     .ConvertAll(index => (index / (float)NUM_POINTS) * 65536)
                     .ConvertAll(angle =>
                     {
                         (float x, float z) = ((float, float))MoreMath.AddVectorToPoint(radius, angle, centerX, centerZ);
                         return (x, height, z);
                     });
-                return points.ConvertAll(
+                return points3D.ConvertAll(
                     vertex => new Map4Vertex(new Vector3(
-                        vertex.x, vertex.y, vertex.z), Color4)).ToArray();
+                        vertex.x, vertex.y, vertex.z), color)).ToArray();
             }
-            List<Map4Vertex[]> vertexArrayForSurfaces = new List<Map4Vertex[]>()
+            List<Map4Vertex[]> vertexArrayForBases = new List<Map4Vertex[]>()
             {
-                GetBaseVertices(maxY),
-                GetBaseVertices(minY),
+                GetBaseVertices(maxY, Color4),
+                GetBaseVertices(minY, Color4),
             };
+            List<Map4Vertex[]> vertexArrayForEdges = new List<Map4Vertex[]>()
+            {
+                GetBaseVertices(maxY, OutlineColor),
+                GetBaseVertices(minY, OutlineColor),
+            };
+
+            List<(float x, float z)> points2D = Enumerable.Range(0, NUM_POINTS).ToList()
+                .ConvertAll(index => (index / (float)NUM_POINTS) * 65536)
+                .ConvertAll(angle => ((float, float))MoreMath.AddVectorToPoint(radius, angle, centerX, centerZ));
+            List<Map4Vertex[]> vertexArrayForCurve = new List<Map4Vertex[]>();
+            for (int i = 0; i < points2D.Count; i++)
+            {
+                (float x1, float z1) = points2D[i];
+                (float x2, float z2) = points2D[(i + 1) % points2D.Count];
+                vertexArrayForCurve.Add(new Map4Vertex[]
+                {
+                    new Map4Vertex(new Vector3(x1, maxY, z1), Color4),
+                    new Map4Vertex(new Vector3(x2, maxY, z2), Color4),
+                    new Map4Vertex(new Vector3(x2, minY, z2), Color4),
+                    new Map4Vertex(new Vector3(x1, minY, z1), Color4),
+                });
+            }
+
+            List<Map4Vertex[]> vertexArrayForSurfaces = vertexArrayForBases.Concat(vertexArrayForCurve).ToList();
 
             vertexArrayForSurfaces.ForEach(vertexes =>
             {
@@ -64,86 +88,9 @@ namespace STROOP.Map3
                 GL.DeleteBuffer(buffer);
             });
 
-
-
-
-            /*
-            float relativeHeight = _relativeHeight ?? 0;
-            List<TriangleDataModel> tris = GetTriangles();
-
-            List<List<(float x, float y, float z)>> centerSurfaces =
-                tris.ConvertAll(tri => tri.Get3DVertices()
-                    .ConvertAll(vertex => OffsetVertex(vertex, 0, relativeHeight, 0)));
-
-            List<List<(float x, float y, float z)>> GetFrontOrBackSurfaces(bool front) =>
-                tris.ConvertAll(tri =>
-                {
-                    bool xProjection = tri.XProjection;
-                    float angle = (float)Math.Atan2(tri.NormX, tri.NormZ);
-                    float projectionMag = Size / (float)Math.Abs(xProjection ? Math.Sin(angle) : Math.Cos(angle));
-                    float projectionDist = front ? projectionMag : -1 * projectionMag;
-                    float xOffset = xProjection ? projectionDist : 0;
-                    float yOffset = relativeHeight;
-                    float zOffset = xProjection ? 0 : projectionDist;
-                    return tri.Get3DVertices().ConvertAll(vertex =>
-                    {
-                        return OffsetVertex(vertex, xOffset, yOffset, zOffset);
-                    });
-                });
-            List<List<(float x, float y, float z)>> frontSurfaces = GetFrontOrBackSurfaces(true);
-            List<List<(float x, float y, float z)>> backSurfaces = GetFrontOrBackSurfaces(false);
-
-            List<List<(float x, float y, float z)>> GetSideSurfaces(int index1, int index2) =>
-                tris.ConvertAll(tri =>
-                {
-                    bool xProjection = tri.XProjection;
-                    float angle = (float)Math.Atan2(tri.NormX, tri.NormZ);
-                    float projectionMag = Size / (float)Math.Abs(xProjection ? Math.Sin(angle) : Math.Cos(angle));
-                    float xOffsetMag = xProjection ? projectionMag : 0;
-                    float zOffsetMag = xProjection ? 0 : projectionMag;
-                    List<(float x, float y, float z)> vertices = tri.Get3DVertices();
-                    return new List<(float x, float y, float z)>()
-                    {
-                        OffsetVertex(vertices[index1], xOffsetMag, relativeHeight, zOffsetMag),
-                        OffsetVertex(vertices[index2], xOffsetMag, relativeHeight, zOffsetMag),
-                        OffsetVertex(vertices[index2], -1 * xOffsetMag, relativeHeight, -1 * zOffsetMag),
-                        OffsetVertex(vertices[index1], -1 * xOffsetMag, relativeHeight, -1 * zOffsetMag),
-                    };
-                });
-            List<List<(float x, float y, float z)>> side1Surfaces = GetSideSurfaces(0, 1);
-            List<List<(float x, float y, float z)>> side2Surfaces = GetSideSurfaces(1, 2);
-            List<List<(float x, float y, float z)>> side3Surfaces = GetSideSurfaces(2, 0);
-
-            List<List<(float x, float y, float z)>> allSurfaces =
-                centerSurfaces
-                .Concat(frontSurfaces)
-                .Concat(backSurfaces)
-                .Concat(side1Surfaces)
-                .Concat(side2Surfaces)
-                .Concat(side3Surfaces)
-                .ToList();
-
-            List<Map4Vertex[]> vertexArray1 = allSurfaces.ConvertAll(
-                vertexList => vertexList.ConvertAll(vertex => new Map4Vertex(new Vector3(
-                    vertex.x, vertex.y, vertex.z), Color4)).ToArray());
-            List<Map4Vertex[]> vertexArray2 = allSurfaces.ConvertAll(
-                vertexList => vertexList.ConvertAll(vertex => new Map4Vertex(new Vector3(
-                    vertex.x, vertex.y, vertex.z), OutlineColor)).ToArray());
-
-            vertexArray1.ForEach(vertexes =>
-            {
-                int buffer = GL.GenBuffer();
-                GL.BindTexture(TextureTarget.Texture2D, Config.Map4Graphics.Utilities.WhiteTexture);
-                GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
-                GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexes.Length * Map4Vertex.Size), vertexes, BufferUsageHint.DynamicDraw);
-                Config.Map4Graphics.BindVertices();
-                GL.DrawArrays(PrimitiveType.Polygon, 0, vertexes.Length);
-                GL.DeleteBuffer(buffer);
-            });
-
             if (OutlineWidth != 0)
             {
-                vertexArray2.ForEach(vertexes =>
+                vertexArrayForEdges.ForEach(vertexes =>
                 {
                     int buffer = GL.GenBuffer();
                     GL.BindTexture(TextureTarget.Texture2D, Config.Map4Graphics.Utilities.WhiteTexture);
@@ -155,7 +102,6 @@ namespace STROOP.Map3
                     GL.DeleteBuffer(buffer);
                 });
             }
-            */
         }
     }
 }
