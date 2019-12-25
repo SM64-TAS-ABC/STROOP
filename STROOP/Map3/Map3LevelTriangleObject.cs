dd696abaa955fcd89011f84c039b48c19c3da434
Scott Buchanan <scob@seas.upenn.edu>
2019-12-25 13:25:55 -0500

    move level tri draw3d function to its class

--- STROOP/Map3/Map3LevelTriangleObject.cs
+++ STROOP/Map3/Map3LevelTriangleObject.cs
@@ -14,6 +14,7 @@ using System.Drawing.Imaging;
 using STROOP.Models;
 using System.Windows.Forms;
 using STROOP.Forms;
+using STROOP.Map3.Map.Graphics;
 
 namespace STROOP.Map3
 {
@@ -30,14 +31,65 @@ namespace STROOP.Map3
             OutlineWidth = 2;
         }
 
-        protected override bool UseAutomaticColoring()
-        {
-            return true;
-        }
-
         public override void DrawOn2DControl()
         {
             // do nothing
+        }
+
+        public override void DrawOn3DControl()
+        {
+            List<List<(float x, float y, float z, Color color)>> triData = GetTriangles()
+                .ConvertAll(tri => new List<(float x, float y, float z, Color color)>()
+                {
+                    (tri.X1, tri.Y1, tri.Z1, ColorUtilities.AddAlpha(GetColorForTri(tri), OpacityByte)),
+                    (tri.X2, tri.Y2, tri.Z2, ColorUtilities.AddAlpha(GetColorForTri(tri), OpacityByte)),
+                    (tri.X3, tri.Y3, tri.Z3, ColorUtilities.AddAlpha(GetColorForTri(tri), OpacityByte)),
+                });
+            Map4Vertex[] vertexArray = triData.SelectMany(vertexList => vertexList).ToList()
+                .ConvertAll(vertex => new Map4Vertex(new Vector3(
+                    vertex.x, vertex.y, vertex.z), vertex.color)).ToArray();
+            List<Map4Vertex[]> vertexArray2 = triData.ConvertAll(
+                vertexList => vertexList.ConvertAll(vertex => new Map4Vertex(new Vector3(
+                    vertex.x, vertex.y, vertex.z), OutlineColor)).ToArray());
+
+            int buffer1 = GL.GenBuffer();
+            GL.BindTexture(TextureTarget.Texture2D, Config.Map4Graphics.Utilities.WhiteTexture);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, buffer1);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexArray.Length * Map4Vertex.Size), vertexArray, BufferUsageHint.DynamicDraw);
+            Config.Map4Graphics.BindVertices();
+            GL.DrawArrays(PrimitiveType.Triangles, 0, vertexArray.Length);
+            GL.DeleteBuffer(buffer1);
+
+            if (OutlineWidth != 0)
+            {
+                vertexArray2.ForEach(vertexes =>
+                {
+                    int buffer2 = GL.GenBuffer();
+                    GL.BindTexture(TextureTarget.Texture2D, Config.Map4Graphics.Utilities.WhiteTexture);
+                    GL.BindBuffer(BufferTarget.ArrayBuffer, buffer2);
+                    GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexes.Length * Map4Vertex.Size), vertexes, BufferUsageHint.DynamicDraw);
+                    GL.LineWidth(OutlineWidth);
+                    Config.Map4Graphics.BindVertices();
+                    GL.DrawArrays(PrimitiveType.LineLoop, 0, vertexes.Length);
+                    GL.DeleteBuffer(buffer2);
+                });
+            }
+        }
+
+        private static Color GetColorForTri(TriangleDataModel tri)
+        {
+            double clampedNormY = MoreMath.Clamp(tri.NormY, -1, 1);
+            switch (tri.Classification)
+            {
+                case TriangleClassification.Wall:
+                    return tri.XProjection ? Color.FromArgb(58, 116, 58) : Color.FromArgb(116, 203, 116);
+                case TriangleClassification.Floor:
+                    return Color.FromArgb(130, 130, 231).Darken(0.6 * (1 - clampedNormY));
+                case TriangleClassification.Ceiling:
+                    return Color.FromArgb(231, 130, 130).Darken(0.6 * (clampedNormY + 1));
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         protected override List<TriangleDataModel> GetTriangles()
