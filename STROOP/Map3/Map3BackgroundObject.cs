034a2f1c618b9a4a4a096c2b2ff252abf928736c
Scott Buchanan <scob@seas.upenn.edu>
2019-12-26 21:44:02 -0500

    refactor get vertices in background

--- STROOP/Map3/Map3BackgroundObject.cs
+++ STROOP/Map3/Map3BackgroundObject.cs
@@ -36,26 +36,36 @@ namespace STROOP.Map3
             return Map3DrawType.Background;
         }
 
-        static readonly Map4Vertex[] _vertices = new Map4Vertex[]
+        private Map4Vertex[] GetVertices()
         {
-            new Map4Vertex(new Vector3(-1, -1, 0), new Vector2(0, 1)),
-            new Map4Vertex(new Vector3(1, -1, 0),  new Vector2(1, 1)),
-            new Map4Vertex(new Vector3(-1, 1, 0),  new Vector2(0, 0)),
-            new Map4Vertex(new Vector3(1, 1, 0),   new Vector2(1, 0)),
-            new Map4Vertex(new Vector3(-1, 1, 0),  new Vector2(0, 0)),
-            new Map4Vertex(new Vector3(1, -1, 0),  new Vector2(1, 1)),
-        };
+            float leftBound = -1;
+            float rightBound = 1;
+            float upperBound = 1;
+            float lowerBound = -1;
+
+            return new Map4Vertex[]
+            {
+                new Map4Vertex(new Vector3(leftBound, lowerBound, 0), new Vector2(0, 1)),
+                new Map4Vertex(new Vector3(rightBound, lowerBound, 0),  new Vector2(1, 1)),
+                new Map4Vertex(new Vector3(leftBound, upperBound, 0),  new Vector2(0, 0)),
+                new Map4Vertex(new Vector3(rightBound, upperBound, 0),   new Vector2(1, 0)),
+                new Map4Vertex(new Vector3(leftBound, upperBound, 0),  new Vector2(0, 0)),
+                new Map4Vertex(new Vector3(rightBound, lowerBound, 0),  new Vector2(1, 1)),
+            };
+        }
 
         public override void DrawOn3DControl()
         {
+            Map4Vertex[] vertices = GetVertices();
+
             int buffer = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(_vertices.Length * Map4Vertex.Size),
-                _vertices, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertices.Length * Map4Vertex.Size),
+                vertices, BufferUsageHint.StaticDraw);
             GL.BindTexture(TextureTarget.Texture2D, TextureId);
             GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
             Config.Map4Graphics.BindVertices();
-            GL.DrawArrays(PrimitiveType.Triangles, 0, _vertices.Length);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, vertices.Length);
             GL.DeleteBuffer(buffer);
         }
     }
