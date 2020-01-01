0478630004c0a21dbfedd628dd72afaeb0a334a0
Scott Buchanan <scob@seas.upenn.edu>
2019-12-31 22:10:31 -0500

    clean up code

--- STROOP/Map/Map3D/Map3DGraphics.cs
+++ STROOP/Map/Map3D/Map3DGraphics.cs
@@ -401,6 +401,8 @@ namespace STROOP.Map.Map3D
                     ((float, float, float))MoreMath.TranslateRelatively(
                         SpecialConfig.Map3DCameraYaw, SpecialConfig.Map3DCameraPitch, SpecialConfig.Map3DCameraRoll,
                         unitDiffX, unitDiffY, 0);
+
+                SpecialConfig.Map3DMode = Map3DCameraMode.CameraPosAndAngle;
                 SpecialConfig.Map3DCameraX = _translateStartPositionX - rotX;
                 SpecialConfig.Map3DCameraY = _translateStartPositionY + rotY;
                 SpecialConfig.Map3DCameraZ = _translateStartPositionZ - rotZ;
@@ -413,6 +415,8 @@ namespace STROOP.Map.Map3D
                 int pixelDiffY = e.Y - _rotateStartMouseY;
                 float angleDiffX = pixelDiffX * scale;
                 float angleDiffY = pixelDiffY * scale;
+
+                SpecialConfig.Map3DMode = Map3DCameraMode.CameraPosAndAngle;
                 SpecialConfig.Map3DCameraYaw = _rotateStartYaw + angleDiffX;
                 SpecialConfig.Map3DCameraPitch = _rotateStartPitch + angleDiffY;
             }
@@ -425,136 +429,11 @@ namespace STROOP.Map.Map3D
                 ((float, float, float))MoreMath.TranslateRelatively(
                     SpecialConfig.Map3DCameraYaw, SpecialConfig.Map3DCameraPitch, SpecialConfig.Map3DCameraRoll,
                     0, 0, multiplier * 100);
+
+            SpecialConfig.Map3DMode = Map3DCameraMode.CameraPosAndAngle;
             SpecialConfig.Map3DCameraX -= rotX;
             SpecialConfig.Map3DCameraY += rotY;
             SpecialConfig.Map3DCameraZ -= rotZ;
         }
-
-        /*
-        volatile bool _mousePressedWithin = false;
-        private void Control_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
-        {
-            _mousePressedWithin = true;
-        }
-        */
-
-        /*
-        Vector2 _pMouseCoords;
-        float? _pMouseScroll = null;
-        bool _mousePressed = false;
-        public void CameraFlyUpdate()
-        {
-            KeyboardState keyState = Keyboard.GetState();
-            bool anyInput = false;
-
-            // Calculate key speed multiplier
-            float speedMul = 1f;
-            if (keyState.IsKeyDown(Key.ControlLeft) || keyState.IsKeyDown(Key.ControlRight))
-                speedMul = 0.0f;
-            else if (keyState.IsKeyDown(Key.ShiftLeft) || keyState.IsKeyDown(Key.ShiftRight))
-                speedMul = 3.0f;
-            else if (keyState.IsKeyDown(Key.AltLeft) || keyState.IsKeyDown(Key.AltRight))
-                speedMul = 0.3f;
-
-            // Handle mouse
-            MouseState mouseState = Mouse.GetState();
-            if (mouseState.LeftButton == OpenTK.Input.ButtonState.Pressed && _mousePressedWithin)
-            {
-                // Reset previous coordinates so no movement occurs during the initial press 
-                if (!_mousePressed)
-                {
-                    _pMouseCoords = new Vector2(mouseState.X, mouseState.Y);
-                }
-
-                // Calcualte mouse delta
-                Vector2 delta = new Vector2(mouseState.X, mouseState.Y) - _pMouseCoords;
-
-                // Add speed multiplier
-                delta *= speedMul * 0.009f;
-                delta *= _perspectiveCamera.FOV / 90;
-
-                // Trackball (add mouse deltas to angle)
-                _cameraManualAngleLat += delta.X;
-                _cameraManualAngleLong += -delta.Y;
-
-                if (_cameraManualAngleLong > Math.PI / 2 - 0.001f)
-                {
-                    _cameraManualAngleLong = (float)(Math.PI / 2) - 0.001f;
-                }
-                else if (_cameraManualAngleLong < -Math.PI / 2 + 0.001f)
-                {
-                    _cameraManualAngleLong = (float)(-Math.PI / 2) + 0.001f;
-                }
-
-                // Update mouse coordinates for next time
-                _pMouseCoords = new Vector2(mouseState.X, mouseState.Y);
-
-                _mousePressed = true;
-                anyInput = true;
-            }
-            else
-            {
-                if (_mousePressed)
-                    _mousePressedWithin = false;
-                _mousePressed = false;
-            }
-
-            // Don't do anything if we don't have focus
-            if (_graphics.Control.Focused)
-                return;
-
-            if (!_pMouseScroll.HasValue)
-                _pMouseScroll = mouseState.ScrollWheelValue;
-            float deltaScroll = mouseState.ScrollWheelValue - _pMouseScroll.Value;
-            //_zoom += deltaScroll * 0.1f * speedMul;
-            _pMouseScroll = mouseState.ScrollWheelValue;
-
-            Vector3 relDeltaPos = new Vector3(0, 0, 0);
-            float posSpeed = speedMul * 5.0f; // Move at a rate relative to the model size
-
-            // Handle Positional Movement 
-            if (keyState.IsKeyDown(Key.W) || keyState.IsKeyDown(Key.Up))
-            {
-                relDeltaPos.Z += posSpeed;
-                anyInput = true;
-            }
-            if (keyState.IsKeyDown(Key.A) || keyState.IsKeyDown(Key.Left))
-            {
-                relDeltaPos.X += posSpeed;
-                anyInput = true;
-            }
-            if (keyState.IsKeyDown(Key.S) || keyState.IsKeyDown(Key.Down))
-            {
-                relDeltaPos.Z += -posSpeed;
-                anyInput = true;
-            }
-            if (keyState.IsKeyDown(Key.D) || keyState.IsKeyDown(Key.Right))
-            {
-                relDeltaPos.X += -posSpeed;
-                anyInput = true;
-            }
-            if (keyState.IsKeyDown(Key.Q))
-            {
-                relDeltaPos.Y += -posSpeed;
-                anyInput = true;
-            }
-            if (keyState.IsKeyDown(Key.E))
-            {
-                relDeltaPos.Y += posSpeed;
-                anyInput = true;
-            }
-
-            // Update camera position
-            // This requires converting the coordinate system from the camera coordinates 
-            // to the world coordinates. The camera X unit is calculate from the 
-            // cross product of the camera Y unit and the camera Z unit. The camera
-            // Y unit is the world Y unit since the Y coordinate is always up.
-            // The Z unit is the normalized camera look vector (to move towards the look),
-            // Hence, move formard.
-            _perspectiveCamera.Position += Vector3.Cross(Vector3.UnitY, _cameraLook) * relDeltaPos.X
-                + Vector3.UnitY * relDeltaPos.Y
-                + _cameraLook * relDeltaPos.Z;
-        }
-        */
     }
 }
