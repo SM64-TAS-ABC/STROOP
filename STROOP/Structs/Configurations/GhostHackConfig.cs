e59ce524fc35b4c6216fb1e0b4059934d11a8f9f
Scott Buchanan <scob@seas.upenn.edu>
2020-05-24 11:22:59 -0400

    add offsets to ghost hack

--- STROOP/Structs/Configurations/GhostHackConfig.cs
+++ STROOP/Structs/Configurations/GhostHackConfig.cs
@@ -10,6 +10,16 @@ namespace STROOP.Structs.Configurations
     {
         public static readonly uint DataStartAddress = 0x80400490;
         public static readonly uint DataStructSize = 0x30;
+
         public static readonly uint NumFramesAddress = 0x804003FC;
+
+        public static readonly uint XOffset = 0x00;
+        public static readonly uint YOffset = 0x28;
+        public static readonly uint ZOffset = 0x08;
+        public static readonly uint HSpeedOffset = 0x18;
+        public static readonly uint YSpeedOffset = 0x1C;
+        public static readonly uint YawFacingOffset = 0x06;
+        public static readonly uint YawIntendedOffset = 0x24;
+        public static readonly uint ActionOffset = 0x20;
     }
 }
