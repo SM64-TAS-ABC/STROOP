b661a3eeab8fc5d892a0adf81a99ca3b2812d9ad
Scott Buchanan <scob@seas.upenn.edu>
2020-05-27 19:58:36 -0400

    fix line segment

--- STROOP/Map/MapLineSegmentObject.cs
+++ STROOP/Map/MapLineSegmentObject.cs
@@ -9,6 +9,7 @@ using STROOP.Utilities;
 using STROOP.Structs.Configurations;
 using STROOP.Structs;
 using OpenTK;
+using System.Windows.Forms;
 
 namespace STROOP.Map
 {
@@ -25,7 +26,7 @@ namespace STROOP.Map
             _posAngle2 = posAngle2;
             _backwardsSize = 0;
 
-            Size = 1;
+            Size = 0;
             OutlineWidth = 3;
             OutlineColor = Color.Red;
         }
@@ -40,14 +41,36 @@ namespace STROOP.Map
 
         protected override List<(float x, float y, float z)> GetVertices()
         {
-            double angle = PositionAngle.GetAngleTo(_posAngle1, _posAngle2, false, false);
             (double x1, double y1, double z1, double angle1) = _posAngle1.GetValues();
-            (double x2, double y2, double z2, double angle2) = _posAngle1.GetValues();
+            (double x2, double y2, double z2, double angle2) = _posAngle2.GetValues();
+            double dist = PositionAngle.GetDistance(_posAngle1, _posAngle2);
+            (double startX, double startZ) = MoreMath.ExtrapolateLine2D(x2, z2, x1, z1, dist + _backwardsSize);
+            (double endX, double endZ) = MoreMath.ExtrapolateLine2D(x1, z1, x2, z2, dist + Size);
 
             List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
-            vertices.Add(((float)_posAngle1.X, (float)_posAngle1.Y, (float)_posAngle1.Z));
-            vertices.Add(((float)_posAngle2.X, (float)_posAngle2.Y, (float)_posAngle2.Z));
+            vertices.Add(((float)startX, 0, (float)startZ));
+            vertices.Add(((float)endX, 0, (float)endZ));
             return vertices;
+        }
+
+        public override ContextMenuStrip GetContextMenuStrip()
+        {
+            if (_contextMenuStrip == null)
+            {
+                ToolStripMenuItem itemSetBackwardsSize = new ToolStripMenuItem("Set Backwards Size...");
+                itemSetBackwardsSize.Click += (sender, e) =>
+                {
+                    string text = DialogUtilities.GetStringFromDialog(labelText: "Enter backwards size.");
+                    double? backwardsSizeNullable = ParsingUtilities.ParseDoubleNullable(text);
+                    if (!backwardsSizeNullable.HasValue) return;
+                    _backwardsSize = (float)backwardsSizeNullable.Value;
+                };
+
+                _contextMenuStrip = new ContextMenuStrip();
+                _contextMenuStrip.Items.Add(itemSetBackwardsSize);
+            }
+
+            return _contextMenuStrip;
         }
 
         public override string GetName()
