81e337f74d86cd74fb9d8aee38e90339648860db
Scott Buchanan <scob@seas.upenn.edu>
2020-05-30 00:14:17 -0400

    add failsafe to float gridlines

--- STROOP/Map/MapFloatGridlinesObject.cs
+++ STROOP/Map/MapFloatGridlinesObject.cs
@@ -32,16 +32,30 @@ namespace STROOP.Map
             float zMax = Config.MapGraphics.MapViewZMax;
 
             List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+            int xCounter = 0;
             for (float x = xMin; x <= xMax; x = MoreMath.GetNextFloat(x))
             {
                 vertices.Add((x, marioY, zMin));
                 vertices.Add((x, marioY, zMax));
+                xCounter++;
+                if (xCounter > 4000) break;
             }
+            int zCounter = 0;
             for (float z = zMin; z <= zMax; z = MoreMath.GetNextFloat(z))
             {
                 vertices.Add((xMin, marioY, z));
                 vertices.Add((xMax, marioY, z));
+                zCounter++;
+                if (zCounter > 4000) break;
             }
+
+            // failsafe to prevent filling the whole screen
+            if (xCounter > Config.MapGui.GLControlMap2D.Width ||
+                zCounter > Config.MapGui.GLControlMap2D.Height)
+            {
+                return new List<(float x, float y, float z)>();
+            }
+
             return vertices;
         }
 
