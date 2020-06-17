2c757e19f58472f19f78bbc70536c7cb345327d5
Scott Buchanan <scob@seas.upenn.edu>
2020-06-16 21:05:02 -0400

    add lines for compass

--- STROOP/Map/MapCompassObject.cs
+++ STROOP/Map/MapCompassObject.cs
@@ -23,7 +23,78 @@ namespace STROOP.Map
 
         public override void DrawOn2DControl()
         {
-            List<List<(float x, float y, float z)>> quadList = new List<List<(float x, float y, float z)>>();
+            float centerX = 0;
+            float centerZ = 0;
+            float arrowLineHeight = 200;
+            float arrowLineWidth = 20;
+            float arrowHeadHeight = 50;
+            float arrowHeadWidth = 50;
+
+            List<(float x, float z)> points = new List<(float x, float z)>();
+            for (int angle = 0; angle < 65536; angle += 16384)
+            {
+                double angleUp = angle;
+                double angleDown = angle + 32768;
+                double angleLeft = angle + 16384;
+                double angleRight = angle - 16384;
+                double angleUpLeft = angle + 8192;
+                double angleUpRight = angle - 8192;
+                double angleDownLeft = angle + 24576;
+                double angleDownRight = angle - 24576;
+
+                (float x, float z) arrowBaseLeft = ((float, float))MoreMath.AddVectorToPoint(arrowLineWidth / Math.Sqrt(2), angleUpLeft, centerX, centerZ);
+                (float x, float z) arrowBaseRight = ((float, float))MoreMath.AddVectorToPoint(arrowLineWidth / Math.Sqrt(2), angleUpRight, centerX, centerZ);
+                (float x, float z) arrowHeadInnerCornerLeft = ((float, float))MoreMath.AddVectorToPoint(arrowLineHeight, angleUp, arrowBaseLeft.x, arrowBaseLeft.z);
+                (float x, float z) arrowHeadInnerCornerRight = ((float, float))MoreMath.AddVectorToPoint(arrowLineHeight, angleUp, arrowBaseRight.x, arrowBaseRight.z);
+                (float x, float z) arrowHeadCornerLeft = ((float, float))MoreMath.AddVectorToPoint((arrowHeadWidth - arrowLineWidth) / 2, angleLeft, arrowHeadInnerCornerLeft.x, arrowHeadInnerCornerLeft.z);
+                (float x, float z) arrowHeadCornerRight = ((float, float))MoreMath.AddVectorToPoint((arrowHeadWidth - arrowLineWidth) / 2, angleRight, arrowHeadInnerCornerRight.x, arrowHeadInnerCornerRight.z);
+                (float x, float z) arrowHeadPoint = ((float, float))MoreMath.AddVectorToPoint(arrowLineHeight + arrowHeadHeight, angleUp, centerX, centerZ);
+
+                points.AddRange(
+                    new List<(float x, float z)>()
+                    {
+                        arrowHeadInnerCornerRight,
+                        arrowHeadCornerRight,
+                        arrowHeadPoint,
+                        arrowHeadCornerLeft,
+                        arrowHeadInnerCornerLeft,
+                        arrowBaseLeft,
+                    });
+            }
+
+            List<(float x, float z)> pointsForControl =
+                points.ConvertAll(point => MapUtilities.ConvertCoordsForControl(point.x, point.z));
+
+            GL.BindTexture(TextureTarget.Texture2D, -1);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            // Draw outline
+            if (OutlineWidth != 0)
+            {
+                GL.Color4(OutlineColor.R, OutlineColor.G, OutlineColor.B, (byte)255);
+                GL.LineWidth(OutlineWidth);
+                GL.Begin(PrimitiveType.LineLoop);
+                foreach ((float x, float z) in pointsForControl)
+                {
+                    GL.Vertex2(x, z);
+                }
+                GL.End();
+            }
+
+            GL.Color4(1, 1, 1, 1.0f);
+
+            /*
+            List<List<(float x, float y, float z)>> quadList = new List<List<(float x, float y, float z)>>()
+            {
+                new List<(float x, float y, float z)>()
+                {
+                    (1000, 0, 1000),
+                    (1000, 0, -1000),
+                    (-1000, 0, -1000),
+                    (-1000, 0, 1000),
+                }
+            };
             List<List<(float x, float z)>> quadListForControl =
                 quadList.ConvertAll(quad => quad.ConvertAll(
                     vertex => MapUtilities.ConvertCoordsForControl(vertex.x, vertex.z)));
@@ -61,6 +132,7 @@ namespace STROOP.Map
             }
 
             GL.Color4(1, 1, 1, 1.0f);
+            */
         }
 
         public override void DrawOn3DControl()
