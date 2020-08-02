814a69868128675214221662171cedbbf1068040
Scott Buchanan <scob@seas.upenn.edu>
2020-08-02 14:57:45 -0400

    fix custom gridlines

--- STROOP/Map/MapCustomGridlinesObject.cs
+++ STROOP/Map/MapCustomGridlinesObject.cs
@@ -26,19 +26,46 @@ namespace STROOP.Map
         {
             float marioY = Config.Stream.GetSingle(MarioConfig.StructAddress + MarioConfig.YOffset);
 
-            int size = (int)Size;
+            int gridlineMin = -8192;
+            int gridlineMax = 8192;
+
+            double size = Size;
             if (size < 1) size = 1;
-            int increment = 16384 / size;
+            double increment = 16384 / size;
+
+            double viewXMin = Config.MapGraphics.MapViewXMin;
+            double viewXMax = Config.MapGraphics.MapViewXMax;
+            double viewXDiff = viewXMax - viewXMin;
+            double viewXDiffPixels = viewXDiff * Config.MapGraphics.MapViewScaleValue;
+
+            double viewZMin = Config.MapGraphics.MapViewZMin;
+            double viewZMax = Config.MapGraphics.MapViewZMax;
+            double viewZDiff = viewZMax - viewZMin;
+            double viewZDiffPixels = viewZDiff * Config.MapGraphics.MapViewScaleValue;
+
+            int xMinMultiple = Math.Max((int)((viewXMin - gridlineMin) / increment) - 1, 0);
+            int xMaxMultiple = Math.Min((int)((viewXMax - gridlineMin) / increment) + 1, (int)size);
+            int numXLines = xMaxMultiple - xMinMultiple + 1;
+
+            int zMinMultiple = Math.Max((int)((viewZMin - gridlineMin) / increment) - 1, 0);
+            int zMaxMultiple = Math.Min((int)((viewZMax - gridlineMin) / increment) + 1, (int)size);
+            int numZLines = zMaxMultiple - zMinMultiple + 1;
+
+            if (numXLines > viewXDiffPixels || numZLines > viewZDiffPixels)
+                return new List<(float x, float y, float z)>();
+            
             List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
-            for (int x = -8192; x <= 8192; x += increment)
+            for (int multiple = xMinMultiple; multiple <= xMaxMultiple; multiple++)
             {
-                vertices.Add((x, marioY, - 8192));
-                vertices.Add((x, marioY, 8192));
+                float x = (float)(multiple * increment + gridlineMin);
+                vertices.Add((x, marioY, gridlineMin));
+                vertices.Add((x, marioY, gridlineMax));
             }
-            for (int z = -8192; z <= 8192; z += increment)
+            for (int multiple = zMinMultiple; multiple <= zMaxMultiple; multiple++)
             {
-                vertices.Add((-8192, marioY, z));
-                vertices.Add((8192, marioY, z));
+                float z = (float)(multiple * increment + gridlineMin);
+                vertices.Add((gridlineMin, marioY, z));
+                vertices.Add((gridlineMax, marioY, z));
             }
             return vertices;
         }
