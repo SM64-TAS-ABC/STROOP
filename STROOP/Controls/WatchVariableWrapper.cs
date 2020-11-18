1a2c4dd642438baabfe15766b98286d6464935a0
Scott Buchanan <scob@seas.upenn.edu>
2020-11-18 05:59:11 -0500

    fix fixed address issue

--- STROOP/Controls/WatchVariableWrapper.cs
+++ STROOP/Controls/WatchVariableWrapper.cs
@@ -416,7 +416,7 @@ namespace STROOP.Controls
 
         public List<uint> GetCurrentAddressesToFix()
         {
-            return WatchVar.GetBaseAddressList();
+            return new List<uint>(WatchVar.GetBaseAddressList());
         }
 
 
