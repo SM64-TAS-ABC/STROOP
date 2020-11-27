fcc5c9687ef5c515ebc83f12384fc3cf66d48b21
Scott Buchanan <scob@seas.upenn.edu>
2020-11-27 13:34:13 -0500

    make pu gridlines work on side view

--- STROOP/Map/MapPuGridlinesObject.cs
+++ STROOP/Map/MapPuGridlinesObject.cs
@@ -115,6 +115,183 @@ namespace STROOP.Map
             }
         }
 
+        protected override List<(float x, float y, float z)> GetVerticesSideView()
+        {
+            switch (_setting)
+            {
+                case PuGridlineSetting.SETTING1:
+                    {
+                        float xCenter = Config.MapGraphics.MapViewCenterXValue;
+                        float zCenter = Config.MapGraphics.MapViewCenterZValue;
+                        int xMin = ((((int)Config.MapGraphics.MapViewXMin) / 65536) - 1) * 65536;
+                        int xMax = ((((int)Config.MapGraphics.MapViewXMax) / 65536) + 1) * 65536;
+                        int yMin = ((((int)Config.MapGraphics.MapViewYMin) / 65536) - 1) * 65536;
+                        int yMax = ((((int)Config.MapGraphics.MapViewYMax) / 65536) + 1) * 65536;
+                        int zMin = ((((int)Config.MapGraphics.MapViewZMin) / 65536) - 1) * 65536;
+                        int zMax = ((((int)Config.MapGraphics.MapViewZMax) / 65536) + 1) * 65536;
+                        switch (Config.MapGraphics.MapViewSideViewAngle)
+                        {
+                            case MapGraphics.MapSideViewAngle.Angle0:
+                            case MapGraphics.MapSideViewAngle.Angle32768:
+                                {
+                                    List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                                    for (int x = xMin; x <= xMax; x += 65536)
+                                    {
+                                        vertices.Add((x, yMin, zCenter));
+                                        vertices.Add((x, yMax, zCenter));
+                                    }
+                                    for (int y = yMin; y <= yMax; y += 65536)
+                                    {
+                                        vertices.Add((xMin, y, zCenter));
+                                        vertices.Add((xMax, y, zCenter));
+                                    }
+                                    return vertices;
+                                }
+                            case MapGraphics.MapSideViewAngle.Angle16384:
+                            case MapGraphics.MapSideViewAngle.Angle49152:
+                                {
+                                    List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                                    for (int z = zMin; z <= zMax; z += 65536)
+                                    {
+                                        vertices.Add((xCenter, yMin, z));
+                                        vertices.Add((xCenter, yMax, z));
+                                    }
+                                    for (int y = yMin; y <= yMax; y += 65536)
+                                    {
+                                        vertices.Add((zCenter, y, zMin));
+                                        vertices.Add((xCenter, y, zMax));
+                                    }
+                                    return vertices;
+                                }
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                case PuGridlineSetting.SETTING2:
+                    {
+                        float xCenter = Config.MapGraphics.MapViewCenterXValue;
+                        float zCenter = Config.MapGraphics.MapViewCenterZValue;
+                        int xMin = ((((int)Config.MapGraphics.MapViewXMin) / 65536) - 1) * 65536 - 32768;
+                        int xMax = ((((int)Config.MapGraphics.MapViewXMax) / 65536) + 1) * 65536 + 32768;
+                        int yMin = ((((int)Config.MapGraphics.MapViewYMin) / 65536) - 1) * 65536 - 32768;
+                        int yMax = ((((int)Config.MapGraphics.MapViewYMax) / 65536) + 1) * 65536 + 32768;
+                        int zMin = ((((int)Config.MapGraphics.MapViewZMin) / 65536) - 1) * 65536 - 32768;
+                        int zMax = ((((int)Config.MapGraphics.MapViewZMax) / 65536) + 1) * 65536 + 32768;
+                        switch (Config.MapGraphics.MapViewSideViewAngle)
+                        {
+                            case MapGraphics.MapSideViewAngle.Angle0:
+                            case MapGraphics.MapSideViewAngle.Angle32768:
+                                {
+                                    List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                                    for (int x = xMin; x <= xMax; x += 65536)
+                                    {
+                                        vertices.Add((x, yMin, zCenter));
+                                        vertices.Add((x, yMax, zCenter));
+                                    }
+                                    for (int y = yMin; y <= yMax; y += 65536)
+                                    {
+                                        vertices.Add((xMin, y, zCenter));
+                                        vertices.Add((xMax, y, zCenter));
+                                    }
+                                    return vertices;
+                                }
+                            case MapGraphics.MapSideViewAngle.Angle16384:
+                            case MapGraphics.MapSideViewAngle.Angle49152:
+                                {
+                                    List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                                    for (int z = zMin; z <= zMax; z += 65536)
+                                    {
+                                        vertices.Add((xCenter, yMin, z));
+                                        vertices.Add((xCenter, yMax, z));
+                                    }
+                                    for (int y = yMin; y <= yMax; y += 65536)
+                                    {
+                                        vertices.Add((zCenter, y, zMin));
+                                        vertices.Add((xCenter, y, zMax));
+                                    }
+                                    return vertices;
+                                }
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                case PuGridlineSetting.SETTING3:
+                    {
+                        float xCenter = Config.MapGraphics.MapViewCenterXValue;
+                        float zCenter = Config.MapGraphics.MapViewCenterZValue;
+                        int xMin = ((((int)Config.MapGraphics.MapViewXMin) / 65536) - 1) * 65536;
+                        int xMax = ((((int)Config.MapGraphics.MapViewXMax) / 65536) + 1) * 65536;
+                        int yMin = ((((int)Config.MapGraphics.MapViewYMin) / 65536) - 1) * 65536;
+                        int yMax = ((((int)Config.MapGraphics.MapViewYMax) / 65536) + 1) * 65536;
+                        int zMin = ((((int)Config.MapGraphics.MapViewZMin) / 65536) - 1) * 65536;
+                        int zMax = ((((int)Config.MapGraphics.MapViewZMax) / 65536) + 1) * 65536;
+                        switch (Config.MapGraphics.MapViewSideViewAngle)
+                        {
+                            case MapGraphics.MapSideViewAngle.Angle0:
+                            case MapGraphics.MapSideViewAngle.Angle32768:
+                                {
+                                    List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                                    for (int x = xMin; x <= xMax; x += 65536)
+                                    {
+                                        for (int y = yMin; y <= yMax; y += 65536)
+                                        {
+                                            float x1 = x - 8192;
+                                            float x2 = x + 8192;
+                                            float y1 = y - 8192;
+                                            float y2 = y + 8192;
+
+                                            vertices.Add((x1, y1, zCenter));
+                                            vertices.Add((x1, y2, zCenter));
+
+                                            vertices.Add((x2, y1, zCenter));
+                                            vertices.Add((x2, y2, zCenter));
+
+                                            vertices.Add((x1, y1, zCenter));
+                                            vertices.Add((x2, y1, zCenter));
+
+                                            vertices.Add((x1, y2, zCenter));
+                                            vertices.Add((x2, y2, zCenter));
+                                        }
+                                    }
+                                    return vertices;
+                                }
+                            case MapGraphics.MapSideViewAngle.Angle16384:
+                            case MapGraphics.MapSideViewAngle.Angle49152:
+                                {
+                                    List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                                    for (int z = zMin; z <= zMax; z += 65536)
+                                    {
+                                        for (int y = yMin; y <= yMax; y += 65536)
+                                        {
+                                            float z1 = z - 8192;
+                                            float z2 = z + 8192;
+                                            float y1 = y - 8192;
+                                            float y2 = y + 8192;
+
+                                            vertices.Add((xCenter, y1, z1));
+                                            vertices.Add((xCenter, y2, z1));
+
+                                            vertices.Add((xCenter, y1, z2));
+                                            vertices.Add((xCenter, y2, z2));
+
+                                            vertices.Add((xCenter, y1, z1));
+                                            vertices.Add((xCenter, y1, z2));
+
+                                            vertices.Add((xCenter, y2, z1));
+                                            vertices.Add((xCenter, y2, z2));
+                                        }
+                                    }
+                                    return vertices;
+                                }
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         public override string GetName()
         {
             return "PU Gridlines";
