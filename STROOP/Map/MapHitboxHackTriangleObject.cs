71641cbff66d9bd14f645e6646a901039f19dbf3
Scott Buchanan <scob@seas.upenn.edu>
2020-11-27 18:36:45 -0500

    separate walls/htriangle logic

--- STROOP/Map/MapHitboxHackTriangleObject.cs
+++ STROOP/Map/MapHitboxHackTriangleObject.cs
@@ -37,6 +37,11 @@ namespace STROOP.Map
             // do nothing
         }
 
+        public override void DrawOn2DControlSideViewCrossSection()
+        {
+            // do nothing
+        }
+
         public override void DrawOn3DControl()
         {
             List<List<(float x, float y, float z, Color color)>> triData = GetTrianglesWithinDist()
