7ab1969055c41536fb5b77ea1feb06420d31440f
Scott Buchanan <scob@seas.upenn.edu>
2020-11-29 15:00:57 -0500

    simplify get2ddimensions

--- STROOP/Map/MapCylinderObject.cs
+++ STROOP/Map/MapCylinderObject.cs
@@ -23,10 +23,7 @@ namespace STROOP.Map
 
         protected override List<(float centerX, float centerZ, float radius)> Get2DDimensions()
         {
-            List<(float centerX, float centerZ, float radius, float minY, float maxY)> dimensions3D = Get3DDimensions();
-            List<(float centerX, float centerZ, float radius)> dimensions2D = dimensions3D.ConvertAll(
-                dimension => (dimension.centerX, dimension.centerZ, dimension.radius));
-            return dimensions2D;
+            return Get3DDimensions().ConvertAll(dimension => (dimension.centerX, dimension.centerZ, dimension.radius));
         }
 
         protected abstract List<(float centerX, float centerZ, float radius, float minY, float maxY)> Get3DDimensions();
