ac13f9f71d4f6fb39b90fdf0bd3ebc4d29c7a847
Scott Buchanan <scob@seas.upenn.edu>
2020-12-05 07:12:42 -0500

    make quad obj work on orth view

--- STROOP/Map/MapQuadObject.cs
+++ STROOP/Map/MapQuadObject.cs
@@ -63,6 +63,48 @@ namespace STROOP.Map
             GL.Color4(1, 1, 1, 1.0f);
         }
 
+        public override void DrawOn2DControlOrthographicView()
+        {
+            List<List<(float x, float y, float z)>> quadList = GetQuadList();
+            List<List<(float x, float z)>> quadListForControl =
+                quadList.ConvertAll(quad => quad.ConvertAll(
+                    vertex => MapUtilities.ConvertCoordsForControlOrthographicView(vertex.x, vertex.y, vertex.z)));
+
+            GL.BindTexture(TextureTarget.Texture2D, -1);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+
+            // Draw quad
+            GL.Color4(Color.R, Color.G, Color.B, OpacityByte);
+            GL.Begin(PrimitiveType.Quads);
+            foreach (List<(float x, float z)> quad in quadListForControl)
+            {
+                foreach ((float x, float z) in quad)
+                {
+                    GL.Vertex2(x, z);
+                }
+            }
+            GL.End();
+
+            // Draw outline
+            if (OutlineWidth != 0)
+            {
+                GL.Color4(OutlineColor.R, OutlineColor.G, OutlineColor.B, (byte)255);
+                GL.LineWidth(OutlineWidth);
+                foreach (List<(float x, float z)> quad in quadListForControl)
+                {
+                    GL.Begin(PrimitiveType.LineLoop);
+                    foreach ((float x, float z) in quad)
+                    {
+                        GL.Vertex2(x, z);
+                    }
+                    GL.End();
+                }
+            }
+
+            GL.Color4(1, 1, 1, 1.0f);
+        }
+
         public override void DrawOn3DControl()
         {
             List<List<(float x, float y, float z)>> quadList = GetQuadList();
