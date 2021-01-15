c45286b7189af6acdd425cac54a3501910d2b8b8
Scott Buchanan <scob@seas.upenn.edu>
2021-01-15 17:56:45 -0500

    create var data class

---
+++ STROOP/Utilities/WatchVariableData.cs
@@ -0,0 +1,24 @@
+﻿using STROOP.Controls;
+using STROOP.Managers;
+using STROOP.Structs.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STROOP.Structs
+{
+    public static class WatchVariableData
+    {
+        public static Dictionary<string, List<WatchVariableControlPrecursor>> Dictionary =
+            new Dictionary<string, List<WatchVariableControlPrecursor>>()
+            {
+                [@"Config/MarioData.xml"] = new List<WatchVariableControlPrecursor>()
+                {
+
+                },
+            };
+    }
+}
