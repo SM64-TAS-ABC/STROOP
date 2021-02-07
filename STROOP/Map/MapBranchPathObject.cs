57439bd9c5d564ae82856b187128ccbc0305c5fb
Scott Buchanan <scob@seas.upenn.edu>
2021-02-07 15:12:45 -0500

    implement reset of branch path

--- STROOP/Map/MapBranchPathObject.cs
+++ STROOP/Map/MapBranchPathObject.cs
@@ -98,89 +98,100 @@ namespace STROOP.Map
 
         public override void DrawOn2DControlOrthographicView()
         {
-            //if (OutlineWidth == 0) return;
+            if (OutlineWidth == 0) return;
 
-            //List<(float x, float y, float z)> vertices = GetDictionaryValues();
-            //List<(float x, float z)> veriticesForControl =
-            //    vertices.ConvertAll(vertex => MapUtilities.ConvertCoordsForControlOrthographicView(vertex.x, vertex.y, vertex.z));
+            List<MapBranchPathObjectSegment> segments = GetSegments();
+            uint globalTimer = Config.Stream.GetUInt32(MiscConfig.GlobalTimerAddress);
 
-            //GL.BindTexture(TextureTarget.Texture2D, -1);
-            //GL.MatrixMode(MatrixMode.Modelview);
-            //GL.LoadIdentity();
-            //GL.LineWidth(OutlineWidth);
-            //for (int i = 0; i < veriticesForControl.Count - 1; i++)
-            //{
-            //    Color color = OutlineColor;
-            //    if (_useBlending)
-            //    {
-            //        int distFromEnd = veriticesForControl.Count - i - 2;
-            //        if (distFromEnd < Size)
-            //        {
-            //            color = ColorUtilities.InterpolateColor(
-            //                OutlineColor, Color, distFromEnd / (double)Size);
-            //        }
-            //        else
-            //        {
-            //            color = Color;
-            //        }
-            //    }
-            //    (float x1, float z1) = veriticesForControl[i];
-            //    (float x2, float z2) = veriticesForControl[i + 1];
-            //    GL.Color4(color.R, color.G, color.B, OpacityByte);
-            //    GL.Begin(PrimitiveType.Lines);
-            //    GL.Vertex2(x1, z1);
-            //    GL.Vertex2(x2, z2);
-            //    GL.End();
-            //}
-            //GL.Color4(1, 1, 1, 1.0f);
+            GL.BindTexture(TextureTarget.Texture2D, -1);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.LineWidth(OutlineWidth);
+            foreach (MapBranchPathObjectSegment segment in segments)
+            {
+                Color color = OutlineColor;
+                if (_useBlending)
+                {
+                    int time1 = (int)(globalTimer - Size);
+                    int time2 = (int)globalTimer;
+                    if (segment.GlobalTimer >= time2)
+                    {
+                        color = OutlineColor;
+                    }
+                    else if (segment.GlobalTimer <= time1)
+                    {
+                        color = Color;
+                    }
+                    else
+                    {
+                        int distFromEnd = (int)(time2 - segment.GlobalTimer);
+                        color = ColorUtilities.InterpolateColor(
+                            OutlineColor, Color, distFromEnd / (double)Size);
+                    }
+                }
+                (float x1, float z1) = MapUtilities.ConvertCoordsForControlOrthographicView(segment.StartX, segment.StartY, segment.StartZ);
+                (float x2, float z2) = MapUtilities.ConvertCoordsForControlOrthographicView(segment.EndX, segment.EndY, segment.EndZ);
+                GL.Color4(color.R, color.G, color.B, OpacityByte);
+                GL.Begin(PrimitiveType.Lines);
+                GL.Vertex2(x1, z1);
+                GL.Vertex2(x2, z2);
+                GL.End();
+            }
+            GL.Color4(1, 1, 1, 1.0f);
         }
 
         public override void DrawOn3DControl()
         {
-            //if (OutlineWidth == 0) return;
+            if (OutlineWidth == 0) return;
 
-            //List<(float x, float y, float z)> vertices = GetDictionaryValues();
-            //List<Map3DVertex[]> vertexArrayList = new List<Map3DVertex[]>();
-            //for (int i = 0; i < vertices.Count - 1; i++)
-            //{
-            //    Color color = OutlineColor;
-            //    if (_useBlending)
-            //    {
-            //        int distFromEnd = vertices.Count - i - 2;
-            //        if (distFromEnd < Size)
-            //        {
-            //            color = ColorUtilities.InterpolateColor(
-            //                OutlineColor, Color, distFromEnd / (double)Size);
-            //        }
-            //        else
-            //        {
-            //            color = Color;
-            //        }
-            //    }
-            //    (float x1, float y1, float z1) = vertices[i];
-            //    (float x2, float y2, float z2) = vertices[i + 1];
+            List<MapBranchPathObjectSegment> segments = GetSegments();
+            uint globalTimer = Config.Stream.GetUInt32(MiscConfig.GlobalTimerAddress);
 
-            //    vertexArrayList.Add(new Map3DVertex[]
-            //    {
-            //        new Map3DVertex(new Vector3(x1, y1, z1), color),
-            //        new Map3DVertex(new Vector3(x2, y2, z2), color),
-            //    });
-            //}
+            List<Map3DVertex[]> vertexArrayList = new List<Map3DVertex[]>();
+            foreach (MapBranchPathObjectSegment segment in segments)
+            {
+                Color color = OutlineColor;
+                if (_useBlending)
+                {
+                    int time1 = (int)(globalTimer - Size);
+                    int time2 = (int)globalTimer;
+                    if (segment.GlobalTimer >= time2)
+                    {
+                        color = OutlineColor;
+                    }
+                    else if (segment.GlobalTimer <= time1)
+                    {
+                        color = Color;
+                    }
+                    else
+                    {
+                        int distFromEnd = (int)(time2 - segment.GlobalTimer);
+                        color = ColorUtilities.InterpolateColor(
+                            OutlineColor, Color, distFromEnd / (double)Size);
+                    }
+                }
 
-            //Matrix4 viewMatrix = GetModelMatrix() * Config.Map3DCamera.Matrix;
-            //GL.UniformMatrix4(Config.Map3DGraphics.GLUniformView, false, ref viewMatrix);
+                vertexArrayList.Add(new Map3DVertex[]
+                {
+                    new Map3DVertex(new Vector3(segment.StartX, segment.StartY, segment.StartZ), color),
+                    new Map3DVertex(new Vector3(segment.EndX, segment.EndY, segment.EndZ), color),
+                });
+            }
 
-            //vertexArrayList.ForEach(vertexes =>
-            //{
-            //    int buffer = GL.GenBuffer();
-            //    GL.BindTexture(TextureTarget.Texture2D, MapUtilities.WhiteTexture);
-            //    GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
-            //    GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexes.Length * Map3DVertex.Size), vertexes, BufferUsageHint.DynamicDraw);
-            //    GL.LineWidth(OutlineWidth);
-            //    Config.Map3DGraphics.BindVertices();
-            //    GL.DrawArrays(PrimitiveType.Lines, 0, vertexes.Length);
-            //    GL.DeleteBuffer(buffer);
-            //});
+            Matrix4 viewMatrix = GetModelMatrix() * Config.Map3DCamera.Matrix;
+            GL.UniformMatrix4(Config.Map3DGraphics.GLUniformView, false, ref viewMatrix);
+
+            vertexArrayList.ForEach(vertexes =>
+            {
+                int buffer = GL.GenBuffer();
+                GL.BindTexture(TextureTarget.Texture2D, MapUtilities.WhiteTexture);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, buffer);
+                GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexes.Length * Map3DVertex.Size), vertexes, BufferUsageHint.DynamicDraw);
+                GL.LineWidth(OutlineWidth);
+                Config.Map3DGraphics.BindVertices();
+                GL.DrawArrays(PrimitiveType.Lines, 0, vertexes.Length);
+                GL.DeleteBuffer(buffer);
+            });
         }
 
         public override void Update()
