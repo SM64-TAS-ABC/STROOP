b01d49164ef1bd168f36b91cf32fd32c5280a985
Scott Buchanan <scob@seas.upenn.edu>
2021-05-07 02:19:52 -0400

    fix sector

--- STROOP/Map/MapObjectSector.cs
+++ STROOP/Map/MapObjectSector.cs
@@ -44,7 +44,7 @@ namespace STROOP.Map
                 (float controlCenterX, float controlCenterZ) = MapUtilities.ConvertCoordsForControlTopDownView(centerX, centerZ);
                 float controlAngle = angle + 32768 - Config.CurrentMapGraphics.MapViewYawValue;
                 float controlRadius = radius * Config.CurrentMapGraphics.MapViewScaleValue;
-                List <(float pointX, float pointZ)> outerPoints = Enumerable.Range(0, SpecialConfig.MapCircleNumPoints2D).ToList()
+                List<(float pointX, float pointZ)> outerPoints = Enumerable.Range(0, SpecialConfig.MapCircleNumPoints2D + 1).ToList()
                     .ConvertAll(index => (index - SpecialConfig.MapCircleNumPoints2D / 2) / (float)(SpecialConfig.MapCircleNumPoints2D / 2))
                     .ConvertAll(proportion => controlAngle + proportion * angleRadius)
                     .ConvertAll(ang => ((float, float))MoreMath.AddVectorToPoint(controlRadius, ang, controlCenterX, controlCenterZ));
