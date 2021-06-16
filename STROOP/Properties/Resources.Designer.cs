8edbd26ed472226db1f82fe99d3b57f81a3c05e5
Scott Buchanan <scob@seas.upenn.edu>
2021-06-16 17:10:40 -0400

    add red locks to resources

--- STROOP/Properties/Resources.Designer.cs
+++ STROOP/Properties/Resources.Designer.cs
@@ -431,6 +431,26 @@ namespace STROOP.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap lock_red {
+            get {
+                object obj = ResourceManager.GetObject("lock_red", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap lock_red_open {
+            get {
+                object obj = ResourceManager.GetObject("lock_red_open", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] python_xshd {
