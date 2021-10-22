c442aab457460a2c7f97ac5c756346bbcdeed95e
Scott Buchanan <scob@seas.upenn.edu>
2021-10-22 14:51:25 -0400

    add water level calulator

---
+++ STROOP/Calculators/WaterMain.cs
@@ -0,0 +1,31 @@
+﻿using STROOP.Forms;
+using STROOP.Managers;
+using STROOP.Structs.Configurations;
+using STROOP.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STROOP.Structs
+{
+    public static class WaterMain
+    {
+        public static void Test()
+        {
+            int waterLevelIndex = WaterLevelCalculator.GetWaterLevelIndex();
+
+            WaterState state = new WaterState();
+            for (int i = 0; i < 1000; i++)
+            {
+                int waterLevel = WaterLevelCalculator.GetWaterLevelFromIndex(waterLevelIndex);
+                Config.Print(i + " = " + waterLevel);
+                waterLevelIndex++;
+
+                //state.Update()
+            }
+        }
+    }
+}
