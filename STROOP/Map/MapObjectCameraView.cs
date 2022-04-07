bbfcc93fbe657839b09353b507f9a02cfd750f8c
Scott Buchanan <scob@seas.upenn.edu>
2022-04-07 19:53:33 -0400

    fix camera view

--- STROOP/Map/MapObjectCameraView.cs
+++ STROOP/Map/MapObjectCameraView.cs
@@ -19,111 +19,49 @@ namespace STROOP.Map
         public MapObjectCameraView()
             : base()
         {
+            Size = 300;
             Opacity = 0.5;
             Color = Color.Yellow;
         }
 
         protected override List<List<(float x, float y, float z, bool isHovered)>> GetQuadList(MapObjectHoverData hoverData)
         {
-            List<(int y, int xMin, int xMax, int zMin, int zMax)> waters = WaterUtilities.GetWaterLevels();
-            List<List<(float x, float y, float z, bool isHovered)>> quads =
-                new List<List<(float x, float y, float z, bool isHovered)>>();
-            for (int i = 0; i < waters.Count; i++)
+            (double camX, double camY, double camZ, double camAngle) = PositionAngle.Camera.GetValues();
+            double fov = Config.Stream.GetFloat(CameraConfig.FOVStructAddress + CameraConfig.FOVValueOffset);
+            double nearZ = 100 - Size;
+            double farZ = 20_000 + Size;
+            double angleDegrees = fov / 2 + 1;
+            double angleRadians = angleDegrees / 360 * 2 * Math.PI;
+            double nearXRadius = nearZ * Math.Tan(angleRadians) + Size;
+            double farXRadius = farZ * Math.Tan(angleRadians) + Size;
+
+            (double x, double z) pointBackCenter = MoreMath.AddVectorToPoint(nearZ, camAngle, camX, camZ);
+            (double x, double z) pointBackLeft = MoreMath.AddVectorToPoint(nearXRadius, camAngle + 16384, pointBackCenter.x, pointBackCenter.z);
+            (double x, double z) pointBackRight = MoreMath.AddVectorToPoint(nearXRadius, camAngle - 16384, pointBackCenter.x, pointBackCenter.z);
+            (double x, double z) pointFrontCenter = MoreMath.AddVectorToPoint(farZ, camAngle, camX, camZ);
+            (double x, double z) pointFrontLeft = MoreMath.AddVectorToPoint(farXRadius, camAngle + 16384, pointFrontCenter.x, pointFrontCenter.z);
+            (double x, double z) pointFrontRight = MoreMath.AddVectorToPoint(farXRadius, camAngle - 16384, pointFrontCenter.x, pointFrontCenter.z);
+
+            return new List<List<(float x, float y, float z, bool isHovered)>>()
             {
-                bool isHovered = this == hoverData?.MapObject && i == hoverData?.Index;
-                var water = waters[i];
-                List<(float x, float y, float z, bool isHovered)> quad =
-                    new List<(float x, float y, float z, bool isHovered)>();
-                quad.Add((water.xMin, water.y, water.zMin, isHovered));
-                quad.Add((water.xMin, water.y, water.zMax, isHovered));
-                quad.Add((water.xMax, water.y, water.zMax, isHovered));
-                quad.Add((water.xMax, water.y, water.zMin, isHovered));
-                quads.Add(quad);
-            }
-            return quads;
+                new List<(float x, float y, float z, bool isHovered)>()
+                {
+                    ((float)pointBackLeft.x, (float)camY, (float)pointBackLeft.z, false),
+                    ((float)pointBackRight.x, (float)camY, (float)pointBackRight.z, false),
+                    ((float)pointFrontRight.x, (float)camY, (float)pointFrontRight.z, false),
+                    ((float)pointFrontLeft.x, (float)camY, (float)pointFrontLeft.z, false),
+                }
+            };
         }
 
         public override string GetName()
         {
-            return "Waters";
+            return "Camera View";
         }
 
         public override Image GetInternalImage()
         {
-            return Config.ObjectAssociations.WatersImage;
-        }
-
-        public override MapObjectHoverData GetHoverDataTopDownView(bool isForObjectDrag, bool forceCursorPosition)
-        {
-            Point? relPosMaybe = MapObjectHoverData.GetPositionMaybe(isForObjectDrag, forceCursorPosition);
-            if (!relPosMaybe.HasValue) return null;
-            Point relPos = relPosMaybe.Value;
-            (float inGameX, float inGameZ) = MapUtilities.ConvertCoordsForInGameTopDownView(relPos.X, relPos.Y);
-            var quadList = GetQuadList(null);
-            for (int i = quadList.Count - 1; i >= 0; i--)
-            {
-                var quad = quadList[i];
-                var simpleQuad = quad.ConvertAll(q => (q.x, q.y, q.z));
-                if (MapUtilities.IsWithinRectangularQuad(simpleQuad, inGameX, inGameZ) || forceCursorPosition)
-                {
-                    return new MapObjectHoverData(this, 0, 0, 0, index: i);
-                }
-            }
-            return null;
-        }
-
-        public override MapObjectHoverData GetHoverDataOrthographicView(bool isForObjectDrag, bool forceCursorPosition)
-        {
-            Point? relPosMaybe = MapObjectHoverData.GetPositionMaybe(isForObjectDrag, forceCursorPosition);
-            if (!relPosMaybe.HasValue) return null;
-            Point relPos = relPosMaybe.Value;
-
-            var quadList = GetQuadList(null);
-            for (int i = quadList.Count - 1; i >= 0; i--)
-            {
-                var quad = quadList[i];
-                var quadForControl = quad.ConvertAll(p => MapUtilities.ConvertCoordsForControlOrthographicView(p.x, p.y, p.z));
-                if (MapUtilities.IsWithinShapeForControl(quadForControl, relPos.X, relPos.Y) || forceCursorPosition)
-                {
-                    return new MapObjectHoverData(this, 0, 0, 0, index: i);
-                }
-            }
-            return null;
-        }
-
-        public override List<ToolStripItem> GetHoverContextMenuStripItems(MapObjectHoverData hoverData)
-        {
-            List<ToolStripItem> output = base.GetHoverContextMenuStripItems(hoverData);
-
-            var quadList = GetQuadList(null);
-            var quad = quadList[hoverData.Index.Value];
-            if (quad.Count == 0) return output;
-
-            double xMin = quad.Min(p => p.x);
-            double xMax = quad.Max(p => p.x);
-            double zMin = quad.Min(p => p.z);
-            double zMax = quad.Max(p => p.z);
-            double y = quad.Max(p => p.y);
-
-            ToolStripMenuItem copyXMin = new ToolStripMenuItem(string.Format("Copy X Min ({0})", xMin));
-            ToolStripMenuItem copyXMax = new ToolStripMenuItem(string.Format("Copy X Max ({0})", xMax));
-            ToolStripMenuItem copyZMin = new ToolStripMenuItem(string.Format("Copy Z Min ({0})", zMin));
-            ToolStripMenuItem copyZMax = new ToolStripMenuItem(string.Format("Copy Z Max ({0})", zMax));
-            ToolStripMenuItem copyY = new ToolStripMenuItem(string.Format("Copy Y ({0})", y));
-
-            copyXMin.Click += (sender, e) => Clipboard.SetText(xMin.ToString());
-            copyXMax.Click += (sender, e) => Clipboard.SetText(xMax.ToString());
-            copyZMin.Click += (sender, e) => Clipboard.SetText(zMin.ToString());
-            copyZMax.Click += (sender, e) => Clipboard.SetText(zMax.ToString());
-            copyY.Click += (sender, e) => Clipboard.SetText(y.ToString());
-
-            output.Insert(0, copyXMin);
-            output.Insert(1, copyXMax);
-            output.Insert(2, copyZMin);
-            output.Insert(3, copyZMax);
-            output.Insert(4, copyY);
-
-            return output;
+            return Config.ObjectAssociations.CameraImage;
         }
     }
 }
