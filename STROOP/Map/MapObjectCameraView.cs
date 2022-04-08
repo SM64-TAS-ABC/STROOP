fccfb3c26324c232e2671b192f18e22347d3b1ad
Scott Buchanan <scob@seas.upenn.edu>
2022-04-08 02:05:47 -0400

    fix camera view

--- STROOP/Map/MapObjectCameraView.cs
+++ STROOP/Map/MapObjectCameraView.cs
@@ -29,6 +29,8 @@ namespace STROOP.Map
             (double camX, double camY, double camZ, double camAngle) = PositionAngle.Camera.GetValues();
             double camPitch = Config.Stream.GetShort(CameraConfig.StructAddress + CameraConfig.FacingPitchOffset);
             double fov = Config.Stream.GetFloat(CameraConfig.FOVStructAddress + CameraConfig.FOVValueOffset);
+            double marioY = Config.Stream.GetFloat(MarioConfig.StructAddress + MarioConfig.YOffset);
+
             double nearZ = 100 - Size;
             double farZ = 20_000 + Size;
             double angleDegrees = fov / 2 + 1;
@@ -43,15 +45,46 @@ namespace STROOP.Map
             (double x, double y, double z) pointFrontLeft = MoreMath.AddVectorToPoint(farXRadius, camAngle + 16384, pointFrontCenter.x, pointFrontCenter.y, pointFrontCenter.z);
             (double x, double y, double z) pointFrontRight = MoreMath.AddVectorToPoint(farXRadius, camAngle - 16384, pointFrontCenter.x, pointFrontCenter.y, pointFrontCenter.z);
 
+            (double x, double y, double z) getPlaneLineIntersection((double x, double y, double z) p0)
+            {
+                // x = x0 + at
+                // y = y0 + bt
+                // z = z0 + ct
+
+                double camAngleRadians = MoreMath.AngleUnitsToRadians(camAngle);
+                double camPitchRadians = MoreMath.AngleUnitsToRadians(camPitch + 16384);
+
+                double a = Math.Cos(camPitchRadians) * Math.Sin(camAngleRadians);
+                double b = Math.Sin(camPitchRadians);
+                double c = Math.Cos(camPitchRadians) * Math.Cos(camAngleRadians);
+
+                // y = M
+                // M = y0 + bt
+                // bt = M - y0
+                // t = (M - y0) / b
+
+                double y = marioY;
+                double t = (y - p0.y) / b;
+                double x = p0.x + a * t;
+                double z = p0.z + c * t;
+
+                return (x, y, z);
+            }
+
+            (double x, double y, double z) finalBackLeft = getPlaneLineIntersection(pointBackLeft);
+            (double x, double y, double z) finalBackRight = getPlaneLineIntersection(pointBackRight);
+            (double x, double y, double z) finalFrontLeft = getPlaneLineIntersection(pointFrontLeft);
+            (double x, double y, double z) finalFrontRight = getPlaneLineIntersection(pointFrontRight);
+
             return new List<List<(float x, float y, float z, bool isHovered)>>()
             {
                 new List<(float x, float y, float z, bool isHovered)>()
                 {
-                    ((float)pointBackLeft.x, (float)pointBackLeft.y, (float)pointBackLeft.z, false),
-                    ((float)pointBackRight.x, (float)pointBackRight.y, (float)pointBackRight.z, false),
-                    ((float)pointFrontRight.x, (float)pointFrontRight.y, (float)pointFrontRight.z, false),
-                    ((float)pointFrontLeft.x, (float)pointFrontLeft.y, (float)pointFrontLeft.z, false),
-                }
+                    ((float)finalBackLeft.x, (float)finalBackLeft.y, (float)finalBackLeft.z, false),
+                    ((float)finalBackRight.x, (float)finalBackRight.y, (float)finalBackRight.z, false),
+                    ((float)finalFrontRight.x, (float)finalFrontRight.y, (float)finalFrontRight.z, false),
+                    ((float)finalFrontLeft.x, (float)finalFrontLeft.y, (float)finalFrontLeft.z, false),
+                },
             };
         }
 
