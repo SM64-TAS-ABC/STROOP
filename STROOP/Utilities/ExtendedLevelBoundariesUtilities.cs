e31f00b8f1b753256873fd433308badae9797ed8
Scott Buchanan <scob@seas.upenn.edu>
2023-07-14 05:57:21 -0400

    add extended level boundaries utils class

---
+++ STROOP/Utilities/ExtendedLevelBoundariesUtilities.cs
@@ -0,0 +1,29 @@
+﻿using STROOP.Managers;
+using STROOP.Structs.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STROOP.Structs
+{
+    public static class ExtendedLevelBoundariesUtilities
+    {
+        public static List<int> GetPointsInRange(int min, int max, int gap, bool alignWithZero, bool convertBounds)
+        {
+            return null;
+        }
+
+        public static short Normalize(int value)
+        {
+            return 0;
+        }
+
+        public static short UnNormalize(int value)
+        {
+            return 0;
+        }
+    }
+}
