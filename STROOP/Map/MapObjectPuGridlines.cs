345fb82b9153a13e2052d23d8b88f40f8c6bf390
Scott Buchanan <scob@seas.upenn.edu>
2023-07-15 03:06:10 -0400

    fix pu gridlines orth intersection points

--- STROOP/Map/MapObjectPuGridlines.cs
+++ STROOP/Map/MapObjectPuGridlines.cs
@@ -481,76 +481,211 @@ namespace STROOP.Map
 
         protected override List<(float x, float y, float z)> GetGridlineIntersectionPositionsOrthographicView()
         {
-            if (_setting != PuGridlineSetting.SETTING1)
+            switch (_setting)
             {
-                return new List<(float x, float y, float z)>();
-            }
-
-            float xCenter = Config.CurrentMapGraphics.MapViewCenterXValue;
-            float yCenter = Config.CurrentMapGraphics.MapViewCenterYValue;
-            float zCenter = Config.CurrentMapGraphics.MapViewCenterZValue;
-
-            float xMin = Config.CurrentMapGraphics.MapViewXMin;
-            float xMax = Config.CurrentMapGraphics.MapViewXMax;
-            float yMin = Config.CurrentMapGraphics.MapViewYMin;
-            float yMax = Config.CurrentMapGraphics.MapViewYMax;
-            float zMin = Config.CurrentMapGraphics.MapViewZMin;
-            float zMax = Config.CurrentMapGraphics.MapViewZMax;
-
-            long goThroughValueX = 0;
-            long goThroughValueY = 0;
-            long goThroughValueZ = 0;
-            if (_useMarioAsOrigin)
-            {
-                (int puXIndex, int puYIndex, int puZIndex) = PuUtilities.GetMarioPuIndexes();
-                goThroughValueX = puXIndex * (long)puSize;
-                goThroughValueY = puYIndex * (long)puSize;
-                goThroughValueZ = puZIndex * (long)puSize;
-            }
-
-            long size = (long)Math.Max(Size, 1);
-            long gap = puSize * size;
-            List<long> xValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
-                (long)Config.CurrentMapGraphics.MapViewXMin, (long)Config.CurrentMapGraphics.MapViewXMax,
-                gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, goThroughValueX, false, true, true);
-            List<long> yValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
-                (long)Config.CurrentMapGraphics.MapViewYMin, (long)Config.CurrentMapGraphics.MapViewYMax,
-                gap, true, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, goThroughValueY, false, true, true);
-            List<long> zValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
-                (long)Config.CurrentMapGraphics.MapViewZMin, (long)Config.CurrentMapGraphics.MapViewZMax,
-                gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, goThroughValueZ, false, true, true);
-
-            if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
-                (Config.CurrentMapGraphics.MapViewYawValue == 0 ||
-                Config.CurrentMapGraphics.MapViewYawValue == 32768))
-            {
-                List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
-                foreach (long x in xValues)
-                {
-                    foreach (long y in yValues)
+                case PuGridlineSetting.SETTING1:
                     {
-                        vertices.Add((x, y, zCenter));
+                        float xCenter = Config.CurrentMapGraphics.MapViewCenterXValue;
+                        float yCenter = Config.CurrentMapGraphics.MapViewCenterYValue;
+                        float zCenter = Config.CurrentMapGraphics.MapViewCenterZValue;
+
+                        float xMin = Config.CurrentMapGraphics.MapViewXMin;
+                        float xMax = Config.CurrentMapGraphics.MapViewXMax;
+                        float yMin = Config.CurrentMapGraphics.MapViewYMin;
+                        float yMax = Config.CurrentMapGraphics.MapViewYMax;
+                        float zMin = Config.CurrentMapGraphics.MapViewZMin;
+                        float zMax = Config.CurrentMapGraphics.MapViewZMax;
+
+                        long goThroughValueX = 0;
+                        long goThroughValueY = 0;
+                        long goThroughValueZ = 0;
+                        if (_useMarioAsOrigin)
+                        {
+                            (int puXIndex, int puYIndex, int puZIndex) = PuUtilities.GetMarioPuIndexes();
+                            goThroughValueX = puXIndex * (long)puSize;
+                            goThroughValueY = puYIndex * (long)puSize;
+                            goThroughValueZ = puZIndex * (long)puSize;
+                        }
+
+                        long size = (long)Math.Max(Size, 1);
+                        long gap = puSize * size;
+                        List<long> xValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewXMin, (long)Config.CurrentMapGraphics.MapViewXMax,
+                            gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, goThroughValueX, false, true, true);
+                        List<long> yValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewYMin, (long)Config.CurrentMapGraphics.MapViewYMax,
+                            gap, true, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, goThroughValueY, false, true, true);
+                        List<long> zValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewZMin, (long)Config.CurrentMapGraphics.MapViewZMax,
+                            gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, goThroughValueZ, false, true, true);
+
+                        if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
+                            (Config.CurrentMapGraphics.MapViewYawValue == 0 ||
+                            Config.CurrentMapGraphics.MapViewYawValue == 32768))
+                        {
+                            List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                            foreach (long x in xValues)
+                            {
+                                foreach (long y in yValues)
+                                {
+                                    vertices.Add((x, y, zCenter));
+                                }
+                            }
+                            return vertices;
+                        }
+                        else if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
+                            (Config.CurrentMapGraphics.MapViewYawValue == 16384 ||
+                            Config.CurrentMapGraphics.MapViewYawValue == 49152))
+                        {
+                            List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                            foreach (long z in zValues)
+                            {
+                                foreach (long y in yValues)
+                                {
+                                    vertices.Add((xCenter, y, z));
+                                }
+                            }
+                            return vertices;
+                        }
+                        else
+                        {
+                            return new List<(float x, float y, float z)>();
+                        }
                     }
-                }
-                return vertices;
-            }
-            else if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
-                (Config.CurrentMapGraphics.MapViewYawValue == 16384 ||
-                Config.CurrentMapGraphics.MapViewYawValue == 49152))
-            {
-                List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
-                foreach (long z in zValues)
-                {
-                    foreach (long y in yValues)
+                case PuGridlineSetting.SETTING2:
                     {
-                        vertices.Add((xCenter, y, z));
+                        float xCenter = Config.CurrentMapGraphics.MapViewCenterXValue;
+                        float yCenter = Config.CurrentMapGraphics.MapViewCenterYValue;
+                        float zCenter = Config.CurrentMapGraphics.MapViewCenterZValue;
+
+                        float xMin = Config.CurrentMapGraphics.MapViewXMin;
+                        float xMax = Config.CurrentMapGraphics.MapViewXMax;
+                        float yMin = Config.CurrentMapGraphics.MapViewYMin;
+                        float yMax = Config.CurrentMapGraphics.MapViewYMax;
+                        float zMin = Config.CurrentMapGraphics.MapViewZMin;
+                        float zMax = Config.CurrentMapGraphics.MapViewZMax;
+
+                        long size = (long)Math.Max(Size, 1);
+                        long gap = puSize * size;
+                        List<long> xValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewXMin, (long)Config.CurrentMapGraphics.MapViewXMax,
+                            gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.SPACED_AROUND_ZERO, 0, false, true, true);
+                        List<long> yValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewYMin, (long)Config.CurrentMapGraphics.MapViewYMax,
+                            gap, true, ExtendedLevelBoundariesUtilities.ValueOffsetType.SPACED_AROUND_ZERO, 0, false, true, true);
+                        List<long> zValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewZMin, (long)Config.CurrentMapGraphics.MapViewZMax,
+                            gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.SPACED_AROUND_ZERO, 0, false, true, true);
+
+                        if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
+                            (Config.CurrentMapGraphics.MapViewYawValue == 0 ||
+                            Config.CurrentMapGraphics.MapViewYawValue == 32768))
+                        {
+                            List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                            foreach (long x in xValues)
+                            {
+                                foreach (long y in yValues)
+                                {
+                                    vertices.Add((x, y, zCenter));
+                                }
+                            }
+                            return vertices;
+                        }
+                        else if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
+                            (Config.CurrentMapGraphics.MapViewYawValue == 16384 ||
+                            Config.CurrentMapGraphics.MapViewYawValue == 49152))
+                        {
+                            List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                            foreach (long z in zValues)
+                            {
+                                foreach (long y in yValues)
+                                {
+                                    vertices.Add((xCenter, y, z));
+                                }
+                            }
+                            return vertices;
+                        }
+                        else
+                        {
+                            return new List<(float x, float y, float z)>();
+                        }
                     }
-                }
-                return vertices;
-            }
-            else
-            {
-                return new List<(float x, float y, float z)>();
+                case PuGridlineSetting.SETTING3:
+                    {
+                        float xCenter = Config.CurrentMapGraphics.MapViewCenterXValue;
+                        float yCenter = Config.CurrentMapGraphics.MapViewCenterYValue;
+                        float zCenter = Config.CurrentMapGraphics.MapViewCenterZValue;
+
+                        float xMin = Config.CurrentMapGraphics.MapViewXMin;
+                        float xMax = Config.CurrentMapGraphics.MapViewXMax;
+                        float yMin = Config.CurrentMapGraphics.MapViewYMin;
+                        float yMax = Config.CurrentMapGraphics.MapViewYMax;
+                        float zMin = Config.CurrentMapGraphics.MapViewZMin;
+                        float zMax = Config.CurrentMapGraphics.MapViewZMax;
+
+                        long size = (long)Math.Max(Size, 1);
+                        long gap = puSize * size;
+                        List<long> xValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewXMin, (long)Config.CurrentMapGraphics.MapViewXMax,
+                            gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, 0, false, true, true);
+                        List<long> yValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewYMin, (long)Config.CurrentMapGraphics.MapViewYMax,
+                            gap, true, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, 0, false, true, true);
+                        List<long> zValues = ExtendedLevelBoundariesUtilities.GetValuesInRange(
+                            (long)Config.CurrentMapGraphics.MapViewZMin, (long)Config.CurrentMapGraphics.MapViewZMax,
+                            gap, false, ExtendedLevelBoundariesUtilities.ValueOffsetType.GO_THROUGH_VALUE, 0, false, true, true);
+
+                        if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
+                            (Config.CurrentMapGraphics.MapViewYawValue == 0 ||
+                            Config.CurrentMapGraphics.MapViewYawValue == 32768))
+                        {
+                            List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                            foreach (long x in xValues)
+                            {
+                                foreach (long y in yValues)
+                                {
+                                    float x1 = ExtendedLevelBoundariesUtilities.GetNext(x, -halfCourseSize, false, false);
+                                    float x2 = ExtendedLevelBoundariesUtilities.GetNext(x, halfCourseSize, false, false);
+                                    float y1 = ExtendedLevelBoundariesUtilities.GetNext(y, -halfCourseSize, true, false);
+                                    float y2 = ExtendedLevelBoundariesUtilities.GetNext(y, halfCourseSize, true, false);
+
+                                    vertices.Add((x1, y1, zCenter));
+                                    vertices.Add((x1, y2, zCenter));
+                                    vertices.Add((x2, y1, zCenter));
+                                    vertices.Add((x2, y2, zCenter));
+                                }
+                            }
+                            return vertices;
+                        }
+                        else if (Config.CurrentMapGraphics.MapViewPitchValue == 0 &&
+                            (Config.CurrentMapGraphics.MapViewYawValue == 16384 ||
+                            Config.CurrentMapGraphics.MapViewYawValue == 49152))
+                        {
+                            List<(float x, float y, float z)> vertices = new List<(float x, float y, float z)>();
+                            foreach (long z in zValues)
+                            {
+                                foreach (long y in yValues)
+                                {
+                                    float z1 = ExtendedLevelBoundariesUtilities.GetNext(z, -halfCourseSize, false, false);
+                                    float z2 = ExtendedLevelBoundariesUtilities.GetNext(z, halfCourseSize, false, false);
+                                    float y1 = ExtendedLevelBoundariesUtilities.GetNext(y, -halfCourseSize, true, false);
+                                    float y2 = ExtendedLevelBoundariesUtilities.GetNext(y, halfCourseSize, true, false);
+
+                                    vertices.Add((xCenter, y1, z1));
+                                    vertices.Add((xCenter, y2, z1));
+                                    vertices.Add((xCenter, y1, z2));
+                                    vertices.Add((xCenter, y2, z2));
+                                }
+                            }
+                            return vertices;
+                        }
+                        else
+                        {
+                            return new List<(float x, float y, float z)>();
+                        }
+                    }
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
 
